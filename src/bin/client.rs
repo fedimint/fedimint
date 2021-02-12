2efa12fc6f81112d07b504651e5362fe06b06b1e
elsirion <elsirion@protonmail.com>
2021-02-12 19:39:27 +0100

    Define default log level

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -13,7 +13,9 @@ use tracing_subscriber::EnvFilter;
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
+        )
         .init();
 
     let opts: ClientOpts = StructOpt::from_args();
