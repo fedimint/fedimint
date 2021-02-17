52e76fa39f08bf1025b1cf48735edc2b558280d9
elsirion <elsirion@protonmail.com>
2021-02-17 22:51:45 +0100

    fedimint: move custom iterators into mod util

--- fedimint/src/lib.rs
+++ fedimint/src/lib.rs
@@ -1,9 +1,12 @@
+mod util;
+
+use crate::util::PartialSigZip;
 use mint_api::{Coin, CoinNonce, PartialSigResponse, RequestId, SigResponse, SignRequest};
 use std::collections::HashSet;
 use std::hash::Hash;
 use tbs::{
     combine_valid_shares, min_shares, sign_blinded_msg, verify_blind_share, Aggregatable,
-    AggregatePublicKey, BlindedMessage, BlindedSignatureShare, PublicKeyShare, SecretKeyShare,
+    AggregatePublicKey, PublicKeyShare, SecretKeyShare,
 };
 use thiserror::Error;
 use tracing::{debug, warn};
@@ -175,60 +178,6 @@ impl Mint {
     ) -> Option<PartialSigResponse> {
         if coins.len() >= new_tokens.0.len() && self.spend(coins) {
             Some(self.sign(new_tokens))
-        } else {
-            None
-        }
-    }
-}
-
-struct PartialSigZip<'a> {
-    psigs: &'a [&'a (usize, PartialSigResponse)],
-    idx: usize,
-    len: usize,
-}
-
-struct PartialSigZipIter<'a> {
-    psigs: &'a [&'a (usize, PartialSigResponse)],
-    row: usize,
-    col: usize,
-}
-
-impl<'a> PartialSigZip<'a> {
-    fn new(psigs: &'a [&'a (usize, PartialSigResponse)], len: usize) -> Self {
-        PartialSigZip { psigs, idx: 0, len }
-    }
-}
-
-impl<'a> Iterator for PartialSigZip<'a> {
-    type Item = PartialSigZipIter<'a>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.idx < self.len {
-            let res_iter = PartialSigZipIter {
-                psigs: self.psigs,
-                row: self.idx,
-                col: 0,
-            };
-            self.idx += 1;
-            Some(res_iter)
-        } else {
-            None
-        }
-    }
-}
-
-impl<'a> Iterator for PartialSigZipIter<'a>
-where
-    Self: 'a,
-{
-    type Item = (&'a usize, &'a BlindedMessage, &'a BlindedSignatureShare);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.col < self.psigs.len() {
-            let (peer_id, row) = &self.psigs[self.col];
-            let (msg, sig) = &row.0[self.row];
-            self.col += 1;
-            Some((peer_id, msg, sig))
         } else {
             None
         }
