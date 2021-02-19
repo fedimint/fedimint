d0b2995c5702d37dbe8d99cf8f4e4c64baf81561
elsirion <elsirion@protonmail.com>
2021-02-19 20:25:51 +0100

    remove unused PID list from startfed.sh

--- scripts/startfed.sh
+++ scripts/startfed.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 SIZE="$1"
-PIDS=()
 
 cargo build
 
 for ((ID=$2; ID<SIZE; ID++)); do
   (target/debug/server cfg/server-$ID.json 2>&1 | sed -e "s/^/mint $ID: /" ) &
-  PIDS+=( "$!" )
 done
 
 read -p "Press enter to stop processes"
