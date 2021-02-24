d2b3d7b633846e7442a5634ef028e5262d471699
elsirion <elsirion@protonmail.com>
2021-02-24 12:48:48 +0100

    run local example in release mode for performance reasons

--- scripts/startfed.sh
+++ scripts/startfed.sh
@@ -2,10 +2,10 @@
 
 SIZE="$1"
 
-cargo build --all-targets
+cargo build --release --all-targets
 
 for ((ID=$2; ID<SIZE; ID++)); do
-  (target/debug/server cfg/server-$ID.json 2>&1 | sed -e "s/^/mint $ID: /" ) &
+  (target/release/server cfg/server-$ID.json 2>&1 | sed -e "s/^/mint $ID: /" ) &
 done
 
 read -p "Press enter to stop processes"
