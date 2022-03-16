34566e352c4239a1d5c84c8be266eadf746192e8
nicolasusca <nicolaleonardo.susca@gmail.com>
2022-03-16 20:51:53 +0100

    WIP: refactor, move response enum to lib

--- mint-client/src/bin/clientd/main.rs
+++ mint-client/src/bin/clientd/main.rs
@@ -1,7 +1,7 @@
 use tide;
 use tide::Request;
 use tide::Response;
-use mint_client::{MintClient};
+use mint_client::{MintClient, ResBody, CoinsByTier, parse_coins, serialize_coins};
 use std::{path::PathBuf, sync::Arc};
 use minimint::config::{load_from_file, ClientConfig};
 use serde::Serialize;
@@ -11,7 +11,6 @@ use minimint_api::{Amount, OutPoint};
 use mint_client::mint::{CoinFinalizationData, SpendableCoin};
 use minimint::modules::mint::tiered::coins::Coins;
 use minimint::outcome::TransactionStatus;
-
 
 #[derive(Clone)]
 pub struct State {
@@ -23,60 +22,6 @@ struct Opts {
     workdir: PathBuf,
 }
 
-#[derive(Serialize)]
-struct InfoResponse {
-    coins : Vec<CoinsByTier>,
-    pending : PendingResponse,
-}
-
-#[derive(Serialize)]
-struct PendingResponse {
-    //TODO Also return Vec<TransactionId>
-    transactions : usize,
-    acc_qty_coins : usize,
-    acc_val_amount : Amount,
-}
-#[derive(Serialize)]
-struct CoinsByTier{
-    tier : u64,
-    quantity : usize,
-}
-#[derive(Serialize)]
-struct SpendResponse {
-    token : String,
-}
-
-#[derive(Serialize)]
-struct ReissueResponse {
-    out_point : OutPoint,
-    status : TransactionStatus,
-}
-
-impl PendingResponse {
-    fn new(all_pending : Vec<CoinFinalizationData>) -> Self {
-        let acc_qty_coins = all_pending.iter().map(|cfd| cfd.coin_count()).sum();
-        let acc_val_amount = all_pending.iter().map(|cfd| cfd.coin_amount()).sum();
-        PendingResponse { transactions : all_pending.len(), acc_qty_coins, acc_val_amount }
-    }
-}
-impl InfoResponse {
-    fn new(coins: Coins<SpendableCoin>, cfd : Vec<CoinFinalizationData>) -> Self {
-        let info_coins : Vec<CoinsByTier> = coins.coins.iter()
-            .map(|(tier, c)| CoinsByTier { quantity : c.len(), tier : tier.milli_sat})
-            .collect();
-        InfoResponse { coins : info_coins, pending : PendingResponse::new(cfd)}
-    }
-}
-
-impl ReissueResponse {
-     fn new(out_point : OutPoint, status : TransactionStatus) -> ReissueResponse {
-
-        ReissueResponse {
-            out_point,
-            status,
-        }
-    }
-}
 #[tokio::main]
 async fn main() -> tide::Result<()>{
     let opts: Opts = StructOpt::from_args();
@@ -106,7 +51,8 @@ async fn main() -> tide::Result<()>{
 async fn info(req: Request<State>) -> tide::Result {
     let mint_client = &req.state().mint_client;
     let cfd = mint_client.fetch_active_issuances();
-    let body= Body::from_json(&InfoResponse::new(mint_client.coins(), cfd)).expect("encoding error");
+    //let body= Body::from_json(&InfoResponse::new(mint_client.coins(), cfd)).expect("encoding error");
+    let body= Body::from_json(&ResBody::build_info(mint_client.coins(), cfd)).expect("encoding error");
     Ok(body.into())
 }
 
@@ -114,7 +60,7 @@ async fn info(req: Request<State>) -> tide::Result {
 async fn pending(req : Request<State>) -> tide::Result {
     let mint_client = &req.state().mint_client;
     let cfd = mint_client.fetch_active_issuances();
-    let body= Body::from_json(&PendingResponse::new(cfd)).expect("encoding error");
+    let body= Body::from_json(&ResBody::build_pending(cfd)).expect("encoding error");
     Ok(body.into())
 }
 /// Endpoint:Spend responds with (adequately) selected spendable coins
@@ -131,7 +77,7 @@ async fn spend(mut req: Request<State>) -> tide::Result {
             //TODO return error in body
         }
     };
-    let body = Body::from_json(&SpendResponse{token}).unwrap();
+    let body = Body::from_json(&ResBody::build_spend(token)).unwrap();
     Ok(body.into())
 }
 ///Endpoint:ReissueValidate starts reissuance and responds when accepted (blocking)
@@ -146,7 +92,7 @@ async fn reissue_validate(mut req: Request<State>) -> tide::Result {
         Err(e) => TransactionStatus::Error(e.to_string()),
         Ok(s) => s
     };
-    let body = Body::from_json(&ReissueResponse::new(out_point, status)).unwrap();
+    let body = Body::from_json(&ResBody::build_reissue(out_point, status)).unwrap();
     let c = Arc::clone(&mint_client);
     tokio::spawn(async move {
         fetch(c).await;
@@ -167,6 +113,7 @@ async fn reissue(mut req: Request<State>) -> tide::Result {
             Ok(s) => fetch(mint_client_task).await,
         };
     });
+    //Use ResBody::Empty ?
     Ok(Response::new(200))
 }
 
@@ -175,13 +122,4 @@ async fn fetch(mint_client : Arc<MintClient>) {
     mint_client.fetch_all_coins().await;
 }
 
-fn serialize_coins(c: &Coins<SpendableCoin>) -> String {
-    let bytes = bincode::serialize(&c).unwrap();
-    base64::encode(&bytes)
-}
-
-fn parse_coins(s: &str) -> Coins<SpendableCoin> {
-    let bytes = base64::decode(s).unwrap();
-    bincode::deserialize(&bytes).unwrap()
-}
 
