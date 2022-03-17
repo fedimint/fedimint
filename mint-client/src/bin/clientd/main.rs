f27e0e31356c45a1986e780b4fb7077f38468fde
nicolasusca <nicolaleonardo.susca@gmail.com>
2022-03-17 19:44:26 +0100

    WIP: Errors

--- mint-client/src/bin/clientd/main.rs
+++ mint-client/src/bin/clientd/main.rs
@@ -3,6 +3,9 @@ use tide::Request;
 use tide::Response;
 use mint_client::{MintClient, ResBody,parse_coins, serialize_coins};
 use std::{path::PathBuf, sync::Arc};
+use std::borrow::BorrowMut;
+use std::sync::Mutex;
+use futures::future::err;
 use minimint::config::{load_from_file, ClientConfig};
 use structopt::StructOpt;
 use tide::Body;
@@ -13,7 +16,8 @@ use minimint::outcome::TransactionStatus;
 
 #[derive(Clone)]
 pub struct State {
-    mint_client: Arc<MintClient>,
+    mint_client : Arc<MintClient>,
+    err_stack : Arc<Mutex<Vec<ResBody>>>,
 }
 
 #[derive(StructOpt)]
@@ -32,9 +36,12 @@ async fn main() -> tide::Result<()>{
         .open_tree("mint-client")
         .unwrap();
     let client = MintClient::new(cfg, Arc::new(db), Default::default());
-
+    let mut test : Vec<ResBody> = Vec::new();
+    test.push(ResBody::Error {err : "69".to_owned()});
+    test.push(ResBody::Empty);
     let state = State {
         mint_client: Arc::new(client),
+        err_stack: Arc::new(Mutex::new(Vec::new())),
     };
 
     let mut app = tide::with_state(state);
@@ -52,7 +59,8 @@ async fn main() -> tide::Result<()>{
 async fn info(req: Request<State>) -> tide::Result {
     let mint_client = &req.state().mint_client;
     let cfd = mint_client.fetch_active_issuances();
-    let body= Body::from_json(&ResBody::build_info(mint_client.coins(), cfd))?; // *1 Do I want to build a ResBody::Err instead ?
+    //This will never fail since ResBody is always build with reliable constructors and cant be 'messed up' so unwrap is ok
+    let body=  Body::from_json(&ResBody::build_info(mint_client.coins(), cfd)).unwrap();
     Ok(body.into())
 }
 
@@ -60,7 +68,8 @@ async fn info(req: Request<State>) -> tide::Result {
 async fn pending(req : Request<State>) -> tide::Result {
     let mint_client = &req.state().mint_client;
     let cfd = mint_client.fetch_active_issuances();
-    let body= Body::from_json(&ResBody::build_pending(cfd))?; // 1*
+    //This will never fail since ResBody is always build with reliable constructors and cant be 'messed up' so unwrap is ok
+    let body = Body::from_json(&ResBody::build_pending(cfd)).unwrap();
     Ok(body.into())
 }
 /// Endpoint:Spend responds with (adequately) selected spendable coins
@@ -69,7 +78,8 @@ async fn spend(mut req: Request<State>) -> tide::Result {
         Ok(i) => i,
         Err(e) => { //Approach A
             let res = ResBody::Error {err : format!("{:?}", e)}; //this dosent seem right
-            let body = Body::from_json(&res)?; // 1*
+            //Will be always Ok so unwrap is ok
+            let body = Body::from_json(&res).unwrap();
             return Ok(body.into());
         },
     };
@@ -83,7 +93,8 @@ async fn spend(mut req: Request<State>) -> tide::Result {
             ResBody::Error {err : format!("{:?}", e)} //this dosent seem right
         }
     };
-    let body = Body::from_json(&res)?; // 1*
+    //Unwrap ok
+    let body = Body::from_json(&res).unwrap();
     Ok(body.into())
 }
 ///Endpoint:ReissueValidate starts reissuance and responds when accepted (blocking)
@@ -100,8 +111,9 @@ async fn reissue_validate(mut req: Request<State>) -> tide::Result {
     };
     let body = Body::from_json(&ResBody::build_reissue(out_point, status))?;
     let c = Arc::clone(&mint_client);
+    let err_stack = Arc::clone(&req.state().err_stack);
     tokio::spawn(async move {
-        fetch(c).await;
+        fetch(c, err_stack).await;
     });
     Ok(body.into())
 }
@@ -109,6 +121,7 @@ async fn reissue_validate(mut req: Request<State>) -> tide::Result {
 async fn reissue(mut req: Request<State>) -> tide::Result {
     let value : String = req.body_json().await?;
     let mint_client = &req.state().mint_client;
+    let err_stack = Arc::clone(&req.state().err_stack);
     let mint_client_task = Arc::clone(&mint_client);
     tokio::spawn(async move {
         let coins : Coins<SpendableCoin> = parse_coins(&value);
@@ -118,18 +131,18 @@ async fn reissue(mut req: Request<State>) -> tide::Result {
             Err(_) => return, //Send via channel to a logger ?
         };
         match mint_client_task.fetch_tx_outcome(out_point.txid, true).await{
-            Ok(_) => fetch(mint_client_task).await,
-            Err(_) => (), //Send via channel to a logger ?
+            Ok(_) => fetch(mint_client_task, Arc::clone(&err_stack)).await,
+            Err(e) => (*err_stack.lock().unwrap()).push(ResBody::Error {err : format!("{:?}", e)}), //this is notoriously ugly
         };
     });
     //Use ResBody::Empty ?
     Ok(Response::new(200))
 }
 
-async fn fetch(mint_client : Arc<MintClient>) {
+async fn fetch(mint_client : Arc<MintClient>, err_stack : Arc<Mutex<Vec<ResBody>>>) {
     //log the returned txids ?
     match mint_client.fetch_all_coins().await {
-        Ok(_) => (), //Send via channel to a logger ?
-        Err(_) => () //Send via channel to a logger ?
+        Ok(_) => (), //Maybe instead of err_stack use a 'general' event_aggregator which stores err and succ events
+        Err(e) => (*err_stack.lock().unwrap()).push(ResBody::Error {err : format!("{:?}", e)}),
     }
 }
