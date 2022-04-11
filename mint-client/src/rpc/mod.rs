bdc451ae0ef48a01efea4663c05adae76c8477f2
nicolasusca <nicolaleonardo.susca@gmail.com>
2022-04-11 16:04:53 +0200

    setup a router where 'methods' and the corrosponding functions(prev. endpoints) can be registered

--- mint-client/src/rpc/mod.rs
+++ mint-client/src/rpc/mod.rs
@@ -8,16 +8,15 @@ The limited permissions granted above are perpetual and will not be revoked.
 This document and the information contained herein is provided "AS IS" and ALL WARRANTIES, EXPRESS OR IMPLIED are DISCLAIMED, INCLUDING BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION HEREIN WILL NOT INFRINGE ANY RIGHTS OR ANY IMPLIED WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
  */
 use crate::clients::user::APIResponse;
-use crate::mint::{CoinFinalizationData, SpendableCoin};
 use crate::{LightningGateway, UserClient};
 use futures::future::BoxFuture;
-use minimint::modules::mint::tiered::coins::Coins;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
 use std::future::Future;
 use std::sync::{Arc, Mutex};
 
+#[allow(dead_code)]
 const JSON_RPC: &str = "2.0";
 ///JSON-RPC Request object
 #[derive(Deserialize)]
@@ -52,4 +51,61 @@ pub struct Response<'a> {
     ///It MUST be the same as the value of the id member in the Request Object.
     ///If there was an error in detecting the id in the Request object (e.g. Parse error/Invalid Request), it MUST be Null.
     pub id: Value,
+}
+
+///RPC-API Endpoint-Router
+#[derive(Clone)]
+pub struct Shared {
+    pub client: Arc<UserClient>,
+    pub gateway: Arc<LightningGateway>,
+    pub events: Arc<Mutex<Vec<APIResponse>>>,
+}
+
+type HandlerArgs = Value;
+type Share = Arc<Shared>;
+type HandlerResult = Value;
+
+pub struct Handler {
+    func: Box<
+        dyn Fn(HandlerArgs, Share) -> BoxFuture<'static, HandlerResult> + Send + Sync + 'static,
+    >,
+}
+
+impl Handler {
+    pub fn new<P>(raw_func: fn(params: Value, shared: Share) -> P) -> Handler
+    where
+        P: Future<Output = HandlerResult> + Send + 'static,
+    {
+        Handler {
+            func: Box::new(move |params, shared| {
+                Box::pin(raw_func(params.clone(), shared.clone()))
+            }),
+        }
+    }
+
+    pub async fn call(&self, args: HandlerArgs, shared: Share) -> HandlerResult {
+        (self.func)(args, shared).await
+    }
+}
+
+pub struct Router {
+    handlers: HashMap<String, Handler>,
+}
+
+impl Router {
+    pub fn new() -> Self {
+        Self {
+            handlers: HashMap::new(),
+        }
+    }
+    pub fn add_handler<P>(mut self, name: &str, fun: fn(Value, Share) -> P) -> Self
+    where
+        P: Future<Output = HandlerResult> + Send + 'static,
+    {
+        self.handlers.insert(name.to_string(), Handler::new(fun));
+        self
+    }
+    pub fn get(&self, name: &str) -> Option<&Handler> {
+        self.handlers.get(name)
+    }
 }
