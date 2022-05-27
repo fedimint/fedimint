9d0865a4f07a68fba019df8dda722b793382b1bb
kitman <josh.kitman@protonmail.com>
2022-05-27 13:12:14 +0000

    Avoid lazy evaluation causing a recompute of blind signatures

--- crypto/tbs/src/lib.rs
+++ crypto/tbs/src/lib.rs
@@ -168,15 +168,19 @@ where
     I: IntoIterator<Item = (usize, BlindedSignatureShare)>,
     I::IntoIter: Clone + ExactSizeIterator,
 {
-    let points = sig_shares.into_iter().take(threshold).map(|(idx, share)| {
-        let x = Scalar::from((idx as u64) + 1);
-        let y = share.0.into();
-        (x, y)
-    });
+    let points = sig_shares
+        .into_iter()
+        .take(threshold)
+        .map(|(idx, share)| {
+            let x = Scalar::from((idx as u64) + 1);
+            let y = share.0.into();
+            (x, y)
+        })
+        .collect::<Vec<(Scalar, G1Projective)>>();
     if points.len() < threshold {
         panic!("Not enough signature shares");
     }
-    let bsig: G1Projective = poly::interpolate_zero(points);
+    let bsig: G1Projective = poly::interpolate_zero(points.into_iter());
     BlindedSignature(bsig.to_affine())
 }
 
