379e9308feeb15c4a07d6385c3ea1c657dd0bca8
elsirion <elsirion@users.noreply.github.com>
2022-06-03 21:28:43 +0000

    deploy: a2adbcf82c4b70c01cf7fd7fd87a4b3a740386c5

--- minimint/db/sidebar-items.js
+++ minimint/db/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["DB_PREFIX_ACCEPTED_TRANSACTION",""],["DB_PREFIX_PROPOSED_TRANSACTION",""]],"struct":[["AcceptedTransactionKey",""],["ProposedTransactionKey",""],["ProposedTransactionKeyPrefix",""]]});
+initSidebarItems({"constant":[["DB_PREFIX_ACCEPTED_TRANSACTION",""],["DB_PREFIX_DROP_PEER",""],["DB_PREFIX_PROPOSED_TRANSACTION",""]],"struct":[["AcceptedTransactionKey",""],["DropPeerKey",""],["DropPeerKeyPrefix",""],["ProposedTransactionKey",""],["ProposedTransactionKeyPrefix",""]]});
