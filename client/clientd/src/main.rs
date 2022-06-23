3bd8ea5480458f6f72672b5c43ce79086d0d6bfe
NicolaLS <nicolaleonardo.susca@gmail.com>
2022-06-23 12:16:18 +0200

    /reissue and fetching

--- client/clientd/src/main.rs
+++ client/clientd/src/main.rs
@@ -12,10 +12,14 @@ use bitcoin_hashes::hex::ToHex;
 use clap::Parser;
 use minimint_api::Amount;
 use minimint_core::config::load_from_file;
+use minimint_core::modules::mint::tiered::coins::Coins;
+use mint_client::mint::SpendableCoin;
 use mint_client::{ClientAndGatewayConfig, UserClient};
 use rand::rngs::OsRng;
 use std::path::PathBuf;
 use std::sync::Arc;
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::Sender;
 use tower::ServiceBuilder;
 use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
 use tracing::{info, Level};
@@ -26,7 +30,8 @@ struct Config {
     workdir: PathBuf,
 }
 struct State {
-    client: UserClient,
+    client: Arc<UserClient>,
+    fetch_tx: Sender<()>,
     rng: OsRng,
 }
 #[tokio::main]
@@ -46,10 +51,19 @@ async fn main() {
         .open_tree("mint-client")
         .unwrap();
 
-    let client = UserClient::new(cfg.client, Box::new(db), Default::default());
+    let client = Arc::new(UserClient::new(
+        cfg.client,
+        Box::new(db),
+        Default::default(),
+    ));
+    let (tx, mut rx) = mpsc::channel(1024);
     let rng = OsRng::new().unwrap();
 
-    let shared_state = Arc::new(State { client, rng });
+    let shared_state = Arc::new(State {
+        client: Arc::clone(&client),
+        fetch_tx: tx,
+        rng,
+    });
 
     let app = Router::new()
         .route("/getInfo", post(info))
@@ -57,6 +71,7 @@ async fn main() {
         .route("/getPeginAdress", post(pegin_address))
         .route("/pegin", post(pegin))
         .route("/spend", post(spend))
+        .route("/reissue", post(reissue))
         .layer(
             ServiceBuilder::new()
                 .layer(
@@ -67,6 +82,13 @@ async fn main() {
                 )
                 .layer(Extension(shared_state)),
         );
+
+    let fetch_client = Arc::clone(&client);
+    tokio::spawn(async move {
+        while rx.recv().await.is_some() {
+            fetch(Arc::clone(&fetch_client)).await;
+        }
+    });
 
     Server::bind(&"127.0.0.1:8081".parse().unwrap())
         .serve(app.into_make_service())
@@ -121,4 +143,22 @@ async fn spend(
 
     let spending_coins = client.select_and_spend_coins(amount).unwrap(); //TODO: handle unwrap()
     Json(SpendResponse::new(spending_coins))
+}
+
+async fn reissue(Extension(state): Extension<Arc<State>>, payload: Json<Coins<SpendableCoin>>) {
+    let state = Arc::clone(&state);
+    let coins = payload.0;
+    tokio::spawn(async move {
+        let client = &state.client;
+        let fetch_tx = state.fetch_tx.clone();
+        let mut rng = state.rng.clone();
+        //TODO: log what happens here and handle unwraps()
+        client.reissue(coins, &mut rng).await.unwrap();
+        fetch_tx.send(()).await.unwrap();
+    });
+}
+
+async fn fetch(client: Arc<UserClient>) {
+    //TODO: log txid or error (handle unwrap)
+    client.fetch_all_coins().await.unwrap();
 }
