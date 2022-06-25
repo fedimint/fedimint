2e9bf17d5879572e8d2329fd5a3fc8fcf1845049
NicolaLS <nicolaleonardo.susca@gmail.com>
2022-06-25 11:26:25 +0200

    invoice serde module

--- client/clientd/src/utils.rs
+++ client/clientd/src/utils.rs
@@ -213,3 +213,31 @@ where
         Ok(Self(decoded))
     }
 }
+
+mod serde_invoice {
+    use serde::de::Error;
+    use serde::{Deserialize, Deserializer, Serializer};
+
+    #[allow(missing_docs)]
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<lightning_invoice::Invoice, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let bolt11 = String::deserialize(deserializer)?
+            .parse::<lightning_invoice::Invoice>()
+            .map_err(|e| D::Error::custom(format!("{:?}", e)))?;
+
+        Ok(bolt11)
+    }
+    #[allow(missing_docs)]
+    #[allow(dead_code)]
+    pub fn serialize<S>(
+        invoice: &lightning_invoice::Invoice,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(invoice.to_string().as_str())
+    }
+}
