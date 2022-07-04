cac01eb0f143a8e83041f2b7027870deb55da1ac
Justin Moon <mail@justinmoon.com>
2022-07-04 11:59:33 +0200

    Update startfed.sh
    * Set default arguments
    * Runt lightningd

--- scripts/startfed.sh
+++ scripts/startfed.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-SIZE="$1"
+FED_SIZE=${1:-4}
+SKIPPED_SERVERS=${2:-0}
 
 cargo build --release --bin server
 
@@ -9,7 +10,10 @@ bitcoind -regtest -fallbackfee=0.0004 -txindex -server -rpcuser=bitcoin -rpcpass
 echo "Waiting for bitcoind to start"
 sleep 3
 
-for ((ID=$2; ID<SIZE; ID++)); do
+lightningd --network regtest --bitcoin-rpcuser=bitcoin --bitcoin-rpcpassword=bitcoin --lightning-dir=ln1 --addr=127.0.0.1:9000 &
+lightningd --network regtest --bitcoin-rpcuser=bitcoin --bitcoin-rpcpassword=bitcoin --lightning-dir=ln2 --addr=127.0.0.1:9001 &
+
+for ((ID=SKIPPED_SERVERS; ID<FED_SIZE; ID++)); do
   echo "starting mint $ID"
   (target/release/server cfg/server-$ID.json 2>&1 | sed -e "s/^/mint $ID: /" ) &
 done
