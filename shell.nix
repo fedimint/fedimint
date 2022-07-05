709c552a69e0714399fb4a66aa52c26fb515f6b5
elsirion <elsirion@protonmail.com>
2022-07-05 10:56:05 +0000

    Pin default nix-shell nixpkgs to the latest stable version to ensure that developers use the same one

--- shell.nix
+++ shell.nix
@@ -1,4 +1,11 @@
-{ pkgs ? import <nixpkgs> {}}:
+let
+  nix-pinned = builtins.fetchTarball {
+    name = "nixos-22.05";
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.05.tar.gz";
+    sha256 = "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik";
+  };
+in
+{ pkgs ? import (nix-pinned) {}}:
 let
   clightning-dev = pkgs.clightning.overrideAttrs (oldAttrs: {
     configureFlags = [ "--enable-developer" "--disable-valgrind" ];
