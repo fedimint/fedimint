779cc11c81d97cfe72b881b89aed3d73a1dd4a41
justinmoon <justinmoon@users.noreply.github.com>
2022-07-09 10:17:13 +0000

    deploy: 82fd9d13f217a992a363962f09a4c21720e037f8

--- minimint/net/sidebar-items.js
+++ minimint/net/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["api",""],["connect",""],["framed",""],["peers",""],["queue",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["api","Implements the client API through which users interact with the federation"],["connect","Provides an abstract network connection interface and multiple implementations"],["framed","Adapter that implements a message based protocol on top of a stream based one"],["peers","Implements a connection manager for communication with other federation members"],["queue",""]]};
