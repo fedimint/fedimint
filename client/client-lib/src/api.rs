a74f2162d5b3a5459c45cb01f6617a25c8c820d3
Maan2003 <manmeetmann2003@gmail.com>
2022-07-10 22:01:48 +0530

    tests for web socket reconnect

--- client/client-lib/src/api.rs
+++ client/client-lib/src/api.rs
@@ -275,3 +275,230 @@ impl<C: JsonRpcClient> WsFederationApi<C> {
         ))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use anyhow::anyhow;
+    use once_cell::sync::Lazy;
+    use std::{
+        collections::HashSet,
+        sync::{
+            atomic::{AtomicBool, AtomicUsize, Ordering},
+            Mutex,
+        },
+    };
+
+    use super::*;
+
+    type Result<T = ()> = std::result::Result<T, JsonRpcError>;
+
+    #[async_trait]
+    trait SimpleClient: Sized {
+        async fn connect() -> Result<Self>;
+        fn is_connected(&self) -> bool {
+            true
+        }
+        // reply with json
+        async fn request(&self, method: &str) -> Result<String>;
+    }
+
+    struct Client<C: SimpleClient>(C);
+
+    #[async_trait]
+    impl<C: SimpleClient + Send + Sync> JsonRpcClient for Client<C> {
+        fn is_connected(&self) -> bool {
+            self.0.is_connected()
+        }
+
+        async fn connect(_url: &str) -> Result<Self> {
+            Ok(Self(C::connect().await?))
+        }
+    }
+
+    #[async_trait]
+    impl<C: SimpleClient + Send + Sync> ClientT for Client<C> {
+        async fn request<'a, R>(
+            &self,
+            method: &'a str,
+            _params: Option<jsonrpsee_types::ParamsSer<'a>>,
+        ) -> Result<R>
+        where
+            R: jsonrpsee_core::DeserializeOwned,
+        {
+            let json = self.0.request(method).await?;
+            Ok(serde_json::from_str(&json).unwrap())
+        }
+
+        async fn notification<'a>(
+            &self,
+            _method: &'a str,
+            _params: Option<jsonrpsee_types::ParamsSer<'a>>,
+        ) -> Result<()> {
+            unimplemented!()
+        }
+
+        async fn batch_request<'a, R>(
+            &self,
+            _batch: Vec<(&'a str, Option<jsonrpsee_types::ParamsSer<'a>>)>,
+        ) -> Result<Vec<R>>
+        where
+            R: jsonrpsee_core::DeserializeOwned + Default + Clone,
+        {
+            unimplemented!()
+        }
+    }
+
+    fn federation_member<C: SimpleClient + Send + Sync>() -> FederationMember<Client<C>> {
+        FederationMember {
+            url: String::new(),
+            peer_id: PeerId::from(0),
+            client: RwLock::new(None),
+        }
+    }
+
+    #[test_log::test(tokio::test)]
+    async fn test_connect() {
+        static CONNECTION_COUNT: AtomicUsize = AtomicUsize::new(0);
+        static CONNECTED: AtomicBool = AtomicBool::new(true);
+        struct Client;
+
+        #[async_trait]
+        impl SimpleClient for Client {
+            async fn connect() -> Result<Self> {
+                CONNECTION_COUNT.fetch_add(1, Ordering::SeqCst);
+                Ok(Client)
+            }
+
+            fn is_connected(&self) -> bool {
+                CONNECTED.load(Ordering::SeqCst)
+            }
+
+            async fn request(&self, _method: &str) -> Result<String> {
+                Ok("null".to_string())
+            }
+        }
+
+        let fed = federation_member::<Client>();
+        assert_eq!(
+            CONNECTION_COUNT.load(Ordering::SeqCst),
+            0,
+            "should not connect before first request"
+        );
+
+        fed.request::<()>("", &[]).await.unwrap();
+        assert_eq!(
+            CONNECTION_COUNT.load(Ordering::SeqCst),
+            1,
+            "should connect once after first request"
+        );
+
+        fed.request::<()>("", &[]).await.unwrap();
+        assert_eq!(
+            CONNECTION_COUNT.load(Ordering::SeqCst),
+            1,
+            "should not connect again before disconnect"
+        );
+
+        // disconnect
+        CONNECTED.store(false, Ordering::SeqCst);
+
+        fed.request::<()>("", &[]).await.unwrap();
+        assert_eq!(
+            CONNECTION_COUNT.load(Ordering::SeqCst),
+            2,
+            "should connect again after disconnect"
+        );
+    }
+
+    #[test_log::test(tokio::test)]
+    async fn concurrent_requests() {
+        static CONNECTION_COUNT: AtomicUsize = AtomicUsize::new(0);
+        static FAIL: Lazy<Mutex<HashSet<usize>>> = Lazy::new(|| Mutex::new(HashSet::new()));
+
+        struct Client(usize);
+
+        #[async_trait]
+        impl SimpleClient for Client {
+            async fn connect() -> Result<Self> {
+                tracing::error!("connect");
+                let id = CONNECTION_COUNT.fetch_add(1, Ordering::SeqCst);
+                // slow down
+                tokio::time::sleep(Duration::from_millis(100)).await;
+                if FAIL.lock().unwrap().contains(&id) {
+                    Err(jsonrpsee_core::Error::Transport(anyhow!(
+                        "intentional error"
+                    )))
+                } else {
+                    Ok(Client(id))
+                }
+            }
+
+            fn is_connected(&self) -> bool {
+                !FAIL.lock().unwrap().contains(&self.0)
+            }
+
+            async fn request(&self, _method: &str) -> Result<String> {
+                if self.is_connected() {
+                    Ok("null".to_string())
+                } else {
+                    Err(jsonrpsee_core::Error::Transport(anyhow!(
+                        "client is disconnected"
+                    )))
+                }
+            }
+        }
+
+        let fed = federation_member::<Client>();
+
+        FAIL.lock().unwrap().insert(0);
+
+        assert!(
+            fed.request::<()>("", &[]).await.is_err(),
+            "connect for client 0 should fail"
+        );
+
+        // connect for client 1 should succeed
+        fed.request::<()>("", &[]).await.unwrap();
+
+        assert_eq!(
+            CONNECTION_COUNT.load(Ordering::SeqCst),
+            2,
+            "should connect again after error in first connect"
+        );
+
+        // force a new connection by disconnecting client 1
+        FAIL.lock().unwrap().insert(1);
+
+        // only connect once even for two concurrent requests
+        let (reqa, reqb) = tokio::join!(fed.request::<()>("", &[]), fed.request::<()>("", &[]));
+        reqa.expect("both request should be successful");
+        reqb.expect("both request should be successful");
+
+        assert_eq!(
+            CONNECTION_COUNT.load(Ordering::SeqCst),
+            3,
+            "should connect once even for two concurrent requests",
+        );
+
+        // force a new connection by disconnecting client 2
+        FAIL.lock().unwrap().insert(2);
+
+        // client 3 should fail
+        // client 4 should succeed
+        FAIL.lock().unwrap().insert(3);
+
+        // only connect once even for two concurrent requests
+        let (reqa, reqb) = tokio::join!(fed.request::<()>("", &[]), fed.request::<()>("", &[]));
+
+        assert_eq!(
+            CONNECTION_COUNT.load(Ordering::SeqCst),
+            5,
+            "should connect again if first concurrent request fails",
+        );
+
+        assert!(
+            reqa.is_err() ^ reqb.is_err(),
+            "exactly one of two request should succeed"
+        );
+    }
+}
