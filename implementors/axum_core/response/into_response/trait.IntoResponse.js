febc8d0df5fb11e287a94dd4c2d32b175b570d4d
justinmoon <justinmoon@users.noreply.github.com>
2022-07-26 12:14:01 +0000

    deploy: ecabd5622fa9ab59b195715aaa23bc2f8efb8907

---
+++ implementors/axum_core/response/into_response/trait.IntoResponse.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["ln_gateway"] = [{"text":"impl IntoResponse for <a class=\"enum\" href=\"ln_gateway/enum.LnGatewayError.html\" title=\"enum ln_gateway::LnGatewayError\">LnGatewayError</a>","synthetic":false,"types":["ln_gateway::LnGatewayError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
