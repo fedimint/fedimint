656fb6868e637bdc7437a06183acdcf53fd071d7
Justin Moon <mail@justinmoon.com>
2022-07-30 12:30:41 +0200

    Simplify a few Encodable/Decodable impls

--- minimint-api/src/encoding/secp256k1.rs
+++ minimint-api/src/encoding/secp256k1.rs
@@ -11,10 +11,8 @@ impl Encodable for secp256k1_zkp::ecdsa::Signature {
 }
 
 impl Decodable for secp256k1_zkp::ecdsa::Signature {
-    fn consensus_decode<D: std::io::Read>(mut d: D) -> Result<Self, DecodeError> {
-        let mut bytes = [0u8; 64];
-        d.read_exact(&mut bytes).map_err(DecodeError::from_err)?;
-        Signature::from_compact(&bytes).map_err(DecodeError::from_err)
+    fn consensus_decode<D: std::io::Read>(d: D) -> Result<Self, DecodeError> {
+        Signature::from_compact(&<[u8; 64]>::consensus_decode(d)?).map_err(DecodeError::from_err)
     }
 }
 
@@ -27,26 +25,22 @@ impl Encodable for secp256k1_zkp::XOnlyPublicKey {
 }
 
 impl Decodable for secp256k1_zkp::XOnlyPublicKey {
-    fn consensus_decode<D: std::io::Read>(mut d: D) -> Result<Self, DecodeError> {
-        let mut bytes = [0u8; 32];
-        d.read_exact(&mut bytes).map_err(DecodeError::from_err)?;
-        secp256k1_zkp::XOnlyPublicKey::from_slice(&bytes[..]).map_err(DecodeError::from_err)
+    fn consensus_decode<D: std::io::Read>(d: D) -> Result<Self, DecodeError> {
+        secp256k1_zkp::XOnlyPublicKey::from_slice(&<[u8; 32]>::consensus_decode(d)?)
+            .map_err(DecodeError::from_err)
     }
 }
 
 impl Encodable for secp256k1_zkp::PublicKey {
-    fn consensus_encode<W: std::io::Write>(&self, mut writer: W) -> Result<usize, std::io::Error> {
-        let bytes = self.serialize();
-        writer.write_all(&bytes[..])?;
-        Ok(bytes.len())
+    fn consensus_encode<W: std::io::Write>(&self, writer: W) -> Result<usize, std::io::Error> {
+        self.serialize().consensus_encode(writer)
     }
 }
 
 impl Decodable for secp256k1_zkp::PublicKey {
-    fn consensus_decode<D: std::io::Read>(mut d: D) -> Result<Self, DecodeError> {
-        let mut bytes = [0u8; 33];
-        d.read_exact(&mut bytes).map_err(DecodeError::from_err)?;
-        secp256k1_zkp::PublicKey::from_slice(&bytes[..]).map_err(DecodeError::from_err)
+    fn consensus_decode<D: std::io::Read>(d: D) -> Result<Self, DecodeError> {
+        secp256k1_zkp::PublicKey::from_slice(&<[u8; 33]>::consensus_decode(d)?)
+            .map_err(DecodeError::from_err)
     }
 }
 
@@ -63,9 +57,8 @@ impl Encodable for secp256k1_zkp::schnorr::Signature {
 }
 
 impl Decodable for secp256k1_zkp::schnorr::Signature {
-    fn consensus_decode<D: std::io::Read>(mut d: D) -> Result<Self, DecodeError> {
-        let mut bytes = [0u8; secp256k1_zkp::constants::SCHNORR_SIGNATURE_SIZE];
-        d.read_exact(&mut bytes).map_err(DecodeError::from_err)?;
+    fn consensus_decode<D: std::io::Read>(d: D) -> Result<Self, DecodeError> {
+        let bytes = <[u8; secp256k1_zkp::constants::SCHNORR_SIGNATURE_SIZE]>::consensus_decode(d)?;
         secp256k1_zkp::schnorr::Signature::from_slice(&bytes).map_err(DecodeError::from_err)
     }
 }
