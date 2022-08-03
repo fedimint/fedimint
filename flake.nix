08c7db5458b2caa775c02eeb5b20fffb5488eca0
Dawid Ciężarkiewicz <dpc@dpc.pw>
2022-08-02 22:48:50 -0700

    Nix Flake PoC

--- flake.nix
+++ flake.nix
@@ -4,16 +4,26 @@
     crane.url = "github:ipetkov/crane";
     crane.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+  outputs = { self, nixpkgs, crane, flake-utils, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
 
-        craneLib = crane.lib.${system};
+        fenix-pkgs = fenix.packages.${system};
+        fenix-channel = fenix-pkgs.stable;
+
+        craneLib = (crane.mkLib pkgs).overrideScope' (final: prev: {
+          cargo = fenix-channel.cargo;
+          rustc = fenix-channel.rustc;
+        });
 
         commonArgs = {
           src = ./.;
@@ -22,6 +32,7 @@
             openssl
             pkg-config
             perl
+            fenix-channel.rustc
           ];
 
           nativeBuildInputs = with pkgs; [
@@ -29,30 +40,71 @@
           ];
         };
 
-        cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {
-          pname = "minimint";
+        minimintDeps = craneLib.buildDepsOnly (commonArgs // {
+          pname = "minimint-dependencies";
         });
 
-        myCrateClippy = craneLib.cargoClippy (commonArgs // {
-          inherit cargoArtifacts;
+        minimintClippy = craneLib.cargoClippy (commonArgs // {
+          cargoArtifacts = minimintDeps;
           cargoClippyExtraArgs = "-- --deny warnings";
         });
 
-        myCrate = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
+        minimint = craneLib.buildPackage (commonArgs // {
+          cargoArtifacts = minimintDeps;
         });
 
-        myCrateCoverage = craneLib.cargoTarpaulin (commonArgs // {
-          inherit cargoArtifacts;
+        minimintCoverage = craneLib.cargoTarpaulin (commonArgs // {
+          cargoArtifacts = minimintDeps;
         });
       in
       {
-        packages.default = myCrate;
+        packages.default = minimint;
         checks = {
-         inherit
-           myCrate
-           myCrateClippy
-           myCrateCoverage;
+          inherit
+            minimint
+            minimintClippy
+            minimintCoverage;
         };
+
+        devShell =
+          let
+            clightning-dev = pkgs.clightning.overrideAttrs (oldAttrs: {
+              configureFlags = [ "--enable-developer" "--disable-valgrind" ];
+            } // pkgs.lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+              NIX_CFLAGS_COMPILE = "-Wno-stringop-truncation";
+            });
+            bitcoind-patch-darwin = pkgs.bitcoind.overrideAttrs (oldAttrs: {
+              doCheck = !(pkgs.stdenv.isDarwin && pkgs.stdenv.isAarch64);
+            });
+          in
+
+          pkgs.mkShell {
+            buildInputs = minimintDeps.buildInputs;
+            nativeBuildInputs = with pkgs; minimintDeps.nativeBuildInputs ++ [
+              fenix-pkgs.rust-analyzer
+              fenix-channel.rustfmt
+              fenix-channel.rustc
+              fenix-channel.cargo
+
+              bc
+              perl
+              bitcoind-patch-darwin
+              clightning-dev
+              jq
+              procps
+              tmux
+              tmuxinator
+
+              # Nix
+              pkgs.nixpkgs-fmt
+              pkgs.shellcheck
+              pkgs.rnix-lsp
+              pkgs.nodePackages.bash-language-server
+            ] ++ lib.optionals stdenv.isDarwin [
+              libiconv
+              darwin.apple_sdk.frameworks.Security
+            ];
+            RUST_SRC_PATH = "${fenix-channel.rust-src}/lib/rustlib/src/rust/library";
+          };
       });
 }
