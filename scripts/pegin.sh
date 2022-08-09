e9eb05f887528247eb55d03fb65214e7a3ee6447
Dawid Ciężarkiewicz <dpc@dpc.pw>
2022-08-09 11:08:52 -0700

    Make new linters happy

--- scripts/pegin.sh
+++ scripts/pegin.sh
@@ -7,7 +7,9 @@ source ./scripts/lib.sh
 
 # Let's define some shortcuts for bitcoind and the mint client
 POLL_INTERVAL=1
+export POLL_INTERVAL
 # Bitcoin amount in satoshi
+
 PEG_IN_AMOUNT=${PEG_IN_AMOUNT:-$1}
 USE_GATEWAY=${2:-0}
 
@@ -18,7 +20,7 @@ echo "Pegging in $PEG_IN_AMOUNT"
 if [ "$USE_GATEWAY" == 1 ]; then ADDR="$($FM_LN1 -H gw-address)"; else ADDR="$($FM_MINT_CLIENT peg-in-address)"; fi
 
 # We send the amount we want to peg-in to this address
-TX_ID="$($FM_BTC_CLIENT sendtoaddress $ADDR 0$(echo "scale=8; $PEG_IN_AMOUNT/100000000" | bc ))"
+TX_ID="$($FM_BTC_CLIENT sendtoaddress $ADDR 0"$(echo "scale=8; $PEG_IN_AMOUNT/100000000" | bc )")"
 
 # Now we "wait" for confirmations
 $FM_BTC_CLIENT generatetoaddress 11 "$($FM_BTC_CLIENT getnewaddress)"
