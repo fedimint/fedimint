a92e1ece92dc256c9a7823ce3de837251dc08f31
justinmoon <justinmoon@users.noreply.github.com>
2022-08-10 21:15:58 +0000

    deploy: 7fcbdd4f95b918190df886492f07ba9689997846

---
+++ ln_gateway/webserver/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["pay_invoice",""],["run_webserver",""]]};
--- source-files.js
+++ source-files.js
@@ -2,13 +2,14 @@ var sourcesIndex = JSON.parse('{\
 "clientd":["",[],["lib.rs"]],\
 "clientd_cli":["",[],["clientd-cli.rs"]],\
 "configgen":["",[],["configgen.rs"]],\
-"ln_gateway":["",[],["ln_gateway.rs"]],\
+"ln_gateway":["",[],["cln.rs","lib.rs","ln.rs","webserver.rs"]],\
 "minimint":["",[["consensus",[],["conflictfilter.rs","debug.rs","interconnect.rs","mod.rs"]],["net",[],["api.rs","connect.rs","framed.rs","mod.rs","peers.rs","queue.rs"]]],["config.rs","db.rs","lib.rs","rng.rs"]],\
 "minimint_api":["",[["db",[],["batch.rs","mem_impl.rs","mod.rs","rocksdb_impl.rs","sled_impl.rs"]],["encoding",[],["btc.rs","mod.rs","secp256k1.rs","tbs.rs"]],["module",[],["audit.rs","interconnect.rs","mod.rs","testing.rs"]]],["config.rs","lib.rs","rand.rs","task.rs"]],\
 "minimint_core":["",[],["config.rs","lib.rs","outcome.rs","transaction.rs"]],\
 "minimint_derive":["",[],["lib.rs"]],\
 "minimint_ln":["",[["contracts",[],["account.rs","incoming.rs","mod.rs","outgoing.rs"]]],["config.rs","db.rs","lib.rs"]],\
 "minimint_mint":["",[["tiered",[],["coins.rs","keys.rs","mod.rs"]]],["config.rs","db.rs","lib.rs"]],\
+"minimint_tests":["",[],["main.rs"]],\
 "minimint_wallet":["",[],["bitcoincore_rpc.rs","bitcoind.rs","config.rs","db.rs","keys.rs","lib.rs","tweakable.rs","txoproof.rs"]],\
 "mint_client":["",[["ln",[],["db.rs","incoming.rs","mod.rs","outgoing.rs"]],["mint",[],["db.rs","mod.rs"]],["wallet",[],["db.rs","mod.rs"]]],["api.rs","lib.rs","transaction.rs","utils.rs"]],\
 "mint_client_cli":["",[],["main.rs"]],\
