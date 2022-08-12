f444d0f181a9260e3fa202755813a864ffd2dd25
elsirion <elsirion@users.noreply.github.com>
2022-08-12 17:44:02 +0000

    deploy: f08efae760db925c9917249c03e57f298c3f40ad

--- source-files.js
+++ source-files.js
@@ -5,7 +5,7 @@ var sourcesIndex = JSON.parse('{\
 "ln_gateway":["",[],["cln.rs","lib.rs","ln.rs","webserver.rs"]],\
 "minimint":["",[["consensus",[],["conflictfilter.rs","debug.rs","interconnect.rs","mod.rs"]],["net",[],["api.rs","connect.rs","framed.rs","mod.rs","peers.rs","queue.rs"]]],["config.rs","db.rs","lib.rs","rng.rs"]],\
 "minimint_api":["",[["db",[],["batch.rs","mem_impl.rs","mod.rs","rocksdb_impl.rs","sled_impl.rs"]],["encoding",[],["btc.rs","mod.rs","secp256k1.rs","tbs.rs"]],["module",[],["audit.rs","interconnect.rs","mod.rs","testing.rs"]]],["config.rs","lib.rs","rand.rs","task.rs"]],\
-"minimint_core":["",[],["config.rs","lib.rs","outcome.rs","transaction.rs"]],\
+"minimint_core":["",[],["config.rs","epoch.rs","lib.rs","outcome.rs","transaction.rs"]],\
 "minimint_derive":["",[],["lib.rs"]],\
 "minimint_ln":["",[["contracts",[],["account.rs","incoming.rs","mod.rs","outgoing.rs"]]],["config.rs","db.rs","lib.rs"]],\
 "minimint_mint":["",[["tiered",[],["coins.rs","keys.rs","mod.rs"]]],["config.rs","db.rs","lib.rs"]],\
