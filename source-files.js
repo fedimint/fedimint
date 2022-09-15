7d6ccd51912585cc69ede6e9d576e5f71c212059
elsirion <elsirion@users.noreply.github.com>
2022-09-15 21:16:59 +0000

    deploy: bc1c1eee7ba624260539912bedd8c36b26df50c8

--- source-files.js
+++ source-files.js
@@ -10,7 +10,7 @@ var sourcesIndex = JSON.parse('{\
 "fedimint_mint":["",[["tiered",[],["tiered_multi.rs"]]],["config.rs","db.rs","lib.rs","tiered.rs"]],\
 "fedimint_wallet":["",[],["bitcoincore_rpc.rs","bitcoind.rs","config.rs","db.rs","keys.rs","lib.rs","tweakable.rs","txoproof.rs"]],\
 "fedimintd":["",[],["fedimintd.rs"]],\
-"ln_gateway":["",[],["cln.rs","lib.rs","ln.rs","webserver.rs"]],\
+"ln_gateway":["",[],["cln.rs","lib.rs","ln.rs","util.rs","webserver.rs"]],\
 "mint_client":["",[["ln",[],["db.rs","incoming.rs","mod.rs","outgoing.rs"]],["mint",[],["db.rs","mod.rs"]],["wallet",[],["db.rs","mod.rs"]]],["api.rs","lib.rs","transaction.rs","utils.rs"]],\
 "mint_client_cli":["",[],["main.rs"]],\
 "mint_rpc_client":["",[],["mint-rpc-client.rs"]],\
