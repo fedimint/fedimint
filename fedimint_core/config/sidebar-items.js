f518461e557e395f6f1adb3a869906338ccd9df9
elsirion <elsirion@users.noreply.github.com>
2022-09-25 13:45:32 +0000

    deploy: add674bcc23399078d2ba9b383c12b420ade2eaf

--- fedimint_core/config/sidebar-items.js
+++ fedimint_core/config/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["load_from_file",""]],"mod":[["serde_binary_human_readable",""]],"struct":[["ClientConfig",""],["FeeConsensus",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["load_from_file",""]],"mod":[["serde_binary_human_readable",""]],"struct":[["ClientConfig",""],["FeeConsensus",""],["Node",""]]};
