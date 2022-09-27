9a8dd31d04e5a7adcce68cecf7c837d6c25c8ad1
Justin Moon <mail@justinmoon.com>
2022-09-27 15:36:36 -0500

    Fix gw-reload.sh

--- scripts/gw-reload.sh
+++ scripts/gw-reload.sh
@@ -4,7 +4,8 @@
 set -euo pipefail
 
 cargo build --release --bin ln_gateway
-$LN1 plugin stop ln_gateway &> /dev/null
-$LN1 -k plugin subcommand=start plugin=$BIN_DIR/ln_gateway fedimint-cfg=$CFG_DIR &> /dev/null
+
+$FM_LN1 plugin stop ln_gateway &> /dev/null || true
+$FM_LN1 -k plugin subcommand=start plugin=$FM_BIN_DIR/ln_gateway fedimint-cfg=$FM_CFG_DIR &> /dev/null
 
 echo "Gateway plugin reloaded"
