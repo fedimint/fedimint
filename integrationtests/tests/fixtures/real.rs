03821a9c54440e7da5c1362c6be2f5d444e6d820
Wilfred Allyn <wilfredallyn@protonmail.com>
2022-10-01 18:30:05 -0400

    update tests to set expiry in lightning invoice

--- integrationtests/tests/fixtures/real.rs
+++ integrationtests/tests/fixtures/real.rs
@@ -27,11 +27,11 @@ pub struct RealLightningTest {
 }
 
 impl LightningTest for RealLightningTest {
-    fn invoice(&self, amount: Amount) -> Invoice {
+    fn invoice(&self, amount: Amount, expiry_time: Option<u64>) -> Invoice {
         let random: u64 = rand::random();
         let invoice = self
             .rpc_other
-            .invoice(amount.milli_sat, &random.to_string(), "", None)
+            .invoice(amount.milli_sat, &random.to_string(), "", expiry_time)
             .unwrap();
         Invoice::from_str(&invoice.bolt11).unwrap()
     }
