f28b59220497ddd2a8da3ce96930a0d89114ce42
justinmoon <justinmoon@users.noreply.github.com>
2022-10-04 23:28:02 +0000

    deploy: ffb62bf96a94343098e8879ec85286e8f435f04b

--- implementors/fedimint_core_api/trait.PluginPendingOutput.js
+++ implementors/fedimint_core_api/trait.PluginPendingOutput.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"fedimint_mint_common":[["impl PluginPendingOutput for <a class=\"struct\" href=\"fedimint_mint_common/struct.MintPendingOutput.html\" title=\"struct fedimint_mint_common::MintPendingOutput\">MintPendingOutput</a>"]]
+"fedimint_mint_common":[["impl <a class=\"trait\" href=\"fedimint_core_api/trait.PluginPendingOutput.html\" title=\"trait fedimint_core_api::PluginPendingOutput\">PluginPendingOutput</a> for <a class=\"struct\" href=\"fedimint_mint_common/struct.MintPendingOutput.html\" title=\"struct fedimint_mint_common::MintPendingOutput\">MintPendingOutput</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
