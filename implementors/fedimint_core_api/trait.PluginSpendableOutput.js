d27266c1670af8a16d3a7159086b8ad838afb525
elsirion <elsirion@users.noreply.github.com>
2022-10-07 14:33:50 +0000

    deploy: a2f26a84934fcf812c6cb578f10347d361c8f87c

--- implementors/fedimint_core_api/trait.PluginSpendableOutput.js
+++ implementors/fedimint_core_api/trait.PluginSpendableOutput.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"fedimint_mint_common":[["impl PluginSpendableOutput for <a class=\"struct\" href=\"fedimint_mint_common/struct.MintSpendableOutput.html\" title=\"struct fedimint_mint_common::MintSpendableOutput\">MintSpendableOutput</a>"]]
+"fedimint_mint_common":[["impl <a class=\"trait\" href=\"fedimint_core_api/trait.PluginSpendableOutput.html\" title=\"trait fedimint_core_api::PluginSpendableOutput\">PluginSpendableOutput</a> for <a class=\"struct\" href=\"fedimint_mint_common/struct.MintSpendableOutput.html\" title=\"struct fedimint_mint_common::MintSpendableOutput\">MintSpendableOutput</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
