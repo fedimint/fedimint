e1c706938d98b658824cfc6b0bfa7d302bfb3ebc
Dawid Ciężarkiewicz <dpc@dpc.pw>
2022-10-11 11:21:09 -0700

    Use debug builds for dev work
    This should make setting up the dev environment much faster.
    
    If we need more performance for crypto operations, we should use:
    
    https://doc.rust-lang.org/cargo/reference/profiles.html#overrides
    
    to optimize only the most important crypto crates.

--- scripts/gw-reload.sh
+++ scripts/gw-reload.sh
@@ -3,7 +3,7 @@
 
 set -euo pipefail
 
-cargo build --release --bin ln_gateway
+cargo build --bin ln_gateway
 
 $FM_LN1 plugin stop ln_gateway &> /dev/null || true
 $FM_LN1 -k plugin subcommand=start plugin=$FM_BIN_DIR/ln_gateway fedimint-cfg=$FM_CFG_DIR &> /dev/null
--- scripts/rust-tests.sh
+++ scripts/rust-tests.sh
@@ -7,4 +7,4 @@ export RUST_LOG=info
 source ./scripts/setup-tests.sh
 
 export FM_TEST_DISABLE_MOCKS=1
-cargo test --release -p fedimint-tests -- --test-threads=1
+cargo test -p fedimint-tests -- --test-threads=1
