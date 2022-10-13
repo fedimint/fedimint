c222e04f44bc013f76525f6d24452f3706e7a4aa
Max Kunzelmann <maxdev@posteo.de>
2022-10-13 19:36:41 +0200

    Remove pinning of old revision of cln-rpc dependency

--- integrationtests/tests/fixtures/real.rs
+++ integrationtests/tests/fixtures/real.rs
@@ -30,7 +30,7 @@ impl LightningTest for RealLightningTest {
     async fn invoice(&mut self, amount: Amount, expiry_time: Option<u64>) -> Invoice {
         let random: u64 = rand::random();
         let invoice_req = requests::InvoiceRequest {
-            amount_msat: AmountOrAny::Amount(ClnRpcAmount::from_msat(amount.milli_sat)),
+            msatoshi: AmountOrAny::Amount(ClnRpcAmount::from_msat(amount.milli_sat)),
             description: "".to_string(),
             label: random.to_string(),
             expiry: expiry_time,
