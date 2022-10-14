8de2ecfc3c216aaa3a9265c59b128aff0daf3b11
dpc <dpc@users.noreply.github.com>
2022-10-14 17:11:33 +0000

    deploy: 708fb73f05b5c1b62f0561c6ec631a8c5b164a08

--- implementors/fedimint_core_api/server/trait.PluginVerificationCache.js
+++ implementors/fedimint_core_api/server/trait.PluginVerificationCache.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"fedimint_mint_server":[["impl <a class=\"trait\" href=\"fedimint_core_api/server/trait.PluginVerificationCache.html\" title=\"trait fedimint_core_api::server::PluginVerificationCache\">PluginVerificationCache</a> for <a class=\"struct\" href=\"fedimint_mint_server/struct.MintVerificationCache.html\" title=\"struct fedimint_mint_server::MintVerificationCache\">MintVerificationCache</a>"]]
+"fedimint_mint_server":[["impl PluginVerificationCache for <a class=\"struct\" href=\"fedimint_mint_server/struct.MintVerificationCache.html\" title=\"struct fedimint_mint_server::MintVerificationCache\">MintVerificationCache</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -22,7 +22,7 @@ var sourcesIndex = JSON.parse('{\
 "fedimint_sled":["",[],["lib.rs"]],\
 "fedimint_wallet":["",[],["bitcoincore_rpc.rs","bitcoind.rs","config.rs","db.rs","keys.rs","lib.rs","tweakable.rs","txoproof.rs"]],\
 "fedimintd":["",[],["main.rs"]],\
-"ln_gateway":["",[],["cln.rs","lib.rs","ln.rs","webserver.rs"]],\
+"ln_gateway":["",[],["cln.rs","lib.rs","ln.rs","rpc.rs","webserver.rs"]],\
 "mint_client":["",[["ln",[],["db.rs","incoming.rs","mod.rs","outgoing.rs"]],["mint",[],["db.rs","mod.rs"]],["wallet",[],["db.rs","mod.rs"]]],["api.rs","lib.rs","query.rs","transaction.rs","utils.rs"]],\
 "mint_rpc_client":["",[],["mint-rpc-client.rs"]],\
 "tbs":["",[["serde_impl",[],["mod.rs","scalar.rs"]]],["hash.rs","lib.rs","poly.rs"]]\
