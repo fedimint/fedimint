296b0f24a302a536e4efe77b425ddad9a2b49e29
dpc <dpc@users.noreply.github.com>
2022-10-20 19:48:32 +0000

    deploy: fd20341185247e6c85689646211a31603cfb74a5

--- fedimint_server/net/api/sidebar-items.js
+++ fedimint_server/net/api/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["attach_endpoints",""],["run_server",""],["server_endpoints",""]],"struct":[["State",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["attach_endpoints",""],["run_server",""],["server_endpoints",""]],"struct":[["InitHandle",""],["RpcHandlerCtx","A state of fedimint server passed to each rpc handler callback"]]};
