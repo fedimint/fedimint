94fad9f64418510f3c1fb072c2f81cefb9279084
elsirion <elsirion@users.noreply.github.com>
2022-10-23 16:18:03 +0000

    deploy: 45b28deb352a373241fa949f6e17a28c5e0f3ec6

--- implementors/fedimint_api/core/trait.PluginInput.js
+++ implementors/fedimint_api/core/trait.PluginInput.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"fedimint_mint_common":[["impl <a class=\"trait\" href=\"fedimint_api/core/trait.PluginInput.html\" title=\"trait fedimint_api::core::PluginInput\">PluginInput</a> for <a class=\"struct\" href=\"fedimint_mint_common/struct.MintInput.html\" title=\"struct fedimint_mint_common::MintInput\">MintInput</a>"]]
+"fedimint_mint_common":[["impl PluginInput for <a class=\"struct\" href=\"fedimint_mint_common/struct.MintInput.html\" title=\"struct fedimint_mint_common::MintInput\">MintInput</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
