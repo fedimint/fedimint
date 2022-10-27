cf318dc74a0bd8fe269a96b2b6fe7d7204dcd8d6
dpc <dpc@users.noreply.github.com>
2022-10-27 18:50:55 +0000

    deploy: 2bdc69a76e1e79d6770d70e61dee89fc24f69dcb

--- implementors/fedimint_api/core/trait.PluginConsensusItem.js
+++ implementors/fedimint_api/core/trait.PluginConsensusItem.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "fedimint_mint_common":[["impl PluginConsensusItem for <a class=\"struct\" href=\"fedimint_mint_common/struct.MintConsensusItem.html\" title=\"struct fedimint_mint_common::MintConsensusItem\">MintConsensusItem</a>"]],
-"fedimint_mint_server":[["impl PluginConsensusItem for <a class=\"struct\" href=\"fedimint_mint_server/struct.MintConsensusItem.html\" title=\"struct fedimint_mint_server::MintConsensusItem\">MintConsensusItem</a>"]]
+"fedimint_mint_server":[["impl <a class=\"trait\" href=\"fedimint_api/core/trait.PluginConsensusItem.html\" title=\"trait fedimint_api::core::PluginConsensusItem\">PluginConsensusItem</a> for <a class=\"struct\" href=\"fedimint_mint_server/struct.MintConsensusItem.html\" title=\"struct fedimint_mint_server::MintConsensusItem\">MintConsensusItem</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
