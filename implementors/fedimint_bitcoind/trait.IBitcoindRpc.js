b68c3306cce7c03a723a0858e9f529016a97af15
dpc <dpc@users.noreply.github.com>
2022-10-28 02:25:37 +0000

    deploy: d4fc5fdce23df7e42368faacc542221cc1a76606

--- implementors/fedimint_bitcoind/trait.IBitcoindRpc.js
+++ implementors/fedimint_bitcoind/trait.IBitcoindRpc.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "fedimint_bitcoind":[],
-"fedimint_testing":[["impl <a class=\"trait\" href=\"fedimint_bitcoind/trait.IBitcoindRpc.html\" title=\"trait fedimint_bitcoind::IBitcoindRpc\">IBitcoindRpc</a> for <a class=\"struct\" href=\"fedimint_testing/bitcoind/struct.FakeBitcoindRpc.html\" title=\"struct fedimint_testing::bitcoind::FakeBitcoindRpc\">FakeBitcoindRpc</a>"]]
+"fedimint_testing":[["impl IBitcoindRpc for <a class=\"struct\" href=\"fedimint_testing/bitcoind/struct.FakeBitcoindRpc.html\" title=\"struct fedimint_testing::bitcoind::FakeBitcoindRpc\">FakeBitcoindRpc</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
