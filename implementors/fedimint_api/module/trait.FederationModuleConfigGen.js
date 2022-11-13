92bba265ea03a5d0f438b22eb7c43bc77a9fe76f
dpc <dpc@users.noreply.github.com>
2022-11-13 17:49:54 +0000

    deploy: c27bd52ac763054a3210415b65c7324b191aed8e

---
+++ implementors/fedimint_api/module/trait.FederationModuleConfigGen.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {
+"fedimint_ln":[["impl FederationModuleConfigGen for <a class=\"struct\" href=\"fedimint_ln/struct.LightningModuleConfigGen.html\" title=\"struct fedimint_ln::LightningModuleConfigGen\">LightningModuleConfigGen</a>"]],
+"fedimint_mint":[["impl FederationModuleConfigGen for <a class=\"struct\" href=\"fedimint_mint/struct.MintConfigGenerator.html\" title=\"struct fedimint_mint::MintConfigGenerator\">MintConfigGenerator</a>"]],
+"fedimint_wallet":[["impl FederationModuleConfigGen for <a class=\"struct\" href=\"fedimint_wallet/struct.WalletConfigGenerator.html\" title=\"struct fedimint_wallet::WalletConfigGenerator\">WalletConfigGenerator</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
