2da5d5036746a0d1c97375956f9445909178a315
dpc <dpc@users.noreply.github.com>
2022-11-15 18:11:39 +0000

    deploy: 156763cc37a1e6b8797c463639c29858bb4388e2

--- implementors/fedimint_api/core/server/trait.InitHandle.js
+++ implementors/fedimint_api/core/server/trait.InitHandle.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"fedimint_server":[["impl&lt;'a&gt; InitHandle for <a class=\"struct\" href=\"fedimint_server/net/api/struct.InitHandle.html\" title=\"struct fedimint_server::net::api::InitHandle\">InitHandle</a>&lt;'a&gt;"]]
+"fedimint_server":[["impl&lt;'a&gt; <a class=\"trait\" href=\"fedimint_api/core/server/trait.InitHandle.html\" title=\"trait fedimint_api::core::server::InitHandle\">InitHandle</a> for <a class=\"struct\" href=\"fedimint_server/net/api/struct.InitHandle.html\" title=\"struct fedimint_server::net::api::InitHandle\">InitHandle</a>&lt;'a&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
