c7eab2f3bec8d16ab0a89e8f3841a6f55a09f7bd
okjodom <okjodom@gmail.com>
2022-11-28 12:14:30 -0800

    clean gateway fixtures

--- gateway/tests/tests/fixtures/mod.rs
+++ gateway/tests/tests/fixtures/mod.rs
@@ -1,12 +1,12 @@
+pub mod client;
+pub mod fed;
+pub mod ln;
+
 use std::sync::Arc;
 
-use anyhow::Result;
 use fedimint_api::task::TaskGroup;
 use ln_gateway::{
-    client::{GatewayClientBuilder, MemoryDbGatewayClientBuilder},
-    config::GatewayConfig,
-    rpc::GatewayRequest,
-    LnGateway,
+    client::GatewayClientBuilder, config::GatewayConfig, rpc::GatewayRequest, LnGateway,
 };
 use tokio::sync::mpsc;
 
@@ -15,12 +15,11 @@ pub struct Fixtures {
     pub task_group: TaskGroup,
 }
 
-pub async fn fixtures(gw_cfg: GatewayConfig) -> Result<Fixtures> {
+pub async fn fixtures(gw_cfg: GatewayConfig) -> anyhow::Result<Fixtures> {
     let task_group = TaskGroup::new();
 
-    let ln_rpc = Arc::new(MockLnRpc::new());
-
-    let client_builder: GatewayClientBuilder = MemoryDbGatewayClientBuilder {}.into();
+    let ln_rpc = Arc::new(ln::MockLnRpc::new());
+    let client_builder: GatewayClientBuilder = client::TestGatewayClientBuilder {}.into();
     let (tx, rx) = mpsc::channel::<GatewayRequest>(100);
 
     let gateway = LnGateway::new(gw_cfg, ln_rpc, client_builder, tx, rx, task_group.clone()).await;
