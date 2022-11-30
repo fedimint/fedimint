a5d0a2f5f8e236c48ce50d72c6927bc22055f502
elsirion <elsirion@users.noreply.github.com>
2022-11-30 23:46:14 +0000

    deploy: 1aae71cfe0c50db6fe54d49ebeb3b442c1be5e9f

--- ln_gateway/rpc/rpc_client/sidebar-items.js
+++ ln_gateway/rpc/rpc_client/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["RpcClient",""]]};
+window.SIDEBAR_ITEMS = {"struct":[["Error","The Errors that may occur when processing a `Request`."],["Response","A Response to a submitted `Request`."],["RpcClient",""]]};
