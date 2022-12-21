6a91a7a0a307db7ede6decb24233d6d56573e20a
dpc <dpc@users.noreply.github.com>
2022-12-21 01:52:10 +0000

    deploy: adf10df2dc2ac72538d8d066768d8b7d4e691627

--- implementors/fedimint_api/core/trait.PluginOutput.js
+++ implementors/fedimint_api/core/trait.PluginOutput.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"fedimint_ln":[["impl PluginOutput for <a class=\"enum\" href=\"fedimint_ln/enum.LightningOutput.html\" title=\"enum fedimint_ln::LightningOutput\">LightningOutput</a>"]],
-"fedimint_mint":[["impl PluginOutput for <a class=\"struct\" href=\"fedimint_mint/struct.MintOutput.html\" title=\"struct fedimint_mint::MintOutput\">MintOutput</a>"]],
-"fedimint_wallet":[["impl PluginOutput for <a class=\"struct\" href=\"fedimint_wallet/struct.WalletOutput.html\" title=\"struct fedimint_wallet::WalletOutput\">WalletOutput</a>"]]
+"fedimint_ln":[["impl <a class=\"trait\" href=\"fedimint_api/core/trait.PluginOutput.html\" title=\"trait fedimint_api::core::PluginOutput\">PluginOutput</a> for <a class=\"enum\" href=\"fedimint_ln/enum.LightningOutput.html\" title=\"enum fedimint_ln::LightningOutput\">LightningOutput</a>"]],
+"fedimint_mint":[["impl <a class=\"trait\" href=\"fedimint_api/core/trait.PluginOutput.html\" title=\"trait fedimint_api::core::PluginOutput\">PluginOutput</a> for <a class=\"struct\" href=\"fedimint_mint/struct.MintOutput.html\" title=\"struct fedimint_mint::MintOutput\">MintOutput</a>"]],
+"fedimint_wallet":[["impl <a class=\"trait\" href=\"fedimint_api/core/trait.PluginOutput.html\" title=\"trait fedimint_api::core::PluginOutput\">PluginOutput</a> for <a class=\"struct\" href=\"fedimint_wallet/struct.WalletOutput.html\" title=\"struct fedimint_wallet::WalletOutput\">WalletOutput</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
