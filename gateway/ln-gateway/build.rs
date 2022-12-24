c928ce53aab69d6b99f2cf36e96206e1c2ce0f99
okjodom <okjodom@gmail.com>
2022-12-24 00:46:58 -0800

    build gateway ln rpc proto spec

--- gateway/ln-gateway/build.rs
+++ gateway/ln-gateway/build.rs
@@ -1,3 +1,8 @@
 fn main() {
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .compile(&["proto/gwlightning.proto"], &["proto/"])
+        .expect("Failed to compile gateway proto files");
     fedimint_build::print_git_hash();
 }
