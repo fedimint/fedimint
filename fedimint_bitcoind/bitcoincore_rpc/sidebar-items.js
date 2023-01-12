ab5bfd5159d7c36121f2943073620308893526ed
justinmoon <justinmoon@users.noreply.github.com>
2023-01-12 14:02:20 +0000

    deploy: 17f300290519f3a118234e98243530e3d476c3b7

--- fedimint_bitcoind/bitcoincore_rpc/sidebar-items.js
+++ fedimint_bitcoind/bitcoincore_rpc/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["RPC_VERIFY_ALREADY_IN_CHAIN",""]],"fn":[["make_bitcoind_rpc",""]],"static":[["CALLSITE",""],["META",""]],"struct":[["Client",""],["ErrorReporting","Wrapper around [`bitcoincore_rpc::Client`] logging failures"]]};
+window.SIDEBAR_ITEMS = {"constant":[["RPC_VERIFY_ALREADY_IN_CHAIN",""]],"fn":[["from_url_to_url_auth",""],["make_bitcoind_rpc",""]],"static":[["CALLSITE",""],["META",""]],"struct":[["Client",""],["ErrorReporting","Wrapper around [`bitcoincore_rpc::Client`] logging failures"]]};
