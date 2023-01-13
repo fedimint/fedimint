82cbc06fad1d50f2aa1da35f7d3743ba5b0c125a
elsirion <elsirion@protonmail.com>
2023-01-13 17:02:38 -0600

    feat: `dbtool` to read, write and delete raw data from our DBs

---
+++ fedimint-dbtool/src/main.rs
@@ -0,0 +1,73 @@
+use anyhow::Result;
+use bitcoin_hashes::hex::ToHex;
+use bytes::Bytes;
+use clap::{Parser, Subcommand};
+use fedimint_api::db::Database;
+use fedimint_api::module::registry::ModuleDecoderRegistry;
+
+#[derive(Debug, Clone, Parser)]
+struct Options {
+    database: String,
+    #[command(subcommand)]
+    command: DbCommand,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum DbCommand {
+    List {
+        #[arg(value_parser = hex_parser)]
+        prefix: Bytes,
+    },
+    Write {
+        #[arg(value_parser = hex_parser)]
+        key: Bytes,
+        #[arg(value_parser = hex_parser)]
+        value: Bytes,
+    },
+    Delete {
+        #[arg(value_parser = hex_parser)]
+        prefix: Bytes,
+    },
+}
+
+fn hex_parser(hex: &str) -> Result<Bytes> {
+    let bytes: Vec<u8> = bitcoin_hashes::hex::FromHex::from_hex(hex)?;
+    Ok(bytes.into())
+}
+
+async fn open_db(path: &str) -> Result<Database> {
+    let rocksdb = fedimint_rocksdb::RocksDb::open(path)?;
+    Ok(Database::new(rocksdb, ModuleDecoderRegistry::default()))
+}
+
+fn print_kv(key: &[u8], value: &[u8]) {
+    println!("{} {}", key.to_hex(), value.to_hex());
+}
+
+#[tokio::main]
+async fn main() {
+    let options: Options = Options::parse();
+
+    let db = open_db(&options.database).await.expect("Failed to open DB");
+    let mut dbtx = db.begin_transaction().await;
+
+    match options.command {
+        DbCommand::List { prefix } => {
+            let prefix_iter = dbtx.raw_find_by_prefix(&prefix).await;
+            for db_res in prefix_iter {
+                let (key, value) = db_res.expect("DB error");
+                print_kv(&key, &value);
+            }
+        }
+        DbCommand::Write { key, value } => {
+            dbtx.raw_insert_bytes(&key, value.into())
+                .await
+                .expect("DB error");
+        }
+        DbCommand::Delete { prefix: key } => {
+            dbtx.raw_remove_entry(&key).await.expect("DB error");
+        }
+    }
+
+    dbtx.commit_tx().await.expect("DB error");
+}
