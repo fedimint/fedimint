004d562bbfff30cf27594842787197c591bb343b
elsirion <elsirion@users.noreply.github.com>
2023-01-22 20:35:57 +0000

    deploy: d42fd05b7ff9c21fa3664bb2ee5d302e5095b405

--- implementors/fedimint_api/db/trait.IDatabase.js
+++ implementors/fedimint_api/db/trait.IDatabase.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "fedimint_api":[],
 "fedimint_rocksdb":[["impl <a class=\"trait\" href=\"fedimint_api/db/trait.IDatabase.html\" title=\"trait fedimint_api::db::IDatabase\">IDatabase</a> for <a class=\"struct\" href=\"fedimint_rocksdb/struct.RocksDb.html\" title=\"struct fedimint_rocksdb::RocksDb\">RocksDb</a>"]],
-"fedimint_sqlite":[["impl <a class=\"trait\" href=\"fedimint_api/db/trait.IDatabase.html\" title=\"trait fedimint_api::db::IDatabase\">IDatabase</a> for <a class=\"struct\" href=\"fedimint_sqlite/struct.SqliteDb.html\" title=\"struct fedimint_sqlite::SqliteDb\">SqliteDb</a>"]]
+"fedimint_sqlite":[["impl IDatabase for <a class=\"struct\" href=\"fedimint_sqlite/struct.SqliteDb.html\" title=\"struct fedimint_sqlite::SqliteDb\">SqliteDb</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
