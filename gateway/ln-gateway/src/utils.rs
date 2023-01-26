f467a93dc276f3754945ad45ac43098ffb91cd40
mxz42 <mxz42@tuta.io>
2023-01-25 19:22:01 -0600

    Emphasize how often the function is run

--- gateway/ln-gateway/src/utils.rs
+++ gateway/ln-gateway/src/utils.rs
@@ -3,34 +3,41 @@ use std::{future::Future, result::Result, time::Duration};
 use tokio::time::sleep;
 use tracing::info;
 
-/// Retry an operation util the operation succeeds, OR
-/// The maximum number of attempts are made without success
-pub async fn retry<F, R, T>(
+/// Run the given closure up to `max_attempts` times while waiting for
+/// the specified `Duration` between attempts
+///
+/// # Returns
+///
+/// - If the closure is run succesfully, the result is immediately
+///   returned.
+/// - The closure is run at most `max_attempts` times after which the
+///   error of the closure is returned.
+pub async fn retry<F, Fut, T>(
     op_name: String,
     op_fn: F,
     wait: Duration,
-    max_retries: u32,
+    max_attempts: u32,
 ) -> Result<T, anyhow::Error>
 where
-    F: Fn() -> R,
-    R: Future<Output = Result<T, anyhow::Error>>,
+    F: Fn() -> Fut,
+    Fut: Future<Output = Result<T, anyhow::Error>>,
 {
-    let mut att = 0;
+    let mut counter = 1;
     loop {
         match op_fn().await {
             Ok(result) => return Ok(result),
-            Err(e) => {
-                att += 1;
-                if att > max_retries {
-                    return Err(e);
+            Err(err) => {
+                if counter == max_attempts {
+                    return Err(err);
                 }
                 info!(
                     "{} failed with error: {}. Retrying in {} seconds",
                     op_name,
-                    e,
+                    err,
                     wait.as_secs()
                 );
                 sleep(wait).await;
+                counter += 1;
             }
         }
     }
