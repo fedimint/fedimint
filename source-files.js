266f93cf65fe0bfdb5d3e5ba1500a545185f6688
elsirion <elsirion@users.noreply.github.com>
2023-02-04 01:40:21 +0000

    deploy: 0f01ecb67e9469b6f7bfef5c67edd618a13f7b13

--- source-files.js
+++ source-files.js
@@ -14,7 +14,7 @@ var sourcesIndex = JSON.parse('{\
 "fedimint_ln":["",[["contracts",[],["account.rs","incoming.rs","mod.rs","outgoing.rs"]]],["common.rs","config.rs","db.rs","lib.rs"]],\
 "fedimint_mint":["",[],["common.rs","config.rs","db.rs","lib.rs"]],\
 "fedimint_rocksdb":["",[],["lib.rs"]],\
-"fedimint_server":["",[["consensus",[],["debug.rs","interconnect.rs","mod.rs"]],["net",[],["api.rs","connect.rs","framed.rs","mod.rs","peers.rs","queue.rs"]]],["config.rs","db.rs","lib.rs","multiplexed.rs"]],\
+"fedimint_server":["",[["consensus",[],["debug.rs","interconnect.rs","mod.rs"]],["net",[],["api.rs","connect.rs","framed.rs","mod.rs","peers.rs","queue.rs"]]],["config.rs","db.rs","lib.rs","logging.rs","multiplexed.rs"]],\
 "fedimint_sqlite":["",[],["lib.rs"]],\
 "fedimint_testing":["",[["btc",[],["bitcoind.rs","fixtures.rs","mod.rs"]]],["lib.rs"]],\
 "fedimint_wallet":["",[],["common.rs","config.rs","db.rs","keys.rs","lib.rs","tweakable.rs","txoproof.rs"]],\
