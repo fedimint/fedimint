82e1e200ae3351fb108503415138b41f90695f91
elsirion <elsirion@users.noreply.github.com>
2023-02-07 13:50:46 +0000

    deploy: d4411ff0cae78773377c04e8bafd5a35c5918ef8

--- implementors/fedimint_api/module/trait.ServerModule.js
+++ implementors/fedimint_api/module/trait.ServerModule.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
-"fedimint_dummy":[["impl ServerModule for <a class=\"struct\" href=\"fedimint_dummy/struct.Dummy.html\" title=\"struct fedimint_dummy::Dummy\">Dummy</a>"]],
-"fedimint_ln":[["impl ServerModule for <a class=\"struct\" href=\"fedimint_ln/struct.Lightning.html\" title=\"struct fedimint_ln::Lightning\">Lightning</a>"]],
-"fedimint_mint":[["impl ServerModule for <a class=\"struct\" href=\"fedimint_mint/struct.Mint.html\" title=\"struct fedimint_mint::Mint\">Mint</a>"]],
-"fedimint_wallet":[["impl ServerModule for <a class=\"struct\" href=\"fedimint_wallet/struct.Wallet.html\" title=\"struct fedimint_wallet::Wallet\">Wallet</a>"]]
+"fedimint_dummy":[["impl <a class=\"trait\" href=\"fedimint_api/module/trait.ServerModule.html\" title=\"trait fedimint_api::module::ServerModule\">ServerModule</a> for <a class=\"struct\" href=\"fedimint_dummy/struct.Dummy.html\" title=\"struct fedimint_dummy::Dummy\">Dummy</a>"]],
+"fedimint_ln":[["impl <a class=\"trait\" href=\"fedimint_api/module/trait.ServerModule.html\" title=\"trait fedimint_api::module::ServerModule\">ServerModule</a> for <a class=\"struct\" href=\"fedimint_ln/struct.Lightning.html\" title=\"struct fedimint_ln::Lightning\">Lightning</a>"]],
+"fedimint_mint":[["impl <a class=\"trait\" href=\"fedimint_api/module/trait.ServerModule.html\" title=\"trait fedimint_api::module::ServerModule\">ServerModule</a> for <a class=\"struct\" href=\"fedimint_mint/struct.Mint.html\" title=\"struct fedimint_mint::Mint\">Mint</a>"]],
+"fedimint_wallet":[["impl <a class=\"trait\" href=\"fedimint_api/module/trait.ServerModule.html\" title=\"trait fedimint_api::module::ServerModule\">ServerModule</a> for <a class=\"struct\" href=\"fedimint_wallet/struct.Wallet.html\" title=\"struct fedimint_wallet::Wallet\">Wallet</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
