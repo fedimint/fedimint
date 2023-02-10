6e0e9d80f5e011f4fec390cb4bc53da34efb9f27
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-02-09 18:44:00 -0800

    feat: run integration tests with more paralellism

--- scripts/rust-tests.sh
+++ scripts/rust-tests.sh
@@ -7,4 +7,4 @@ export RUST_LOG="${RUST_LOG:-info}"
 source ./scripts/setup-tests.sh ""
 
 export FM_TEST_DISABLE_MOCKS=1
-env RUST_BACKTRACE=1 cargo test -p fedimint-tests "$@"
+env RUST_BACKTRACE=1 cargo test -p fedimint-tests -- --test-threads=$(($(nproc) * 2)) "$@"
