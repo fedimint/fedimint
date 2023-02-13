54e345ae71efead1430c93a3f97adc479ed09356
mxz42 <mxz42@tuta.io>
2023-02-13 14:11:07 -0600

    chore: `fedimint-sqlite`: group imports on module level

--- fedimint-sqlite/src/lib.rs
+++ fedimint-sqlite/src/lib.rs
@@ -155,10 +155,11 @@ impl<'a> IDatabaseTransaction<'a> for SqliteDbTransaction<'a> {
 mod fedimint_sqlite_tests {
     use std::fs;
 
-    use fedimint_api::{
-        core::ModuleInstanceId, db::Database, module::registry::ModuleDecoderRegistry,
-    };
-    use rand::{rngs::OsRng, RngCore};
+    use fedimint_api::core::ModuleInstanceId;
+    use fedimint_api::db::Database;
+    use fedimint_api::module::registry::ModuleDecoderRegistry;
+    use rand::rngs::OsRng;
+    use rand::RngCore;
 
     use crate::SqliteDb;
 
