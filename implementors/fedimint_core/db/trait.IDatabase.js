b7167444dfa5ab8408c43cd4ef09f99502387c4a
m1sterc001guy <m1sterc001guy@users.noreply.github.com>
2023-02-14 19:25:37 +0000

    deploy: 0fe205efd92ebf1223a0136285f4b6c044f58d16

--- implementors/fedimint_core/db/trait.IDatabase.js
+++ implementors/fedimint_core/db/trait.IDatabase.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "fedimint_core":[],
-"fedimint_rocksdb":[["impl IDatabase for <a class=\"struct\" href=\"fedimint_rocksdb/struct.RocksDb.html\" title=\"struct fedimint_rocksdb::RocksDb\">RocksDb</a>"]],
+"fedimint_rocksdb":[["impl <a class=\"trait\" href=\"fedimint_core/db/trait.IDatabase.html\" title=\"trait fedimint_core::db::IDatabase\">IDatabase</a> for <a class=\"struct\" href=\"fedimint_rocksdb/struct.RocksDb.html\" title=\"struct fedimint_rocksdb::RocksDb\">RocksDb</a>"]],
 "fedimint_sqlite":[["impl IDatabase for <a class=\"struct\" href=\"fedimint_sqlite/struct.SqliteDb.html\" title=\"struct fedimint_sqlite::SqliteDb\">SqliteDb</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
