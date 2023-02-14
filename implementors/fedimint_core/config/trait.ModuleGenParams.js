103e836eea09556b96d38728b37e2bba7d9079cd
dpc <dpc@users.noreply.github.com>
2023-02-14 03:56:31 +0000

    deploy: b471834896cd49fc48101e3431183ad7ce3e6bda

--- implementors/fedimint_core/config/trait.ModuleGenParams.js
+++ implementors/fedimint_core/config/trait.ModuleGenParams.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"fedimint_dummy":[["impl <a class=\"trait\" href=\"fedimint_core/config/trait.ModuleGenParams.html\" title=\"trait fedimint_core::config::ModuleGenParams\">ModuleGenParams</a> for <a class=\"struct\" href=\"fedimint_dummy/struct.DummyConfigGenParams.html\" title=\"struct fedimint_dummy::DummyConfigGenParams\">DummyConfigGenParams</a>"]],
-"fedimint_mint":[["impl <a class=\"trait\" href=\"fedimint_core/config/trait.ModuleGenParams.html\" title=\"trait fedimint_core::config::ModuleGenParams\">ModuleGenParams</a> for <a class=\"struct\" href=\"fedimint_mint/struct.MintGenParams.html\" title=\"struct fedimint_mint::MintGenParams\">MintGenParams</a>"]],
-"fedimint_wallet":[["impl <a class=\"trait\" href=\"fedimint_core/config/trait.ModuleGenParams.html\" title=\"trait fedimint_core::config::ModuleGenParams\">ModuleGenParams</a> for <a class=\"struct\" href=\"fedimint_wallet/struct.WalletGenParams.html\" title=\"struct fedimint_wallet::WalletGenParams\">WalletGenParams</a>"]]
+"fedimint_dummy":[["impl ModuleGenParams for <a class=\"struct\" href=\"fedimint_dummy/struct.DummyConfigGenParams.html\" title=\"struct fedimint_dummy::DummyConfigGenParams\">DummyConfigGenParams</a>"]],
+"fedimint_mint":[["impl ModuleGenParams for <a class=\"struct\" href=\"fedimint_mint/struct.MintGenParams.html\" title=\"struct fedimint_mint::MintGenParams\">MintGenParams</a>"]],
+"fedimint_wallet":[["impl ModuleGenParams for <a class=\"struct\" href=\"fedimint_wallet/struct.WalletGenParams.html\" title=\"struct fedimint_wallet::WalletGenParams\">WalletGenParams</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/fedimint_core/core/trait.Output.js
+++ implementors/fedimint_core/core/trait.Output.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
-"fedimint_dummy":[["impl <a class=\"trait\" href=\"fedimint_core/core/trait.Output.html\" title=\"trait fedimint_core::core::Output\">Output</a> for <a class=\"struct\" href=\"fedimint_dummy/struct.DummyOutput.html\" title=\"struct fedimint_dummy::DummyOutput\">DummyOutput</a>"]],
-"fedimint_ln":[["impl <a class=\"trait\" href=\"fedimint_core/core/trait.Output.html\" title=\"trait fedimint_core::core::Output\">Output</a> for <a class=\"enum\" href=\"fedimint_ln/enum.LightningOutput.html\" title=\"enum fedimint_ln::LightningOutput\">LightningOutput</a>"]],
-"fedimint_mint":[["impl <a class=\"trait\" href=\"fedimint_core/core/trait.Output.html\" title=\"trait fedimint_core::core::Output\">Output</a> for <a class=\"struct\" href=\"fedimint_mint/struct.MintOutput.html\" title=\"struct fedimint_mint::MintOutput\">MintOutput</a>"]],
-"fedimint_wallet":[["impl <a class=\"trait\" href=\"fedimint_core/core/trait.Output.html\" title=\"trait fedimint_core::core::Output\">Output</a> for <a class=\"struct\" href=\"fedimint_wallet/struct.WalletOutput.html\" title=\"struct fedimint_wallet::WalletOutput\">WalletOutput</a>"]]
+"fedimint_dummy":[["impl Output for <a class=\"struct\" href=\"fedimint_dummy/struct.DummyOutput.html\" title=\"struct fedimint_dummy::DummyOutput\">DummyOutput</a>"]],
+"fedimint_ln":[["impl Output for <a class=\"enum\" href=\"fedimint_ln/enum.LightningOutput.html\" title=\"enum fedimint_ln::LightningOutput\">LightningOutput</a>"]],
+"fedimint_mint":[["impl Output for <a class=\"struct\" href=\"fedimint_mint/struct.MintOutput.html\" title=\"struct fedimint_mint::MintOutput\">MintOutput</a>"]],
+"fedimint_wallet":[["impl Output for <a class=\"struct\" href=\"fedimint_wallet/struct.WalletOutput.html\" title=\"struct fedimint_wallet::WalletOutput\">WalletOutput</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
