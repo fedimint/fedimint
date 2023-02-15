870e931c1c85d68942fb47750e9a573d2024bc12
justinmoon <justinmoon@users.noreply.github.com>
2023-02-15 02:03:35 +0000

    deploy: 39ba8b52f4827a52d12aeb1a28f08f60f19eecb8

--- implementors/clap/derive/trait.CommandFactory.js
+++ implementors/clap/derive/trait.CommandFactory.js
@@ -1,7 +1,7 @@
 (function() {var implementors = {
 "dbtool":[["impl CommandFactory for <a class=\"struct\" href=\"dbtool/struct.Options.html\" title=\"struct dbtool::Options\">Options</a>"]],
 "distributedgen":[["impl CommandFactory for <a class=\"struct\" href=\"distributedgen/struct.Cli.html\" title=\"struct distributedgen::Cli\">Cli</a>"]],
-"fedimint_cli":[["impl CommandFactory for <a class=\"struct\" href=\"fedimint_cli/struct.Cli.html\" title=\"struct fedimint_cli::Cli\">Cli</a>"],["impl CommandFactory for <a class=\"struct\" href=\"fedimint_cli/struct.CliNoWorkdir.html\" title=\"struct fedimint_cli::CliNoWorkdir\">CliNoWorkdir</a>"]],
+"fedimint_cli":[["impl CommandFactory for <a class=\"struct\" href=\"fedimint_cli/struct.CliNoWorkdir.html\" title=\"struct fedimint_cli::CliNoWorkdir\">CliNoWorkdir</a>"],["impl CommandFactory for <a class=\"struct\" href=\"fedimint_cli/struct.Cli.html\" title=\"struct fedimint_cli::Cli\">Cli</a>"]],
 "gateway_cli":[["impl CommandFactory for <a class=\"struct\" href=\"gateway_cli/struct.Cli.html\" title=\"struct gateway_cli::Cli\">Cli</a>"]],
 "gateway_cln_extension":[["impl CommandFactory for <a class=\"struct\" href=\"gateway_cln_extension/struct.ClnExtensionOpts.html\" title=\"struct gateway_cln_extension::ClnExtensionOpts\">ClnExtensionOpts</a>"]],
 "gatewayd":[["impl CommandFactory for <a class=\"struct\" href=\"gatewayd/struct.GatewayOpts.html\" title=\"struct gatewayd::GatewayOpts\">GatewayOpts</a>"]],
