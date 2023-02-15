aa92a6f8b9cadb87e7c668242c9e50a8cb4c9189
dpc <dpc@users.noreply.github.com>
2023-02-15 02:51:02 +0000

    deploy: 88cf6fa115bf4658839faa23f90dc20b444717ff

--- implementors/clap/derive/trait.Parser.js
+++ implementors/clap/derive/trait.Parser.js
@@ -1,7 +1,7 @@
 (function() {var implementors = {
 "dbtool":[["impl Parser for <a class=\"struct\" href=\"dbtool/struct.Options.html\" title=\"struct dbtool::Options\">Options</a>"]],
 "distributedgen":[["impl Parser for <a class=\"struct\" href=\"distributedgen/struct.Cli.html\" title=\"struct distributedgen::Cli\">Cli</a>"]],
-"fedimint_cli":[["impl Parser for <a class=\"struct\" href=\"fedimint_cli/struct.Cli.html\" title=\"struct fedimint_cli::Cli\">Cli</a>"],["impl Parser for <a class=\"struct\" href=\"fedimint_cli/struct.CliNoWorkdir.html\" title=\"struct fedimint_cli::CliNoWorkdir\">CliNoWorkdir</a>"]],
+"fedimint_cli":[["impl Parser for <a class=\"struct\" href=\"fedimint_cli/struct.CliNoWorkdir.html\" title=\"struct fedimint_cli::CliNoWorkdir\">CliNoWorkdir</a>"],["impl Parser for <a class=\"struct\" href=\"fedimint_cli/struct.Cli.html\" title=\"struct fedimint_cli::Cli\">Cli</a>"]],
 "gateway_cli":[["impl Parser for <a class=\"struct\" href=\"gateway_cli/struct.Cli.html\" title=\"struct gateway_cli::Cli\">Cli</a>"]],
 "gateway_cln_extension":[["impl Parser for <a class=\"struct\" href=\"gateway_cln_extension/struct.ClnExtensionOpts.html\" title=\"struct gateway_cln_extension::ClnExtensionOpts\">ClnExtensionOpts</a>"]],
 "gatewayd":[["impl Parser for <a class=\"struct\" href=\"gatewayd/struct.GatewayOpts.html\" title=\"struct gatewayd::GatewayOpts\">GatewayOpts</a>"]],
