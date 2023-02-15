a30f37de310c329a1aca6b3a5f71b385d1445dd3
dpc <dpc@users.noreply.github.com>
2023-02-15 02:01:49 +0000

    deploy: 38d88495ceb3ee6d300efff2b56436ee47e7bdb2

--- implementors/bitcoin_hashes/trait.Hash.js
+++ implementors/bitcoin_hashes/trait.Hash.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
 "fedimint_core":[["impl <a class=\"trait\" href=\"fedimint_core/trait.BitcoinHash.html\" title=\"trait fedimint_core::BitcoinHash\">Hash</a> for <a class=\"struct\" href=\"fedimint_core/struct.TransactionId.html\" title=\"struct fedimint_core::TransactionId\">TransactionId</a>"]],
-"fedimint_ln":[["impl Hash for <a class=\"struct\" href=\"fedimint_ln/contracts/incoming/struct.OfferId.html\" title=\"struct fedimint_ln::contracts::incoming::OfferId\">OfferId</a>"],["impl Hash for <a class=\"struct\" href=\"fedimint_ln/contracts/struct.ContractId.html\" title=\"struct fedimint_ln::contracts::ContractId\">ContractId</a>"]],
+"fedimint_ln":[["impl Hash for <a class=\"struct\" href=\"fedimint_ln/contracts/struct.ContractId.html\" title=\"struct fedimint_ln::contracts::ContractId\">ContractId</a>"],["impl Hash for <a class=\"struct\" href=\"fedimint_ln/contracts/incoming/struct.OfferId.html\" title=\"struct fedimint_ln::contracts::incoming::OfferId\">OfferId</a>"]],
 "fedimint_server":[],
 "hkdf":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/fedimint_core/core/trait.Decoder.js
+++ implementors/fedimint_core/core/trait.Decoder.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
-"fedimint_dummy":[["impl <a class=\"trait\" href=\"fedimint_core/core/trait.Decoder.html\" title=\"trait fedimint_core::core::Decoder\">Decoder</a> for <a class=\"struct\" href=\"fedimint_dummy/common/struct.DummyDecoder.html\" title=\"struct fedimint_dummy::common::DummyDecoder\">DummyDecoder</a>"]],
-"fedimint_ln":[["impl <a class=\"trait\" href=\"fedimint_core/core/trait.Decoder.html\" title=\"trait fedimint_core::core::Decoder\">Decoder</a> for <a class=\"struct\" href=\"fedimint_ln/common/struct.LightningDecoder.html\" title=\"struct fedimint_ln::common::LightningDecoder\">LightningDecoder</a>"]],
-"fedimint_mint":[["impl <a class=\"trait\" href=\"fedimint_core/core/trait.Decoder.html\" title=\"trait fedimint_core::core::Decoder\">Decoder</a> for <a class=\"struct\" href=\"fedimint_mint/common/struct.MintDecoder.html\" title=\"struct fedimint_mint::common::MintDecoder\">MintDecoder</a>"]],
-"fedimint_wallet":[["impl <a class=\"trait\" href=\"fedimint_core/core/trait.Decoder.html\" title=\"trait fedimint_core::core::Decoder\">Decoder</a> for <a class=\"struct\" href=\"fedimint_wallet/common/struct.WalletDecoder.html\" title=\"struct fedimint_wallet::common::WalletDecoder\">WalletDecoder</a>"]]
+"fedimint_dummy":[["impl Decoder for <a class=\"struct\" href=\"fedimint_dummy/common/struct.DummyDecoder.html\" title=\"struct fedimint_dummy::common::DummyDecoder\">DummyDecoder</a>"]],
+"fedimint_ln":[["impl Decoder for <a class=\"struct\" href=\"fedimint_ln/common/struct.LightningDecoder.html\" title=\"struct fedimint_ln::common::LightningDecoder\">LightningDecoder</a>"]],
+"fedimint_mint":[["impl Decoder for <a class=\"struct\" href=\"fedimint_mint/common/struct.MintDecoder.html\" title=\"struct fedimint_mint::common::MintDecoder\">MintDecoder</a>"]],
+"fedimint_wallet":[["impl Decoder for <a class=\"struct\" href=\"fedimint_wallet/common/struct.WalletDecoder.html\" title=\"struct fedimint_wallet::common::WalletDecoder\">WalletDecoder</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
