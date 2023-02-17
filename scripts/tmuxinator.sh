69c69ef453f2707a484e13c747a993084ffd6999
Justin Moon <mail@justinmoon.com>
2023-02-17 15:39:33 -0600

    fix: delete .tmpenv file from tmuxinator setup
    Tmuxinator works just fine without this file. Seems that
    environment is passed to the tmuxinator environment by default.

--- scripts/tmuxinator.sh
+++ scripts/tmuxinator.sh
@@ -18,13 +18,9 @@ export FM_VERBOSE_OUTPUT=0
 source scripts/build.sh
 echo "Running in temporary directory $FM_TEST_DIR"
 
-env | sed -En 's/(FM_[^=]*).*/\1/gp' | while read var; do printf 'export %s=%q\n' "$var" "${!var}"; done > .tmpenv
-
 export FM_BITCOIND_RPC="http://bitcoin:bitcoin@127.0.0.1:18443"
 
 SHELL=$(which bash) tmuxinator local
 tmux -L fedimint-dev kill-session -t fedimint-dev || true
 pkill bitcoind
 pkill lightningd
-
-rm .tmpenv
