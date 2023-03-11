d1f95fc6acaafc8eb4926b492f62f8767a757f2e
Justin Moon <mail@justinmoon.com>
2023-03-11 13:53:29 -0600

    refactor: start_federation bash function

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -243,3 +243,19 @@ function start_esplora() {
   echo $! >> $FM_PID_FILE
   echo "started esplora"
 }
+
+function start_federation() {
+  echo "starting federation"
+  await_bitcoin_rpc
+
+  START_SERVER=${1:-0}
+  END_SERVER=${2:-$FM_FED_SIZE}
+
+  # Start the federation members inside the temporary directory
+  for ((ID=START_SERVER; ID<END_SERVER; ID++)); do
+    echo "starting mint $ID"
+    export FM_PASSWORD="pass$ID"
+    ( ($FM_BIN_DIR/fedimintd $FM_CFG_DIR/server-$ID 2>&1 & echo $! >&3 ) 3>>$FM_PID_FILE | sed -e "s/^/mint $ID: /" ) &
+  done
+  echo "started federation"
+}
