223e9fd655ac507fd36d61282a569eeaf16daf79
okjodom <okjodom@gmail.com>
2023-03-13 23:07:32 +0300

    fix: configure proto3 optionals in grpc proto compiler

--- gateway/ln-gateway/build.rs
+++ gateway/ln-gateway/build.rs
@@ -8,6 +8,7 @@ fn main() {
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile(&[proto_path], &[include_path])
         .unwrap_or_else(|e| panic!("failed to compile gateway proto files: {e}"));
     fedimint_build::set_code_version();
