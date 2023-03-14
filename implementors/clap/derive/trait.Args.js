10a7d517aa093ca8cc1aab16b8c0a5dc67159d42
dpc <dpc@users.noreply.github.com>
2023-03-14 18:05:40 +0000

    deploy: f56e63e70e43265f8e1d94d65aa0131904989a7a

--- implementors/clap/derive/trait.Args.js
+++ implementors/clap/derive/trait.Args.js
@@ -2,6 +2,7 @@
 "dbtool":[["impl Args for <a class=\"struct\" href=\"dbtool/struct.Options.html\" title=\"struct dbtool::Options\">Options</a>"]],
 "fedimint_cli":[["impl Args for <a class=\"struct\" href=\"fedimint_cli/struct.Cli.html\" title=\"struct fedimint_cli::Cli\">Cli</a>"],["impl Args for <a class=\"struct\" href=\"fedimint_cli/struct.CliNoWorkdir.html\" title=\"struct fedimint_cli::CliNoWorkdir\">CliNoWorkdir</a>"]],
 "fedimintd":[["impl Args for <a class=\"struct\" href=\"fedimintd/distributed_gen/struct.Cli.html\" title=\"struct fedimintd::distributed_gen::Cli\">Cli</a>"],["impl Args for <a class=\"struct\" href=\"fedimintd/fedimintd/struct.ServerOpts.html\" title=\"struct fedimintd::fedimintd::ServerOpts\">ServerOpts</a>"]],
+"fixtures":[["impl Args for <a class=\"struct\" href=\"fixtures/struct.Args.html\" title=\"struct fixtures::Args\">Args</a>"]],
 "gateway_cli":[["impl Args for <a class=\"struct\" href=\"gateway_cli/struct.Cli.html\" title=\"struct gateway_cli::Cli\">Cli</a>"]],
 "gateway_cln_extension":[["impl Args for <a class=\"struct\" href=\"gateway_cln_extension/struct.ClnExtensionOpts.html\" title=\"struct gateway_cln_extension::ClnExtensionOpts\">ClnExtensionOpts</a>"]],
 "gatewayd":[["impl Args for <a class=\"struct\" href=\"gatewayd/struct.GatewayOpts.html\" title=\"struct gatewayd::GatewayOpts\">GatewayOpts</a>"]],
--- implementors/clap/derive/trait.CommandFactory.js
+++ implementors/clap/derive/trait.CommandFactory.js
@@ -2,6 +2,7 @@
 "dbtool":[["impl CommandFactory for <a class=\"struct\" href=\"dbtool/struct.Options.html\" title=\"struct dbtool::Options\">Options</a>"]],
 "fedimint_cli":[["impl CommandFactory for <a class=\"struct\" href=\"fedimint_cli/struct.Cli.html\" title=\"struct fedimint_cli::Cli\">Cli</a>"],["impl CommandFactory for <a class=\"struct\" href=\"fedimint_cli/struct.CliNoWorkdir.html\" title=\"struct fedimint_cli::CliNoWorkdir\">CliNoWorkdir</a>"]],
 "fedimintd":[["impl CommandFactory for <a class=\"struct\" href=\"fedimintd/distributed_gen/struct.Cli.html\" title=\"struct fedimintd::distributed_gen::Cli\">Cli</a>"],["impl CommandFactory for <a class=\"struct\" href=\"fedimintd/fedimintd/struct.ServerOpts.html\" title=\"struct fedimintd::fedimintd::ServerOpts\">ServerOpts</a>"]],
+"fixtures":[["impl CommandFactory for <a class=\"struct\" href=\"fixtures/struct.Args.html\" title=\"struct fixtures::Args\">Args</a>"]],
 "gateway_cli":[["impl CommandFactory for <a class=\"struct\" href=\"gateway_cli/struct.Cli.html\" title=\"struct gateway_cli::Cli\">Cli</a>"]],
 "gateway_cln_extension":[["impl CommandFactory for <a class=\"struct\" href=\"gateway_cln_extension/struct.ClnExtensionOpts.html\" title=\"struct gateway_cln_extension::ClnExtensionOpts\">ClnExtensionOpts</a>"]],
 "gatewayd":[["impl CommandFactory for <a class=\"struct\" href=\"gatewayd/struct.GatewayOpts.html\" title=\"struct gatewayd::GatewayOpts\">GatewayOpts</a>"]],
