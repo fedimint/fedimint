267471887badf49f82f7f18077d213716237e272
dpc <dpc@users.noreply.github.com>
2023-03-16 15:19:08 +0000

    deploy: 7727eff48b44a4a092dc0a70100aeb2cf19fbea1

--- implementors/fedimint_core/api/trait.DynTryIntoOutcome.js
+++ implementors/fedimint_core/api/trait.DynTryIntoOutcome.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"mint_client":[["impl DynTryIntoOutcome for <a class=\"enum\" href=\"mint_client/outcome/legacy/enum.OutputOutcome.html\" title=\"enum mint_client::outcome::legacy::OutputOutcome\">OutputOutcome</a>"]]
+"mint_client":[["impl <a class=\"trait\" href=\"fedimint_core/api/trait.DynTryIntoOutcome.html\" title=\"trait fedimint_core::api::DynTryIntoOutcome\">DynTryIntoOutcome</a> for <a class=\"enum\" href=\"mint_client/outcome/legacy/enum.OutputOutcome.html\" title=\"enum mint_client::outcome::legacy::OutputOutcome\">OutputOutcome</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
