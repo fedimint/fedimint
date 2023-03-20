8151f3ae034cf287047595cfc81f9598da4e58ec
Josh Kitman <josh.kitman@protonmail.com>
2023-03-20 02:57:41 +0000

    Make attach_endpoints generic

--- fedimint-server/src/consensus/interconnect.rs
+++ fedimint-server/src/consensus/interconnect.rs
@@ -3,8 +3,9 @@ use fedimint_core::core::ModuleInstanceId;
 use fedimint_core::module::interconnect::ModuleInterconect;
 use fedimint_core::module::{ApiError, ApiRequestErased};
 use serde_json::Value;
-use crate::net::api::HasApiContext;
+
 use crate::consensus::FedimintConsensus;
+use crate::net::api::HasApiContext;
 
 pub struct FedimintInterconnect<'a> {
     pub fedimint: &'a FedimintConsensus,
@@ -25,13 +26,9 @@ impl<'a> ModuleInterconect for FedimintInterconnect<'a> {
                     .into_iter()
                     .find(|endpoint| endpoint.path == path)
                     .ok_or_else(|| ApiError::not_found(String::from("Method not found")))?;
+                let (state, context) = self.fedimint.context(&data, Some(id)).await;
 
-                return (endpoint.handler)(
-                    module,
-                    self.fedimint.context(&data, Some(id)).await,
-                    data,
-                )
-                .await;
+                return (endpoint.handler)(state, context, data).await;
             }
         }
         panic!("Module not registered: {id}");
--- fedimint-server/src/consensus/mod.rs
+++ fedimint-server/src/consensus/mod.rs
@@ -20,9 +20,7 @@ use fedimint_core::module::audit::Audit;
 use fedimint_core::module::registry::{
     ModuleDecoderRegistry, ModuleRegistry, ServerModuleRegistry,
 };
-use fedimint_core::module::{
-    ModuleError, TransactionItemAmount,
-};
+use fedimint_core::module::{ModuleError, TransactionItemAmount};
 use fedimint_core::outcome::TransactionStatus;
 use fedimint_core::server::{DynServerModule, DynVerificationCache};
 use fedimint_core::task::TaskGroup;
