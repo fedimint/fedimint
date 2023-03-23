ef0a3b959e95f1ee05b1dcba40b31503a916e444
dpc <dpc@users.noreply.github.com>
2023-03-23 19:29:32 +0000

    deploy: 3ad222fad42cdb8399efa1499a5d23b7b9902042

--- implementors/clap/derive/trait.Parser.js
+++ implementors/clap/derive/trait.Parser.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
 "dbtool":[["impl Parser for <a class=\"struct\" href=\"dbtool/struct.Options.html\" title=\"struct dbtool::Options\">Options</a>"]],
-"fedimint_cli":[["impl Parser for <a class=\"struct\" href=\"fedimint_cli/struct.Cli.html\" title=\"struct fedimint_cli::Cli\">Cli</a>"],["impl Parser for <a class=\"struct\" href=\"fedimint_cli/struct.CliNoWorkdir.html\" title=\"struct fedimint_cli::CliNoWorkdir\">CliNoWorkdir</a>"]],
+"fedimint_cli":[["impl Parser for <a class=\"struct\" href=\"fedimint_cli/struct.Opts.html\" title=\"struct fedimint_cli::Opts\">Opts</a>"]],
 "fedimintd":[["impl Parser for <a class=\"struct\" href=\"fedimintd/distributed_gen/struct.Cli.html\" title=\"struct fedimintd::distributed_gen::Cli\">Cli</a>"],["impl Parser for <a class=\"struct\" href=\"fedimintd/fedimintd/struct.ServerOpts.html\" title=\"struct fedimintd::fedimintd::ServerOpts\">ServerOpts</a>"]],
 "fixtures":[["impl Parser for <a class=\"struct\" href=\"fixtures/struct.Args.html\" title=\"struct fixtures::Args\">Args</a>"]],
 "gateway_cli":[["impl Parser for <a class=\"struct\" href=\"gateway_cli/struct.Cli.html\" title=\"struct gateway_cli::Cli\">Cli</a>"]],
--- implementors/clap/derive/trait.Subcommand.js
+++ implementors/clap/derive/trait.Subcommand.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
 "dbtool":[["impl Subcommand for <a class=\"enum\" href=\"dbtool/enum.DbCommand.html\" title=\"enum dbtool::DbCommand\">DbCommand</a>"]],
-"fedimint_cli":[["impl Subcommand for <a class=\"enum\" href=\"fedimint_cli/enum.CommandNoWorkdir.html\" title=\"enum fedimint_cli::CommandNoWorkdir\">CommandNoWorkdir</a>"],["impl Subcommand for <a class=\"enum\" href=\"fedimint_cli/enum.Command.html\" title=\"enum fedimint_cli::Command\">Command</a>"]],
+"fedimint_cli":[["impl Subcommand for <a class=\"enum\" href=\"fedimint_cli/enum.Command.html\" title=\"enum fedimint_cli::Command\">Command</a>"]],
 "fedimintd":[["impl Subcommand for <a class=\"enum\" href=\"fedimintd/distributed_gen/enum.Command.html\" title=\"enum fedimintd::distributed_gen::Command\">Command</a>"]],
 "fixtures":[["impl Subcommand for <a class=\"enum\" href=\"fixtures/enum.Cmd.html\" title=\"enum fixtures::Cmd\">Cmd</a>"]],
 "gateway_cli":[["impl Subcommand for <a class=\"enum\" href=\"gateway_cli/enum.Commands.html\" title=\"enum gateway_cli::Commands\">Commands</a>"]],
