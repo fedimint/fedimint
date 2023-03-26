6dc74815fc3b3cf026215fe4eec4c11185bfdaa4
elsirion <elsirion@protonmail.com>
2023-03-26 21:04:57 +0200

    feat: implement e-cash spend state machine

--- fedimint-client/src/lib.rs
+++ fedimint-client/src/lib.rs
@@ -21,7 +21,8 @@ use secp256k1_zkp::Secp256k1;
 use crate::module::gen::{ClientModuleGen, ClientModuleGenRegistry};
 use crate::module::{ClientModuleRegistry, DynPrimaryClientModule, IClientModule};
 use crate::sm::{
-    ActiveState, DynState, Executor, GlobalContext, InactiveState, OperationId, OperationState,
+    ActiveState, ClientSMDatabaseTransaction, DynState, Executor, GlobalContext, InactiveState,
+    OperationId, OperationState,
 };
 use crate::transaction::{
     tx_submission_sm_decoder, ClientInput, ClientOutput, TransactionBuilder,
@@ -49,7 +50,7 @@ pub trait IGlobalClientContext: Debug + MaybeSend + MaybeSync + 'static {
     /// committed.
     async fn finalize_and_submit_transaction(
         &self,
-        dbtx: &mut DatabaseTransaction<'_>,
+        dbtx: &mut ClientSMDatabaseTransaction<'_, '_>,
         operation_id: OperationId,
         tx_builder: TransactionBuilder,
     ) -> anyhow::Result<TransactionId>;
@@ -126,11 +127,17 @@ impl IGlobalClientContext for ClientInner {
 
     async fn finalize_and_submit_transaction(
         &self,
-        dbtx: &mut DatabaseTransaction<'_>,
+        dbtx: &mut ClientSMDatabaseTransaction<'_, '_>,
         operation_id: OperationId,
         tx_builder: TransactionBuilder,
     ) -> anyhow::Result<TransactionId> {
-        ClientInner::finalize_and_submit_transaction(self, dbtx, operation_id, tx_builder).await
+        ClientInner::finalize_and_submit_transaction(
+            self,
+            dbtx.global_tx(),
+            operation_id,
+            tx_builder,
+        )
+        .await
     }
 
     async fn await_active_state(&self, state: DynState<DynGlobalClientContext>) -> ActiveState {
