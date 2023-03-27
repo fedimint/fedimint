278dc1c481d1efc5f68186b44e0f29cda0e53a3d
Maan2003 <manmeetmann2003@gmail.com>
2023-03-28 02:06:19 +0530

    fix: kill_fedimint_processes fails if no PID is added $FM_PID_FILE

--- scripts/build.sh
+++ scripts/build.sh
@@ -43,6 +43,7 @@ mkdir -p $FM_LND_DIR
 mkdir -p $FM_BTC_DIR
 mkdir -p $FM_CFG_DIR
 mkdir -p $FM_ELECTRS_DIR
+touch $FM_PID_FILE
 
 # Copy configs to data directories
 cp misc/test/bitcoin.conf $FM_BTC_DIR
