794b0923e98aeb40918e993c58bc8a5c6a887fa8
okjodom <okjodom@gmail.com>
2023-04-03 15:24:09 +0300

    feat: add mintgate subtree into gateway/ui

---
+++ gateway/ui/src/components/TabHeader.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button, useTab, useMultiStyleConfig } from '@chakra-ui/react';
+
+interface TabHeaderProps {
+	children?: React.ReactNode;
+}
+
+export const TabHeader = React.forwardRef(function TabHeader(
+	props: TabHeaderProps,
+	ref?: React.Ref<HTMLElement> | undefined
+): JSX.Element {
+	const tabProps = useTab({ ...props, ref });
+	const styles = useMultiStyleConfig('Tabs', tabProps);
+
+	return (
+		<Button __css={styles.tab} {...tabProps}>
+			{tabProps.children}
+		</Button>
+	);
+});
