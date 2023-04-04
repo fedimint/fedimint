00e4b8fbd30bff76a21e13928941aacf6c774e09
mxz42 <mxz42@tuta.io>
2023-04-04 16:26:20 -0600

    chore: put aliases in its own file to be easily sourced
    When opening a new tmux pane in tmuxinator, you can easily source the
    aliases file with `. scripts/aliases.sh` or `source scripts/aliases.sh`

---
+++ scripts/aliases.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+alias lightning-cli="\$FM_LIGHTNING_CLI"
+alias lncli="\$FM_LNCLI"
+alias bitcoin-cli="\$FM_BTC_CLIENT"
+alias fedimint-cli="\$FM_MINT_CLIENT"
+alias gateway-cln="\$FM_GWCLI_CLN"
+alias gateway-lnd="\$FM_GWCLI_LND"
+alias mint_rpc_client="\$FM_MINT_RPC_CLIENT"
+alias dbtool="\$FM_DB_TOOL"
+# alias restart="./scripts/restart-tmux.sh"
