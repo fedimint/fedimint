1108040c08ae17c931c4e075d78af6897187f77e
m1sterc001guy <m1sterc001guy@users.noreply.github.com>
2023-04-10 14:47:34 +0000

    deploy: 51d736d99885b3b7b9e43657878303c9d8472d99

--- implementors/clap/derive/trait.Subcommand.js
+++ implementors/clap/derive/trait.Subcommand.js
@@ -5,6 +5,6 @@
 "fedimintd":[["impl Subcommand for <a class=\"enum\" href=\"fedimintd/distributed_gen/enum.Command.html\" title=\"enum fedimintd::distributed_gen::Command\">Command</a>"]],
 "fixtures":[["impl Subcommand for <a class=\"enum\" href=\"fixtures/enum.Cmd.html\" title=\"enum fixtures::Cmd\">Cmd</a>"]],
 "gateway_cli":[["impl Subcommand for <a class=\"enum\" href=\"gateway_cli/enum.Commands.html\" title=\"enum gateway_cli::Commands\">Commands</a>"]],
-"ln_gateway":[["impl Subcommand for <a class=\"enum\" href=\"ln_gateway/enum.Mode.html\" title=\"enum ln_gateway::Mode\">Mode</a>"]],
+"ln_gateway":[["impl Subcommand for <a class=\"enum\" href=\"ln_gateway/enum.LightningMode.html\" title=\"enum ln_gateway::LightningMode\">LightningMode</a>"]],
 "recoverytool":[["impl Subcommand for <a class=\"enum\" href=\"recoverytool/enum.TweakSource.html\" title=\"enum recoverytool::TweakSource\">TweakSource</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
