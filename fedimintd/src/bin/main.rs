868ab504e03a84c5b4f591e5ec9ce55039307de2
elsirion <elsirion@protonmail.com>
2023-04-14 11:56:54 +0200

    wip: test ci

--- fedimintd/src/bin/main.rs
+++ fedimintd/src/bin/main.rs
@@ -2,5 +2,6 @@ use fedimintd::fedimintd::Fedimintd;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    println!("Hello, world!");
     Fedimintd::new()?.with_default_modules().run().await
 }
