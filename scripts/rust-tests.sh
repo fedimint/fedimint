0ae6c31d2d2b1d65de15582901557d5812665482
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-04-14 16:19:26 -0700

    chore: report more timings in major tests

--- scripts/rust-tests.sh
+++ scripts/rust-tests.sh
@@ -2,7 +2,7 @@
 # Runs the all the Rust integration tests
 
 set -euo pipefail
-export RUST_LOG="${RUST_LOG:-info}"
+export RUST_LOG="${RUST_LOG:-info,timing=debug}"
 
 # Convert RUST_LOG to lowercase
 # if RUST_LOG is none, don't show output of test setup
