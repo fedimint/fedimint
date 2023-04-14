1cb955c3dba7fbecb86a77f0fe958666429c1dc7
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-04-14 14:31:31 -0700

    chore: display start and end of each part of rust-tests.sh

--- scripts/rust-tests.sh
+++ scripts/rust-tests.sh
@@ -4,6 +4,8 @@
 set -euo pipefail
 export RUST_LOG="${RUST_LOG:-info}"
 
+
+>&2 echo "### Setting up tests"
 # Convert RUST_LOG to lowercase
 # if RUST_LOG is none, don't show output of test setup
 if [ "${RUST_LOG,,}" = "none" ]; then
@@ -12,18 +14,26 @@ else
   set -x
   source ./scripts/setup-tests.sh ""
 fi
+>&2 echo "### Setting up tests - complete"
 
 export FM_TEST_USE_REAL_DAEMONS=1
+
+>&2 echo "### Testing against bitcoind"
 env RUST_BACKTRACE=1 cargo test -p fedimint-tests -- --test-threads=$(($(nproc) * 2)) "$@"
+>&2 echo "### Testing against bitcoind - complete"
 
 # Switch to electrum and run wallet tests
 unset FM_BITCOIND_RPC
 export FM_ELECTRUM_RPC="tcp://127.0.0.1:50001"
+>&2 echo "### Testing against electrs"
 env RUST_BACKTRACE=1 cargo test -p fedimint-tests wallet -- --test-threads=$(($(nproc) * 2)) "$@"
+>&2 echo "### Testing against electrs - complete"
 
 # Switch to esplora and run wallet tests
 unset FM_ELECTRUM_RPC
 export FM_ESPLORA_RPC="http://127.0.0.1:50002"
+>&2 echo "### Testing against esplora"
 env RUST_BACKTRACE=1 cargo test -p fedimint-tests wallet -- --test-threads=$(($(nproc) * 2)) "$@"
+>&2 echo "### Testing against esplora - complete"
 
 echo "fm success: rust-tests"
