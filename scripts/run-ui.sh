1311d98d92cb03b6be158ea1eece9ecfaabe2642
Will O'Beirne <wbobeirne@gmail.com>
2023-05-13 12:02:47 -0500

    fix: attempt tail for all federations, not just 2

--- scripts/run-ui.sh
+++ scripts/run-ui.sh
@@ -4,12 +4,15 @@ export FM_BITCOIN_NETWORK="regtest"
 export FM_UI_KIND=${1:-"old"}
 export FM_FED_SIZE=${2:-2}
 export FM_FED_NAME=${3:-"Cypherpunk Federation"}
+export RUST_BACKTRACE="full"
 
 source scripts/build.sh $FM_FED_SIZE
 
-tail -n +0 -F $FM_LOGS_DIR/fedimintd-0.log &
-echo $! >> $FM_PID_FILE
-tail -n +0 -F $FM_LOGS_DIR/fedimintd-1.log &
-echo $! >> $FM_PID_FILE
+for ((i=0; i < FM_FED_SIZE; i++));
+do
+  echo $i
+  tail -n +0 -F "$FM_LOGS_DIR/fedimintd-$i.log" &
+  echo $! >> $FM_PID_FILE
+done
 
 devimint run-ui $FM_UI_KIND
