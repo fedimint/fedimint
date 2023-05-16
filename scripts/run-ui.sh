59150815473eb090699ff85051e6722cb35a6589
Will O'Beirne <wbobeirne@gmail.com>
2023-05-16 10:13:52 -0500

    fix: remove accidental RUST_BACKTRACE
    Co-authored-by: Ethan Tuttle <133234413+EthnTuttle@users.noreply.github.com>

--- scripts/run-ui.sh
+++ scripts/run-ui.sh
@@ -4,7 +4,6 @@ export FM_BITCOIN_NETWORK="regtest"
 export FM_UI_KIND=${1:-"old"}
 export FM_FED_SIZE=${2:-2}
 export FM_FED_NAME=${3:-"Cypherpunk Federation"}
-export RUST_BACKTRACE="full"
 
 source scripts/build.sh $FM_FED_SIZE
 
