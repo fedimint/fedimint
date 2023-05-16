986b3af1214e5c79d5eb25b3265d1ca45ef60829
Ethan Tuttle <tuttle.ethan@protonmail.com>
2023-05-15 21:10:44 -0400

    fix(ui): check for nix shell before running commands

--- scripts/run-ui.sh
+++ scripts/run-ui.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+if [[ -z "${IN_NIX_SHELL:-}" ]]; then
+  echo "It is recommended to run this command from a Nix dev shell. Use 'nix develop .#fedimint-ui' first"
+  sleep 3
+fi
+
 export FM_BITCOIN_NETWORK="regtest"
 export FM_UI_KIND=${1:-"old"}
 export FM_FED_SIZE=${2:-2}
