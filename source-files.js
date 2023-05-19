775ed3b389d575904f71002c959587c3aee86f0b
jkitman <jkitman@users.noreply.github.com>
2023-05-19 22:06:26 +0000

    deploy: f16a14b78a73c7c8eee76df26f1654e233d6a6cc

--- source-files.js
+++ source-files.js
@@ -31,7 +31,7 @@ var sourcesIndex = JSON.parse('{\
 "fedimint_wallet_common":["",[],["config.rs","db.rs","keys.rs","lib.rs","tweakable.rs","txoproof.rs"]],\
 "fedimint_wallet_server":["",[],["lib.rs"]],\
 "fedimint_wallet_tests":["",[],["lib.rs"]],\
-"fedimintd":["",[],["distributed_gen.rs","fedimintd.rs","lib.rs","ui.rs"]],\
+"fedimintd":["",[],["distributed_gen.rs","fedimintd.rs","lib.rs"]],\
 "gateway_cli":["",[],["main.rs"]],\
 "gateway_cln_extension":["",[],["cln_extension.rs"]],\
 "gatewayd":["",[],["gatewayd.rs"]],\
