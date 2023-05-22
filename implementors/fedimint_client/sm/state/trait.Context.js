b2f74f5bb524f7a061b445bc6aa1fc8448171d43
justinmoon <justinmoon@users.noreply.github.com>
2023-05-22 17:34:38 +0000

    deploy: 5018a48420ccf59e750acc3a47615129a4a0e314

--- implementors/fedimint_client/sm/state/trait.Context.js
+++ implementors/fedimint_client/sm/state/trait.Context.js
@@ -2,5 +2,6 @@
 "fedimint_client":[],
 "fedimint_dummy_client":[["impl Context for <a class=\"struct\" href=\"fedimint_dummy_client/struct.DummyClientContext.html\" title=\"struct fedimint_dummy_client::DummyClientContext\">DummyClientContext</a>"]],
 "fedimint_ln_client":[["impl Context for <a class=\"struct\" href=\"fedimint_ln_client/struct.LightningClientContext.html\" title=\"struct fedimint_ln_client::LightningClientContext\">LightningClientContext</a>"]],
-"fedimint_mint_client":[["impl Context for <a class=\"struct\" href=\"fedimint_mint_client/struct.MintClientContext.html\" title=\"struct fedimint_mint_client::MintClientContext\">MintClientContext</a>"]]
+"fedimint_mint_client":[["impl Context for <a class=\"struct\" href=\"fedimint_mint_client/struct.MintClientContext.html\" title=\"struct fedimint_mint_client::MintClientContext\">MintClientContext</a>"]],
+"fedimint_wallet_client":[["impl Context for <a class=\"struct\" href=\"fedimint_wallet_client/struct.WalletClientContext.html\" title=\"struct fedimint_wallet_client::WalletClientContext\">WalletClientContext</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
