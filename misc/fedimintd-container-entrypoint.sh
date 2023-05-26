8db9f2a9e943fd2a2cccbd6ed446089eab4dc751
Ethan Tuttle <tuttle.ethan@protonmail.com>
2023-05-25 22:07:36 -0400

    fix: remove password requirement for docker image

--- misc/fedimintd-container-entrypoint.sh
+++ misc/fedimintd-container-entrypoint.sh
@@ -10,11 +10,6 @@ if [ -z "$FM_DATA_DIR" ]; then
   exit 1
 fi
 
-if [ -z "$FM_PASSWORD" ]; then
-  1>&2 echo "Must provide FM_PASSWORD environment variable"
-  exit 1
-fi
-
 if [ ! -e "$FM_DATA_DIR" ]; then
   1>&2 echo "Creating $FM_DATA_DIR"
   mkdir -p "$FM_DATA_DIR" && chmod 0600 "$FM_DATA_DIR"
