49aea0f04ce8ae309bcf52bf3685e3f447dcffe3
Justin Moeller <jumoell@protonmail.com>
2023-05-31 11:46:36 -0500

    fix: test both gateways and rename GatewayFixtures

--- gateway/ln-gateway/src/ng/tests.rs
+++ gateway/ln-gateway/src/ng/tests.rs
@@ -1,4 +1,5 @@
 use std::str::FromStr;
+use std::sync::Arc;
 
 use assert_matches::assert_matches;
 use fedimint_client::module::gen::ClientModuleGenRegistry;
@@ -12,28 +13,31 @@ use fedimint_ln_client::{LightningClientExt, LightningClientGen, LnPayState};
 use fedimint_ln_common::config::LightningGenParams;
 use fedimint_ln_server::LightningGen;
 use fedimint_testing::federation::FederationTest;
-use fedimint_testing::fixtures::{Fixtures, GatewayFixtures};
-use fedimint_testing::ln::LightningNodeType;
+use fedimint_testing::fixtures::{Fixtures, LightningFixtures};
+use fedimint_testing::ln::{LightningNodeType, LightningTest};
+use futures::Future;
 use lightning::routing::gossip::RoutingFees;
+use ln_gateway::lnrpc_client::ILnRpcClient;
 use ln_gateway::ng::{GatewayClientExt, GatewayClientGen, GatewayExtPayStates};
+use tracing::debug;
 use url::Url;
 
-async fn fixtures(gateway_node: &LightningNodeType) -> (Fixtures, GatewayFixtures) {
+async fn fixtures(gateway_node: &LightningNodeType) -> (Fixtures, LightningFixtures) {
     let fixtures = Fixtures::new_primary(1, DummyClientGen, DummyGen, DummyGenParams::default());
     let ln_params = LightningGenParams::regtest(fixtures.bitcoin_rpc());
     let fixtures = fixtures.with_module(0, LightningClientGen, LightningGen, ln_params);
-    let gateway_fixtures = GatewayFixtures::new(gateway_node).await;
-    (fixtures, gateway_fixtures)
+    let lightning_fixtures = LightningFixtures::new(gateway_node).await;
+    (fixtures, lightning_fixtures)
 }
 
 async fn new_gateway_client(
     fed: &FederationTest,
-    gateway_fixtures: &GatewayFixtures,
+    gateway_lnrpc: Arc<dyn ILnRpcClient>,
 ) -> anyhow::Result<Client> {
     let mut registry = ClientModuleGenRegistry::new();
     registry.attach(DummyClientGen);
     registry.attach(GatewayClientGen {
-        lightning_client: gateway_fixtures.gateway_lightning_client.clone(),
+        lightning_client: gateway_lnrpc.clone(),
         fees: RoutingFees {
             base_msat: 0,
             proportional_millionths: 0,
@@ -47,81 +51,104 @@ async fn new_gateway_client(
     Ok(gateway)
 }
 
-#[tokio::test(flavor = "multi_thread")]
-async fn test_gateway_client_pay_valid_invoice() -> anyhow::Result<()> {
-    let gateway_node = LightningNodeType::Lnd;
+async fn gateway_test<B>(
+    f: impl FnOnce(
+            Fixtures,
+            Arc<dyn LightningTest>,
+            FederationTest,
+            Client, // User Client
+            Client, // Gateway Client
+        ) -> B
+        + Copy,
+) -> anyhow::Result<()>
+where
+    B: Future<Output = anyhow::Result<()>>,
+{
+    let gateway_nodes = [LightningNodeType::Cln, LightningNodeType::Lnd];
+    for gateway_node in gateway_nodes {
+        debug!("Running tests with {gateway_node:?}");
+        let (fixtures, lightning_fixtures) = fixtures(&gateway_node).await;
+        let fed = fixtures.new_fed().await;
+        let user_client = fed.new_client().await;
 
-    let (fixtures, gateway_fixtures) = fixtures(&gateway_node).await;
-    let fed = fixtures.new_fed().await;
-    let user_client = fed.new_client().await;
-
-    let gateway = new_gateway_client(&fed, &gateway_fixtures).await?;
-
-    // Print money for user_client
-    let (_, outpoint) = user_client.print_money(sats(1000)).await?;
-    user_client.receive_money(outpoint).await?;
-    assert_eq!(user_client.get_balance().await, sats(1000));
-
-    // Create test invoice
-    let invoice = gateway_fixtures
-        .other_lightning_client
-        .invoice(sats(250), None)
+        let gateway =
+            new_gateway_client(&fed, lightning_fixtures.gateway_lightning_client.clone()).await?;
+        f(
+            fixtures,
+            lightning_fixtures.other_lightning_client,
+            fed,
+            user_client,
+            gateway,
+        )
         .await?;
-
-    // User client pays test invoice
-    let (pay_op, contract_id) = user_client.pay_bolt11_invoice(invoice.clone()).await?;
-    let mut pay_sub = user_client.subscribe_ln_pay(pay_op).await?.into_stream();
-    assert_eq!(pay_sub.ok().await?, LnPayState::Created);
-    let funded = pay_sub.ok().await?;
-    assert_matches!(funded, LnPayState::Funded);
-
-    let gw_pay_op = gateway.gateway_pay_bolt11_invoice(contract_id).await?;
-    let mut gw_pay_sub = gateway
-        .gateway_subscribe_ln_pay(gw_pay_op)
-        .await?
-        .into_stream();
-    assert_eq!(gw_pay_sub.ok().await?, GatewayExtPayStates::Created);
-    assert_eq!(gw_pay_sub.ok().await?, GatewayExtPayStates::Preimage);
-    assert_eq!(gw_pay_sub.ok().await?, GatewayExtPayStates::Success);
-
+    }
     Ok(())
 }
 
 #[tokio::test(flavor = "multi_thread")]
+async fn test_gateway_client_pay_valid_invoice() -> anyhow::Result<()> {
+    gateway_test(
+        |_, other_lightning_client, _, user_client, gateway| async move {
+            // Print money for user_client
+            let (_, outpoint) = user_client.print_money(sats(1000)).await?;
+            user_client.receive_money(outpoint).await?;
+            assert_eq!(user_client.get_balance().await, sats(1000));
+
+            // Create test invoice
+            let invoice = other_lightning_client.invoice(sats(250), None).await?;
+
+            // User client pays test invoice
+            let (pay_op, contract_id) = user_client.pay_bolt11_invoice(invoice.clone()).await?;
+            let mut pay_sub = user_client.subscribe_ln_pay(pay_op).await?.into_stream();
+            assert_eq!(pay_sub.ok().await?, LnPayState::Created);
+            let funded = pay_sub.ok().await?;
+            assert_matches!(funded, LnPayState::Funded);
+
+            let gw_pay_op = gateway.gateway_pay_bolt11_invoice(contract_id).await?;
+            let mut gw_pay_sub = gateway
+                .gateway_subscribe_ln_pay(gw_pay_op)
+                .await?
+                .into_stream();
+            assert_eq!(gw_pay_sub.ok().await?, GatewayExtPayStates::Created);
+            assert_eq!(gw_pay_sub.ok().await?, GatewayExtPayStates::Preimage);
+            assert_eq!(gw_pay_sub.ok().await?, GatewayExtPayStates::Success);
+            Ok(())
+        },
+    )
+    .await
+}
+
+#[tokio::test(flavor = "multi_thread")]
 async fn test_gateway_client_pay_invalid_invoice() -> anyhow::Result<()> {
-    let gateway_node = LightningNodeType::Lnd;
+    gateway_test(
+        |_, other_lightning_client, _, user_client, gateway| async move {
+            // Print money for user client
+            let (_, outpoint) = user_client.print_money(sats(1000)).await?;
+            user_client.receive_money(outpoint).await?;
+            assert_eq!(user_client.get_balance().await, sats(1000));
 
-    let (fixtures, gateway_fixtures) = fixtures(&gateway_node).await;
-    let fed = fixtures.new_fed().await;
-    let user_client = fed.new_client().await;
+            // Create test invalid invoice
+            let invoice = other_lightning_client
+                .invalid_invoice(sats(250), None)
+                .unwrap();
 
-    let gateway = new_gateway_client(&fed, &gateway_fixtures).await?;
+            // User client pays test invoice
+            let (pay_op, contract_id) = user_client.pay_bolt11_invoice(invoice.clone()).await?;
+            let mut pay_sub = user_client.subscribe_ln_pay(pay_op).await?.into_stream();
+            assert_eq!(pay_sub.ok().await?, LnPayState::Created);
+            let funded = pay_sub.ok().await?;
+            assert_matches!(funded, LnPayState::Funded);
 
-    // Print money for client2
-    let (_, outpoint) = user_client.print_money(sats(1000)).await?;
-    user_client.receive_money(outpoint).await?;
-    assert_eq!(user_client.get_balance().await, sats(1000));
+            let gw_pay_op = gateway.gateway_pay_bolt11_invoice(contract_id).await?;
+            let mut gw_pay_sub = gateway
+                .gateway_subscribe_ln_pay(gw_pay_op)
+                .await?
+                .into_stream();
+            assert_eq!(gw_pay_sub.ok().await?, GatewayExtPayStates::Created);
+            assert_eq!(gw_pay_sub.ok().await?, GatewayExtPayStates::Canceled);
 
-    // Create test invoice
-    let invoice = gateway_fixtures
-        .other_lightning_client
-        .invalid_invoice(sats(250), None)
-        .unwrap();
-
-    // User client pays test invoice
-    let (pay_op, contract_id) = user_client.pay_bolt11_invoice(invoice.clone()).await?;
-    let mut pay_sub = user_client.subscribe_ln_pay(pay_op).await?.into_stream();
-    assert_eq!(pay_sub.ok().await?, LnPayState::Created);
-    let funded = pay_sub.ok().await?;
-    assert_matches!(funded, LnPayState::Funded);
-
-    let gw_pay_op = gateway.gateway_pay_bolt11_invoice(contract_id).await?;
-    let mut gw_pay_sub = gateway
-        .gateway_subscribe_ln_pay(gw_pay_op)
-        .await?
-        .into_stream();
-    assert_eq!(gw_pay_sub.ok().await?, GatewayExtPayStates::Created);
-    assert_eq!(gw_pay_sub.ok().await?, GatewayExtPayStates::Canceled);
-
-    Ok(())
+            Ok(())
+        },
+    )
+    .await
 }
