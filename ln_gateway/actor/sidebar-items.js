d32a1d7b9eb1b354fcfb690e63f68d879b8b229d
okjodom <okjodom@users.noreply.github.com>
2023-06-07 21:03:00 +0000

    deploy: c9c82a8df7483c9146d065161962464d84077a54

--- ln_gateway/actor/sidebar-items.js
+++ ln_gateway/actor/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["GW_ANNOUNCEMENT_TTL","How long a gateway announcement stays valid"]],"enum":[["BuyPreimage",""]],"struct":[["GatewayActor",""],["LightningSenderStream",""]],"type":[["RouteHTLCStream",""]]};
+window.SIDEBAR_ITEMS = {"constant":[["GW_ANNOUNCEMENT_TTL","How long a gateway announcement stays valid"]],"enum":[["BuyPreimage",""]],"struct":[["GatewayActor",""]]};
