f477fe3385f394c998041327c93f922654d25463
maan2003 <manmeetmann2003@gmail.com>
2023-06-14 19:39:09 +0530

    fix: actually test wasm in CI

--- scripts/wasm-tests.sh
+++ scripts/wasm-tests.sh
@@ -12,4 +12,4 @@ devimint wait
 echo Funding LND gateway e-cash wallet ...
 scripts/pegin.sh 20000.0 1 "LND" 
 
-CARGO_BUILD_TARGET_DIR=$CARGO_BUILD_TARGET_DIR/wasm nix develop .#crossWasm -c wasm-pack test --firefox --headless fedimint-wasm-tests
+wasm-pack test --firefox --headless fedimint-wasm-tests
