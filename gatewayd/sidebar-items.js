3e05e95ba68c16dee8999ec7b5c95a173390ec91
okjodom <okjodom@users.noreply.github.com>
2023-06-15 19:44:00 +0000

    deploy: ea226caba4579152e5277c23f40c88770f3fb4e5

--- gatewayd/sidebar-items.js
+++ gatewayd/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["DB_FILE",""],["SHUTDOWN_TIMEOUT",""]],"fn":[["main","Fedimint Gateway Binary"]],"struct":[["GatewayOpts",""]]};
+window.SIDEBAR_ITEMS = {"constant":[["DB_FILE",""]],"fn":[["main","Fedimint Gateway Binary"]],"struct":[["GatewayOpts",""]]};
