862f8eac65fdeaf3b5b73a78930f0bb548c98e12
vnprc <vnprc@protonmail.com>
2023-06-22 15:26:34 -0400

    fix: remove check for git HEAD file using relative path
    The set_code_version() function was failing for some binaries, resulting
    in these binaries returning the fake commit hash from the `version-hash`
    command. This change removes the check for the HEAD file using a hard
    coded relative path and instead attempts the `git rev-parse HEAD`
    command directly. Errors are collected and printed to stderr.

--- fedimint-build/src/lib.rs
+++ fedimint-build/src/lib.rs
@@ -1,21 +1,31 @@
 use std::env;
-use std::fs::File;
+use std::io::{self, Write};
 use std::process::Command;
+
+const FAKE_COMMIT_HASH: &str = "01234569afbe457afa1d2683a099c7af48a523c1";
 
 pub fn set_code_version() {
     if env::var_os("CODE_VERSION").is_none() {
-        let git_hash = if File::open("./../../.git/HEAD").is_ok() {
-            let output = Command::new("git")
-                .args(["rev-parse", "HEAD"])
-                .output()
-                .unwrap();
-            String::from_utf8(output.stdout).unwrap()
-        } else {
-            // we set a fake hash here and make it easily recognizable
-            // by giving it a prefix (0x0123456)
-            String::from("01234569afbe457afa1d2683a099c7af48a523c1")
-        };
-        println!("cargo:rustc-env=CODE_VERSION={git_hash}");
+        let output = Command::new("git").args(["rev-parse", "HEAD"]).output();
+
+        match output {
+            Ok(output) => {
+                if output.status.success() {
+                    let git_hash = String::from_utf8_lossy(&output.stdout).trim().to_string();
+                    println!("cargo:rustc-env=CODE_VERSION={}", git_hash);
+                } else {
+                    let error_message = String::from_utf8_lossy(&output.stderr);
+                    io::stderr().write_all(error_message.as_bytes()).unwrap();
+                    println!("cargo:rustc-env=CODE_VERSION={}", FAKE_COMMIT_HASH);
+                }
+            }
+            Err(error) => {
+                io::stderr()
+                    .write_fmt(format_args!("Failed to execute git command: {}", error))
+                    .unwrap();
+                println!("cargo:rustc-env=CODE_VERSION={}", FAKE_COMMIT_HASH);
+            }
+        }
     }
     println!("cargo:rerun-if-env-changed=CODE_VERSION");
 }
