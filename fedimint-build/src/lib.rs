1a864c919fbfcb5638d33adfd33e01f6177bb4e3
vnprc <vnprc@protonmail.com>
2023-06-22 15:26:34 -0400

    fix: improve version hash detection and handling
    Don't silently ignore failures.

--- fedimint-build/src/lib.rs
+++ fedimint-build/src/lib.rs
@@ -1,21 +1,49 @@
 use std::env;
-use std::fs::File;
 use std::process::Command;
 
-pub fn set_code_version() {
-    if env::var_os("CODE_VERSION").is_none() {
-        let git_hash = if File::open("./../../.git/HEAD").is_ok() {
-            let output = Command::new("git")
-                .args(["rev-parse", "HEAD"])
-                .output()
-                .unwrap();
-            String::from_utf8(output.stdout).unwrap()
-        } else {
-            // we set a fake hash here and make it easily recognizable
-            // by giving it a prefix (0x0123456)
-            String::from("01234569afbe457afa1d2683a099c7af48a523c1")
-        };
-        println!("cargo:rustc-env=CODE_VERSION={git_hash}");
+const FORCE_GIT_HASH_ENV: &str = "FEDIMINT_BUILD_FORCE_GIT_HASH";
+
+fn set_code_version_inner() -> Result<(), String> {
+    println!("cargo:rerun-if-env-changed={FORCE_GIT_HASH_ENV}");
+
+    if let Ok(hash) = env::var(FORCE_GIT_HASH_ENV) {
+        eprintln!("Forced hash via {FORCE_GIT_HASH_ENV} to {hash}");
+        println!("cargo:rustc-env=CODE_VERSION={hash}");
+        return Ok(());
     }
-    println!("cargo:rerun-if-env-changed=CODE_VERSION");
+
+    let output = match Command::new("git").args(["rev-parse", "HEAD"]).output() {
+        Ok(output) => output,
+        Err(e) => {
+            return Err(format!("Failed to execute `git` command: {e}"));
+        }
+    };
+
+    if !output.status.success() {
+        return Err(format!(
+            "`git` command failed: stderr: {}; stdout: {}",
+            String::from_utf8_lossy(&output.stderr),
+            String::from_utf8_lossy(&output.stdout)
+        ));
+    }
+
+    let hash = match String::from_utf8(output.stdout) {
+        Ok(hash) => hash.trim().to_string(),
+        Err(e) => {
+            return Err(format!("Invalid UTF-8 sequence detected: {e}"));
+        }
+    };
+
+    println!("cargo:rustc-env=CODE_VERSION={hash}");
+
+    Ok(())
+}
+
+pub fn set_code_version() {
+    match set_code_version_inner() {
+        Ok(()) => {}
+        Err(e) => {
+            panic!("Failed to detect git hash version: {e}. Set {FORCE_GIT_HASH_ENV} to skip this check")
+        }
+    }
 }
