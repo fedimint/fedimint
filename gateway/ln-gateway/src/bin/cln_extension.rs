0f71b37b40bb5877e4c94159250b5774f58c2051
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-08-16 12:19:10 -0700

    fix: lightningd API changes

--- gateway/ln-gateway/src/bin/cln_extension.rs
+++ gateway/ln-gateway/src/bin/cln_extension.rs
@@ -24,7 +24,7 @@ use ln_gateway::gatewaylnrpc::{
     InterceptHtlcResponse, PayInvoiceRequest, PayInvoiceResponse,
 };
 use secp256k1::PublicKey;
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use tokio::io::{stdin, stdout};
 use tokio::sync::{mpsc, oneshot, Mutex};
@@ -76,23 +76,10 @@ async fn main() -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-/// The core-lightning `htlc_accepted` event's `amount` field has a "msat"
-/// suffix
-fn as_fedimint_amount<'de, D>(amount: D) -> Result<Amount, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let amount = String::deserialize(amount)?;
-    Ok(Amount::from_msats(
-        amount[0..amount.len() - 4].parse::<u64>().unwrap(),
-    ))
-}
-
 // TODO: upstream these structs to cln-plugin
 // See: https://github.com/ElementsProject/lightning/blob/master/doc/PLUGINS.md#htlc_accepted
 #[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct Htlc {
-    #[serde(deserialize_with = "as_fedimint_amount")]
     pub amount_msat: Amount,
     // TODO: use these to validate we can actually redeem the HTLC in time
     pub cltv_expiry: u32,
@@ -108,7 +95,6 @@ pub struct Htlc {
 pub struct Onion {
     #[serde(default)]
     pub short_channel_id: Option<String>,
-    #[serde(deserialize_with = "as_fedimint_amount")]
     pub forward_msat: Amount,
 }
 
