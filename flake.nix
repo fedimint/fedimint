2d02e4279b638dc38d5453705d447bf4ae1b926a
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-09-19 09:26:40 -0700

    Revert "Revert "chore: use a new incremental zstd crane mode""
    This reverts commit 26ea2aecc2cc4b65e737ea6fcf8e4938bc5b37d2.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-kitman.url = "github:jkitman/nixpkgs/add-esplora-pkg";
-    crane.url = "github:ipetkov/crane?rev=6c25eff4edca8556df21f55c63e49f20efe4be95";
+    crane.url = "github:dpc/crane?rev=0fd1a6da318194d7d035b393bceff3942fcfc111";
     crane.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     fenix = {
