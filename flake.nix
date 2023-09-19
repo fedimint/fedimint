e734151f335dc3b8e4f5353615211ce081272bde
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-09-19 12:20:10 -0700

    chore: rebase use-zstd crane version

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-kitman.url = "github:jkitman/nixpkgs/add-esplora-pkg";
-    crane.url = "github:dpc/crane?rev=0fd1a6da318194d7d035b393bceff3942fcfc111";
+    crane.url = "github:dpc/crane?rev=60dcbcab46446bb852473a995fb0008d74c8b78d";
     crane.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     fenix = {
