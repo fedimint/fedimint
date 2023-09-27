a1d32e2bade8d2c201e7404ebd7f507a7b68e0ce
elsirion <elsirion@protonmail.com>
2023-09-27 17:48:51 +0200

    fix(lightning): replace bincode encoding impl

--- fedimint-core/src/encoding/tbs.rs
+++ fedimint-core/src/encoding/tbs.rs
@@ -172,6 +172,42 @@ impl Decodable for tbs::BlindingKey {
     }
 }
 
+impl Encodable for threshold_crypto::Ciphertext {
+    fn consensus_encode<W: Write>(&self, writer: &mut W) -> Result<usize, Error> {
+        self.to_bytes().consensus_encode(writer)
+    }
+}
+
+impl Decodable for threshold_crypto::Ciphertext {
+    fn consensus_decode<R: Read>(
+        reader: &mut R,
+        modules: &ModuleDecoderRegistry,
+    ) -> Result<Self, DecodeError> {
+        let ciphertext_bytes = Vec::<u8>::consensus_decode(reader, modules)?;
+        threshold_crypto::Ciphertext::from_bytes(&ciphertext_bytes).ok_or_else(|| {
+            DecodeError::from_str("Error decoding threshold_crypto::Ciphertext from bytes")
+        })
+    }
+}
+
+impl Encodable for threshold_crypto::DecryptionShare {
+    fn consensus_encode<W: Write>(&self, writer: &mut W) -> Result<usize, Error> {
+        self.to_bytes().consensus_encode(writer)
+    }
+}
+
+impl Decodable for threshold_crypto::DecryptionShare {
+    fn consensus_decode<R: Read>(
+        reader: &mut R,
+        modules: &ModuleDecoderRegistry,
+    ) -> Result<Self, DecodeError> {
+        let decryption_share_bytes = <[u8; 48]>::consensus_decode(reader, modules)?;
+        threshold_crypto::DecryptionShare::from_bytes(&decryption_share_bytes).ok_or_else(|| {
+            DecodeError::from_str("Error decoding threshold_crypto::DecryptionShare from bytes")
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use tbs::{BlindedMessage, BlindingKey};
@@ -188,5 +224,19 @@ mod tests {
     fn test_bkey() {
         let bkey = BlindingKey::random();
         test_roundtrip(bkey);
+    }
+
+    #[test_log::test]
+    fn test_ciphertext() {
+        let sks = threshold_crypto::SecretKeySet::random(1, &mut rand::thread_rng());
+        let pks = sks.public_keys();
+        let pk = pks.public_key();
+
+        let message = b"Hello world!";
+        let ciphertext = pk.encrypt(message);
+        let decryption_share = sks.secret_key_share(0).decrypt_share(&ciphertext).unwrap();
+
+        test_roundtrip(ciphertext);
+        test_roundtrip(decryption_share);
     }
 }
