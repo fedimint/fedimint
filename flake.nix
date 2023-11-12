5e141d9ca96b9cdd0c81ad6cd9eb014c54e58217
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-11-11 20:14:30 -0800

    chore: update nixpkgs (`nix flake update`)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs?rev=5e4c2ada4fcd54b99d56d7bd62f384511a7e2593"; #ref=nixos-unstable
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-kitman.url = "github:jkitman/nixpkgs/add-esplora-pkg";
     flake-utils.url = "github:numtide/flake-utils";
     flakebox = {
