8872e4f9138a10123140cb088e1f54657940a5f2
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-12-04 11:43:35 -0800

    chore: use older version of clightning

--- flake.nix
+++ flake.nix
@@ -32,7 +32,17 @@
                   inherit (prev.darwin.apple_sdk.frameworks) Security;
                 };
 
-                clightning = prev.clightning.overrideAttrs (oldAttrs: {
+                # syncing channels doesn't work right on newer versions, exactly like described here
+                # https://bitcoin.stackexchange.com/questions/84765/how-can-channel-policy-be-missing
+                # note that config-time `--enable-developer` turns into run-time `--developer` at some
+                # point
+                clightning = prev.clightning.overrideAttrs (oldAttrs: rec {
+                  version = "23.05.2";
+                  src = prev.fetchurl {
+                    url = "https://github.com/ElementsProject/lightning/releases/download/v${version}/clightning-v${version}.zip";
+                    sha256 = "sha256-Tj5ybVaxpk5wmOw85LkeU4pgM9NYl6SnmDG2gyXrTHw=";
+                  };
+                  makeFlags = [ "VERSION=v${version}" ];
                   configureFlags = [ "--enable-developer" "--disable-valgrind" ];
                 } // pkgs.lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
                   NIX_CFLAGS_COMPILE = "-Wno-stringop-truncation -w";
