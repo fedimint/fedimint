d3eba69ba4df8ca5324745f0340eef69eab142d1
elsirion <elsirion@protonmail.com>
2023-12-04 11:57:24 +0100

    chore: make `just mprocs` parametric

--- scripts/dev/mprocs/run.sh
+++ scripts/dev/mprocs/run.sh
@@ -8,4 +8,4 @@ ensure_in_dev_shell
 build_workspace
 add_target_dir_to_path
 
-devimint --link-test-dir ./target/devimint dev-fed --exec bash -c 'mprocs -c misc/mprocs.yaml 2>$FM_LOGS_DIR/devimint-outer.log'
+devimint --link-test-dir ./target/devimint "$@" dev-fed --exec bash -c 'mprocs -c misc/mprocs.yaml 2>$FM_LOGS_DIR/devimint-outer.log'
