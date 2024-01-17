4f819a94a187bdc4cbe87546b75aead84ae40b9b
Brad Stachurski <bradley.stachurski@gmail.com>
2024-01-17 18:08:00 +0000

    wip: full suite

--- scripts/tests/backwards-compatibility-test.sh
+++ scripts/tests/backwards-compatibility-test.sh
@@ -52,79 +52,46 @@ function use_gateway_binaries_for_version() {
 }
 
 test_results="fed_version,client_version,gateway_version,exit_code\n"
-# versions+=("current")
-# for fed_version in "${versions[@]}"; do
-#   for client_version in "${versions[@]}"; do
-#     for gateway_version in "${versions[@]}"; do
-#       # test-ci-all already tests binaries running the same version, so no need to run again
-#       if [[ "$fed_version" == "$client_version" && "$fed_version" == "$gateway_version" ]]; then
-#         continue
-#       fi
+versions+=("current")
+for fed_version in "${versions[@]}"; do
+  for client_version in "${versions[@]}"; do
+    for gateway_version in "${versions[@]}"; do
+      # test-ci-all already tests binaries running the same version, so no need to run again
+      if [[ "$fed_version" == "$client_version" && "$fed_version" == "$gateway_version" ]]; then
+        continue
+      fi
 
-#       use_fed_binaries_for_version "$fed_version"
-#       use_client_binaries_for_version "$client_version"
-#       use_gateway_binaries_for_version "$gateway_version"
+      use_fed_binaries_for_version "$fed_version"
+      use_client_binaries_for_version "$client_version"
+      use_gateway_binaries_for_version "$gateway_version"
 
-#       >&2 echo "========== Starting backwards-compatibility run ==========="
-#       >&2 echo "fed version: $fed_version"
-#       >&2 echo "client version: $client_version"
-#       >&2 echo "gateway version: $gateway_version"
-#       >&2 df -h
-#       >&2 nix run nixpkgs#du-dust /
+      >&2 echo "========== Starting backwards-compatibility run ==========="
+      >&2 echo "fed version: $fed_version"
+      >&2 echo "client version: $client_version"
+      >&2 echo "gateway version: $gateway_version"
+      >&2 df -h
+      >&2 nix run nixpkgs#du-dust /
 
-#       # continue running against other versions if there's a failure
-#       set +e
-#       (./scripts/tests/test-ci-all.sh)
-#       exit_code=$?
-#       set -e
-#       test_results="$test_results$fed_version,$client_version,$gateway_version,$exit_code\n"
+      # continue running against other versions if there's a failure
+      set +e
+      (./scripts/tests/test-ci-all.sh)
+      exit_code=$?
+      set -e
+      test_results="$test_results$fed_version,$client_version,$gateway_version,$exit_code\n"
 
-#       # cleanup devimint
-#       tmpdir=$(dirname "$(mktemp -u)")
-#       rm -rf "$tmpdir"/devimint-*
+      # cleanup devimint
+      tmpdir=$(dirname "$(mktemp -u)")
+      rm -rf "$tmpdir"/devimint-*
 
-#       >&2 echo "========== Finished backwards-compatibility run ==========="
-#       >&2 echo "fed version: $fed_version"
-#       >&2 echo "client version: $client_version"
-#       >&2 echo "gateway version: $gateway_version"
-#       >&2 df -h
-#       >&2 nix run nixpkgs#du-dust /
-#     done
-#   done
-# done
-
-fed_version=v0.2.1
-gateway_version=v0.2.1
-client_version=current
-
-use_fed_binaries_for_version "$fed_version"
-use_client_binaries_for_version "$client_version"
-use_gateway_binaries_for_version "$gateway_version"
-
->&2 echo "========== Starting backwards-compatibility run ==========="
->&2 echo "fed version: $fed_version"
->&2 echo "client version: $client_version"
->&2 echo "gateway version: $gateway_version"
->&2 df -h
->&2 nix run nixpkgs#du-dust /
-
-# continue running against other versions if there's a failure
-set +e
-(./scripts/tests/test-ci-all.sh)
-exit_code=$?
-set -e
-test_results="$test_results$fed_version,$client_version,$gateway_version,$exit_code\n"
-
-# cleanup devimint
-tmpdir=$(dirname "$(mktemp -u)")
-rm -rf "$tmpdir"/devimint-*
-
->&2 echo "========== Finished backwards-compatibility run ==========="
->&2 echo "fed version: $fed_version"
->&2 echo "client version: $client_version"
->&2 echo "gateway version: $gateway_version"
->&2 df -h
->&2 nix run nixpkgs#du-dust /
+      >&2 echo "========== Finished backwards-compatibility run ==========="
+      >&2 echo "fed version: $fed_version"
+      >&2 echo "client version: $client_version"
+      >&2 echo "gateway version: $gateway_version"
+      >&2 df -h
+      >&2 nix run nixpkgs#du-dust /
+    done
+  done
+done
 
 >&2 echo "Backwards-compatibility tests summary:"
 echo -e "$test_results" | >&2 column -t -s ','
