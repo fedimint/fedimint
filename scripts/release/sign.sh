0944baf1d8240c27dc60069b3e819a5bea26a169
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-02-20 09:04:38 -0800

    chore: switch to signing bundled binaries
    Re #4339

--- scripts/release/sign.sh
+++ scripts/release/sign.sh
@@ -13,18 +13,25 @@ release_dir_base="releases/bins"
 release_dir="${release_dir_base}/${prefix}"
 sha256sum_path="releases/${prefix}.SHA256SUMS"
 
-mkdir -p "${release_dir}"
-
 
 >&2 echo "Building..."
-for out in fedimint-pkgs gateway-pkgs ; do
-  nix build "git+file:.?ref=refs/tags/${tag}#${out}"
-  cp -f result/bin/* "${release_dir}/"
+
+# TODO: add gateway-cln-extension once available as an output
+for bin in fedimintd fedimint-cli fedimint-dbtool gateway-cli gatewayd ; do
+  out="git+file:.?ref=refs/tags/${tag}#${bin}"
+  nix build "$out"
+  mkdir -p "${release_dir}/nixos"
+  cp -f result/bin/* "${release_dir}/nixos/"
+
+  # TODO: re-export pinned bundlers from our own flake, so they are pinned at the release time
+  # and use the bundler exported in the release: '--bundler git+file:?ref/targs/'
+  # TODO: switch back to upstream after https://github.com/matthewbauer/nix-bundle/pull/103 is available
+  nix bundle --bundler "github:dpc/bundlers?branch=24-02-21-tar-deterministic&rev=e8aafe89a11ae0a5f3ce97d1d7d0fcfb354c79eb" "$out" -o result
+  cp -f -L result "${release_dir}/$bin"
 done
 
-
 >&2 echo "Checksumming..."
-( cd "${release_dir_base}"; sha256sum -- "${prefix}"/* ) > "${sha256sum_path}"
+( cd "${release_dir_base}"; find "${prefix}" -type f -print0 | LC_ALL=C sort -z | xargs -0 sha256sum ) > "${sha256sum_path}"
 
 
 >&2 echo "Signinig..."
