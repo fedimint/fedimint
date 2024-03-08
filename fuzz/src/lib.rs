3199076075219945bc42188f7c6110c619d3f845
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-03-07 21:16:32 -0800

    chore(fuzz): ensure MAX_DECODE_SIZE respected

--- fuzz/src/lib.rs
+++ fuzz/src/lib.rs
@@ -1,10 +1,12 @@
-use fedimint_core::encoding::{Decodable, Encodable};
+use fedimint_core::encoding::{self, Decodable, Encodable};
 
 pub fn test_decodable<T>(data: &[u8])
 where
     T: Decodable + Encodable,
 {
     if let Ok(v) = T::consensus_decode(&mut &data[..], &Default::default()) {
+        assert!(data.len() <= encoding::MAX_DECODE_SIZE);
+
         let encoded_vec = v.consensus_encode_to_vec();
         // helps debugging to have it standalone
         let encoded = encoded_vec.as_slice();
