54e373682624e7dd400bae02850f6b7b8281aefc
maan2003 <manmeetmann2003@gmail.com>
2024-04-11 00:57:02 +0530

    chore: auto update on list-gateways

--- devimint/src/federation.rs
+++ devimint/src/federation.rs
@@ -398,15 +398,8 @@ impl Federation {
     pub async fn await_gateways_registered(&self) -> Result<()> {
         let start_time = Instant::now();
         debug!(target: LOG_DEVIMINT, "Awaiting LN gateways registration");
-        let fedimint_cli_version = crate::util::FedimintCli::version_or_default().await;
-        let command = if fedimint_cli_version < *VERSION_0_3_0_ALPHA {
-            "list-gateways"
-        } else {
-            "update-gateway-cache"
-        };
-
         poll("gateways registered", || async {
-            let num_gateways = cmd!(self.client, command)
+            let num_gateways = cmd!(self.client, "list-gateways")
                 .out_json()
                 .await
                 .map_err(ControlFlow::Continue)?
