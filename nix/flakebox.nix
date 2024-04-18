a965f9a3b8b47a27d576bd8218dd99a4d13323cd
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-04-18 16:32:02 -0700

    chore(ci): make the rust tests deadlines more intolerant

--- nix/flakebox.nix
+++ nix/flakebox.nix
@@ -399,7 +399,7 @@ rec {
     lib.concatStringsSep "\n"
       (
         lib.replicate times ''
-          env RUST_BACKTRACE=1 RUST_LOG=info,timing=debug cargo nextest run --locked --workspace --all-targets --cargo-profile $CARGO_PROFILE --profile $CARGO_PROFILE --test-threads=$(($(nproc) * 2))
+          env RUST_BACKTRACE=1 RUST_LOG=info,timing=debug cargo nextest run --locked --workspace --all-targets --cargo-profile $CARGO_PROFILE --profile ccov --test-threads=$(($(nproc) * 2))
         ''
       ) + ''
       mkdir -p $out
