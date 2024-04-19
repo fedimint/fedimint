cf2a8127d70b2f75a21532b740e5806fee7a8d29
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-04-18 21:07:40 -0700

    chore(ci): parallelize single_federation_test

--- gateway/ln-gateway/tests/integration_tests.rs
+++ gateway/ln-gateway/tests/integration_tests.rs
@@ -99,16 +99,19 @@ where
     let lnd2 = fixtures.lnd().await;
     let cln2 = fixtures.cln().await;
 
-    for (gateway_ln, other_node) in [(lnd1, cln1), (cln2, lnd2)] {
-        let fed = fixtures.new_fed().await;
-        let mut gateway = fixtures
-            .new_gateway(gateway_ln, 0, Some(DEFAULT_GATEWAY_PASSWORD.to_string()))
-            .await;
-        gateway.connect_fed(&fed).await;
-        let user_client = fed.new_client().await;
-        let bitcoin = fixtures.bitcoin();
-        f(gateway, other_node, fed, user_client, bitcoin).await?;
-    }
+    futures::future::try_join_all([(lnd1, cln1), (cln2, lnd2)].into_iter().map(
+        |(gateway_ln, other_node)| async {
+            let fed = fixtures.new_fed().await;
+            let mut gateway = fixtures
+                .new_gateway(gateway_ln, 0, Some(DEFAULT_GATEWAY_PASSWORD.to_string()))
+                .await;
+            gateway.connect_fed(&fed).await;
+            let user_client = fed.new_client().await;
+            let bitcoin = fixtures.bitcoin();
+            f(gateway, other_node, fed, user_client, bitcoin).await
+        },
+    ))
+    .await?;
     Ok(())
 }
 
