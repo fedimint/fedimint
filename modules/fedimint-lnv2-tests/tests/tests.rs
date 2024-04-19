2ff7837a4b87910c263e07db4f49bd3b9f14cba3
joschisan <122358257+joschisan@users.noreply.github.com>
2024-04-19 09:50:30 +0200

    cleanup: dont use ln legacy to get the gateway api anymore

--- modules/fedimint-lnv2-tests/tests/tests.rs
+++ modules/fedimint-lnv2-tests/tests/tests.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 
 use fedimint_client::ClientHandle;
-use fedimint_core::bitcoin_migration::bitcoin30_to_bitcoin29_secp256k1_public_key;
 use fedimint_core::config::FederationId;
 use fedimint_core::core::OperationId;
 use fedimint_core::util::NextOrPending;
@@ -29,7 +28,9 @@ fn fixtures() -> Fixtures {
         LightningGenParams::regtest(bitcoin_server.clone()),
     );
 
-    // we attach the legacy lightning module for gateway registration
+    // TODO: We still have to attach the legacy lightning module such that the
+    // gateway can connect to a federation. Remove this once connection to a
+    // federation does not require lightning legacy anymore.
     fixtures.with_module(
         fedimint_ln_client::LightningClientInit,
         fedimint_ln_server::LightningInit,
@@ -73,7 +74,8 @@ async fn can_pay_external_invoice_exactly_once() -> anyhow::Result<()> {
 
     let fixtures = fixtures();
     let fed = fixtures.new_fed().await;
-    let gw = gateway(&fixtures, &fed).await;
+    let gateway_test = gateway(&fixtures, &fed).await;
+    let gateway_api = gateway_test.gateway.versioned_api.clone();
 
     let cln = fixtures.cln().await;
     let invoice = cln.invoice(Amount::from_sats(100), None).await?;
@@ -88,22 +90,14 @@ async fn can_pay_external_invoice_exactly_once() -> anyhow::Result<()> {
 
     client.await_primary_module_output(op, outpoint).await?;
 
-    let gateway = client
-        .get_first_module::<fedimint_ln_client::LightningClientModule>()
-        .select_gateway(&bitcoin30_to_bitcoin29_secp256k1_public_key(
-            gw.get_gateway_id(),
-        ))
-        .await
-        .expect("Could not select gateway");
-
     let operation_id = client
         .get_first_module::<LightningClientModule>()
-        .send(gateway.api.clone(), invoice.clone())
+        .send(gateway_api.clone(), invoice.clone())
         .await?;
 
     let send_result = client
         .get_first_module::<LightningClientModule>()
-        .send(gateway.api.clone(), invoice.clone())
+        .send(gateway_api.clone(), invoice.clone())
         .await;
 
     assert_eq!(
@@ -123,7 +117,7 @@ async fn can_pay_external_invoice_exactly_once() -> anyhow::Result<()> {
 
     let send_result = client
         .get_first_module::<LightningClientModule>()
-        .send(gateway.api, invoice)
+        .send(gateway_api, invoice)
         .await;
 
     assert_eq!(
@@ -143,7 +137,8 @@ async fn refund_unpayable_invoice() -> anyhow::Result<()> {
 
     let fixtures = fixtures();
     let fed = fixtures.new_fed().await;
-    let gw = gateway(&fixtures, &fed).await;
+    let gateway_test = gateway(&fixtures, &fed).await;
+    let gateway_api = gateway_test.gateway.versioned_api.clone();
 
     let cln = fixtures.cln().await;
     let invoice = cln.unpayable_invoice(Amount::from_sats(100), None);
@@ -158,17 +153,9 @@ async fn refund_unpayable_invoice() -> anyhow::Result<()> {
 
     client.await_primary_module_output(op, outpoint).await?;
 
-    let gateway = client
-        .get_first_module::<fedimint_ln_client::LightningClientModule>()
-        .select_gateway(&bitcoin30_to_bitcoin29_secp256k1_public_key(
-            gw.get_gateway_id(),
-        ))
-        .await
-        .expect("Could not select gateway");
-
     let op = client
         .get_first_module::<LightningClientModule>()
-        .send(gateway.api, invoice)
+        .send(gateway_api, invoice)
         .await?;
 
     let mut sub = client
@@ -189,21 +176,14 @@ async fn refund_unpayable_invoice() -> anyhow::Result<()> {
 async fn can_make_self_payment_exactly_once() -> anyhow::Result<()> {
     let fixtures = fixtures();
     let fed = fixtures.new_fed().await;
-    let gw = gateway(&fixtures, &fed).await;
+    let gateway_test = gateway(&fixtures, &fed).await;
+    let gateway_api = gateway_test.gateway.versioned_api.clone();
 
     let client = fed.new_client().await;
 
-    let gateway = client
-        .get_first_module::<fedimint_ln_client::LightningClientModule>()
-        .select_gateway(&bitcoin30_to_bitcoin29_secp256k1_public_key(
-            gw.get_gateway_id(),
-        ))
-        .await
-        .expect("Could not select gateway");
-
     let (invoice, receive_op) = client
         .get_first_module::<LightningClientModule>()
-        .receive(gateway.api.clone(), Amount::from_sats(100))
+        .receive(gateway_api.clone(), Amount::from_sats(100))
         .await?;
 
     let (print_op, print_outpoint) = client
@@ -219,7 +199,7 @@ async fn can_make_self_payment_exactly_once() -> anyhow::Result<()> {
     // liquidity in the federation yet
     let send_op_refunded = client
         .get_first_module::<LightningClientModule>()
-        .send(gateway.api.clone(), invoice.clone())
+        .send(gateway_api.clone(), invoice.clone())
         .await?;
 
     let mut sub = client
@@ -234,11 +214,11 @@ async fn can_make_self_payment_exactly_once() -> anyhow::Result<()> {
     assert_eq!(sub.ok().await?, SendState::Refunded);
 
     // now we print liquidity such that the second payment attempt is successful
-    print_liquidity(&gw, fed.id()).await;
+    print_liquidity(&gateway_test, fed.id()).await;
 
     let send_op = client
         .get_first_module::<LightningClientModule>()
-        .send(gateway.api.clone(), invoice.clone())
+        .send(gateway_api.clone(), invoice.clone())
         .await?;
 
     // verify that the first and second payment attempt have different operation ids
@@ -247,7 +227,7 @@ async fn can_make_self_payment_exactly_once() -> anyhow::Result<()> {
     // this will return an error as the second payment attempt is still pending
     let send_result = client
         .get_first_module::<LightningClientModule>()
-        .send(gateway.api.clone(), invoice.clone())
+        .send(gateway_api.clone(), invoice.clone())
         .await;
 
     assert_eq!(
@@ -261,7 +241,7 @@ async fn can_make_self_payment_exactly_once() -> anyhow::Result<()> {
     // this will return an error as the second payment attempt was successful
     let send_result = client
         .get_first_module::<LightningClientModule>()
-        .send(gateway.api, invoice)
+        .send(gateway_api, invoice)
         .await;
 
     assert_eq!(
@@ -279,25 +259,18 @@ async fn direct_swap() -> anyhow::Result<()> {
     let fed_send = fixtures.new_fed().await;
     let fed_receive = fixtures.new_fed().await;
 
-    let mut gw = gateway(&fixtures, &fed_send).await;
+    let mut gateway_test = gateway(&fixtures, &fed_send).await;
+    let gateway_api = gateway_test.gateway.versioned_api.clone();
 
-    gw.connect_fed(&fed_receive).await;
+    gateway_test.connect_fed(&fed_receive).await;
 
-    print_liquidity(&gw, fed_receive.id()).await;
+    print_liquidity(&gateway_test, fed_receive.id()).await;
 
     let client_receive = fed_receive.new_client().await;
 
-    let gateway = client_receive
-        .get_first_module::<fedimint_ln_client::LightningClientModule>()
-        .select_gateway(&bitcoin30_to_bitcoin29_secp256k1_public_key(
-            gw.get_gateway_id(),
-        ))
-        .await
-        .expect("Could not select gateway");
-
     let (invoice, receive_op) = client_receive
         .get_first_module::<LightningClientModule>()
-        .receive(gateway.api.clone(), Amount::from_sats(100))
+        .receive(gateway_api.clone(), Amount::from_sats(100))
         .await?;
 
     let client_send = fed_send.new_client().await;
@@ -313,7 +286,7 @@ async fn direct_swap() -> anyhow::Result<()> {
 
     let send_op = client_send
         .get_first_module::<LightningClientModule>()
-        .send(gateway.api, invoice)
+        .send(gateway_api, invoice)
         .await?;
 
     verify_payment_success(
