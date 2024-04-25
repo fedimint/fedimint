340eee1ef7bcfa71e9f04d9041c545bfcd7567ed
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-04-24 23:48:56 -0700

    chore: more debug

--- scripts/tests/test-ci-all-backcompat.sh
+++ scripts/tests/test-ci-all-backcompat.sh
@@ -3,4 +3,9 @@
 # previous versions did not have unknown module so can't support it,
 # disable devimint enabling it in Federations
 export FM_USE_UNKNOWN_MODULE=0
+
+path="$CARGO_BUILD_TARGET_DIR/ci/deps/libserde_json-09c44f0222198895.rmeta"
+ls -alh "$path"
+md5sum "$path"
+
 ./scripts/tests/test-ci-all.sh "$@"
