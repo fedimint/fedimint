56532b71759d10185c03eb3de3174e3c1ea5f1bd
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-04-26 17:33:12 -0700

    chore: debug ci flake

--- scripts/tests/test-ci-all-backcompat.sh
+++ scripts/tests/test-ci-all-backcompat.sh
@@ -3,4 +3,7 @@
 # previous versions did not have unknown module so can't support it,
 # disable devimint enabling it in Federations
 export FM_USE_UNKNOWN_MODULE=0
+
+export RUST_LOG=${RUST_LOG:-debug}
+
 ./scripts/tests/test-ci-all.sh "$@"
