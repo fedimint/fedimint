952a5d240f3bb9bc0c6477fd43e812f3e029fe57
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-04-29 13:31:35 -0700

    chore(backward-compat): disable `h2` logs that spam the output

--- scripts/tests/test-ci-all-backcompat.sh
+++ scripts/tests/test-ci-all-backcompat.sh
@@ -4,6 +4,6 @@
 # disable devimint enabling it in Federations
 export FM_USE_UNKNOWN_MODULE=0
 
-export RUST_LOG=${RUST_LOG:-debug}
+export RUST_LOG=${RUST_LOG:-h2=off,debug}
 
 ./scripts/tests/test-ci-all.sh "$@"
