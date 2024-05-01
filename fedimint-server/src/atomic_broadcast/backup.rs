beb5892f0fb7ac485fe47b9b5091ecd9bda2dda8
joschisan <122358257+joschisan@users.noreply.github.com>
2024-05-01 10:07:07 +0200

    cleanup: cleanup reading and writing of aleph bft backup

--- fedimint-server/src/atomic_broadcast/backup.rs
+++ fedimint-server/src/atomic_broadcast/backup.rs
@@ -1,64 +1,71 @@
-use std::io::Cursor;
-
 use async_trait::async_trait;
 use fedimint_core::db::{Database, IDatabaseTransactionOpsCoreTyped};
+use futures::StreamExt;
+use tracing::info;
 
-use crate::db::AlephUnitsKey;
+use crate::db::{AlephUnitsKey, AlephUnitsPrefix};
 use crate::LOG_CONSENSUS;
 
-/// This function loads the aleph bft backup from disk and creates a UnitSaver
-/// instance which allows aleph bft to append further bytes to the existing
-/// backup
-pub async fn load_session(db: Database) -> (Cursor<Vec<u8>>, UnitSaver) {
-    let mut buffer = vec![];
-    let mut units_index = 0;
-    let mut dbtx = db.begin_transaction().await;
-
-    while let Some(bytes) = dbtx.get_value(&AlephUnitsKey(units_index)).await {
-        buffer.extend(bytes);
-        units_index += 1;
-    }
-
-    std::mem::drop(dbtx);
-
-    if !buffer.is_empty() {
-        tracing::info!(target: LOG_CONSENSUS, buffer_len = %buffer.len(), "Recovering from an in-session-shutdown");
-    }
-
-    // the cursor enables aleph bft to read the units via std::io::Read
-    let unit_loader = Cursor::new(buffer);
-
-    // we pass the first free unit index to the UnitSaver as an offset
-    let unit_saver = UnitSaver::new(db, units_index);
-
-    (unit_loader, unit_saver)
-}
-
-/// The UnitSaver enables aleph bft to store its local directed acyclic graph of
-/// units on disk in order to recover from a mid session crash.
-pub struct UnitSaver {
+pub struct BackupReader {
     db: Database,
-    units_index: u64,
 }
 
-impl UnitSaver {
-    fn new(db: Database, units_index: u64) -> Self {
-        Self { db, units_index }
+impl BackupReader {
+    pub fn new(db: Database) -> Self {
+        Self { db }
     }
 }
 
 #[async_trait]
-impl aleph_bft::BackupWriter for UnitSaver {
+impl aleph_bft::BackupReader for BackupReader {
+    async fn read(&mut self) -> std::io::Result<Vec<u8>> {
+        let mut dbtx = self.db.begin_transaction().await;
+
+        let units = dbtx
+            .find_by_prefix(&AlephUnitsPrefix)
+            .await
+            .map(|entry| entry.1)
+            .collect::<Vec<Vec<u8>>>()
+            .await;
+
+        if !units.is_empty() {
+            info!(target: LOG_CONSENSUS, units_len = %units.len(), "Recovering from an in-session-shutdown");
+        }
+
+        Ok(units.into_iter().flatten().collect())
+    }
+}
+
+pub struct BackupWriter {
+    db: Database,
+}
+
+impl BackupWriter {
+    pub fn new(db: Database) -> Self {
+        Self { db }
+    }
+}
+
+#[async_trait]
+impl aleph_bft::BackupWriter for BackupWriter {
     async fn append(&mut self, data: &[u8]) -> std::io::Result<()> {
         let mut dbtx = self.db.begin_transaction().await;
 
-        dbtx.insert_new_entry(&AlephUnitsKey(self.units_index), &data.to_owned())
+        let index = dbtx
+            .find_by_prefix_sorted_descending(&AlephUnitsPrefix)
+            .await
+            .next()
+            .await
+            .map(|entry| (entry.0 .0) + 1)
+            .unwrap_or(0);
+
+        dbtx.insert_new_entry(&AlephUnitsKey(index), &data.to_owned())
             .await;
 
         dbtx.commit_tx_result()
             .await
             .expect("This is the only place where we write to this key");
-        self.units_index += 1;
+
         Ok(())
     }
 }
