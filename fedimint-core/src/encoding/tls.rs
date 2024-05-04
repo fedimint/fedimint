8583547c8d50a703cba98ce2e478ba82d43d1590
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2024-05-04 18:21:29 +0530

    test_consensus_encode is done
    Signed-off-by: sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>

--- fedimint-core/src/encoding/tls.rs
+++ fedimint-core/src/encoding/tls.rs
@@ -13,8 +13,9 @@ impl Encodable for rustls::Certificate {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use std::io::Cursor;
+
+    use super::*;
 
     struct MockCertificate(Vec<u8>);
 
@@ -41,18 +42,17 @@ mod tests {
             (vec![], "\0"),
             (vec![0x00, 0x01, 0x02, 0x03, 0x04], "\n0001020304"),
         ];
-    
+
         for (data, expected_output) in &test_cases {
             let certificate = MockCertificate::new(data.clone());
-    
+
             let mut output_buffer = Cursor::new(Vec::new());
             let result = certificate.consensus_encode(&mut output_buffer);
-    
+
             assert!(result.is_ok());
-    
+
             let encoded_data = String::from_utf8(output_buffer.into_inner()).unwrap();
             assert_eq!(encoded_data, *expected_output);
         }
     }
-    
 }
