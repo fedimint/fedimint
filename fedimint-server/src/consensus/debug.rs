bbcb7df09b8d09ecd6a6681ed1d83ff88fdaa328
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-06-03 11:43:31 -0700

    chore: dump citems dump on consensus mismatch

--- fedimint-server/src/consensus/debug.rs
+++ fedimint-server/src/consensus/debug.rs
@@ -1,5 +1,9 @@
 use std::fmt;
 
+use bitcoin_hashes::{sha256, Hash as _};
+use fedimint_core::encoding::Encodable as _;
+use fedimint_core::session_outcome::AcceptedItem;
+
 use crate::ConsensusItem;
 
 /// A newtype for a nice [`fmt::Debug`] of a [`ConsensusItem`]
@@ -36,6 +40,60 @@ impl<'ci> fmt::Debug for DebugConsensusItem<'ci> {
                 f.write_fmt(format_args!("Unknown CI variant: {variant}"))?;
             }
         }
+        Ok(())
+    }
+}
+
+/// A compact citem formatter, useful for debugging in case of consensus failure
+///
+/// Unlike [`DebugConsensusItem`], this one is used when a (potentially) long
+/// list of citems are dumped, so it needs to be very compact.
+pub struct DebugConsensusItemCompact<'a>(pub &'a AcceptedItem);
+
+impl<'a> fmt::Display for DebugConsensusItemCompact<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut engine = sha256::HashEngine::default();
+        let len = self
+            .0
+            .consensus_encode(&mut engine)
+            .map_err(|_| fmt::Error)?;
+        let hash = *sha256::Hash::from_engine(engine).as_byte_array();
+        f.write_fmt(format_args!(
+            "{}; peer={}; len={}; ",
+            hex::encode(&hash[0..12]),
+            self.0.peer,
+            len
+        ))?;
+
+        match &self.0.item {
+            ConsensusItem::Transaction(ref tx) => {
+                f.write_fmt(format_args!("txid={}; ", tx.tx_hash()))?;
+                f.write_str("inputs_module_ids=")?;
+                for (i, input) in tx.inputs.iter().enumerate() {
+                    if i != 0 {
+                        f.write_str(",")?;
+                    }
+                    f.write_fmt(format_args!("{}", input.module_instance_id()))?;
+                }
+                f.write_str("; outputs_module_ids=")?;
+                for (i, output) in tx.outputs.iter().enumerate() {
+                    if i != 0 {
+                        f.write_str(",")?;
+                    }
+                    f.write_fmt(format_args!("{}", output.module_instance_id()))?;
+                }
+            }
+            ConsensusItem::Module(module_citem) => {
+                f.write_fmt(format_args!(
+                    "citem={}; ",
+                    module_citem.module_instance_id()
+                ))?;
+            }
+            ConsensusItem::Default { variant, .. } => {
+                f.write_fmt(format_args!("unknown variant={variant}"))?;
+            }
+        }
+
         Ok(())
     }
 }
