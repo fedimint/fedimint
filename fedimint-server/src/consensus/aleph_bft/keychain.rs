36aa1ef62a65f5944e911602aa08be969c1e740f
joschisan <122358257+joschisan@users.noreply.github.com>
2024-06-04 16:02:18 +0200

    refactor: remove keychain attribute from consensus engine

--- fedimint-server/src/consensus/aleph_bft/keychain.rs
+++ fedimint-server/src/consensus/aleph_bft/keychain.rs
@@ -2,9 +2,10 @@ use std::collections::BTreeMap;
 use std::io::Write;
 
 use aleph_bft::Keychain as KeychainTrait;
+use bitcoin_hashes::Hash;
 use fedimint_core::encoding::Encodable;
 use fedimint_core::session_outcome::SchnorrSignature;
-use fedimint_core::{secp256k1, BitcoinHash, NumPeersExt, PeerId};
+use fedimint_core::{secp256k1, NumPeersExt, PeerId};
 use secp256k1::hashes::sha256;
 use secp256k1::{schnorr, KeyPair, Message, PublicKey};
 
@@ -12,16 +13,18 @@ use crate::config::ServerConfig;
 
 #[derive(Clone, Debug)]
 pub struct Keychain {
-    peer_id: PeerId,
-    public_keys: BTreeMap<PeerId, PublicKey>,
+    identity: PeerId,
+    pks: BTreeMap<PeerId, PublicKey>,
+    message_tag: sha256::Hash,
     keypair: KeyPair,
 }
 
 impl Keychain {
     pub fn new(cfg: &ServerConfig) -> Self {
         Keychain {
-            peer_id: cfg.local.identity,
-            public_keys: cfg.consensus.broadcast_public_keys.clone(),
+            identity: cfg.local.identity,
+            pks: cfg.consensus.broadcast_public_keys.clone(),
+            message_tag: cfg.consensus.broadcast_public_keys.consensus_hash(),
             keypair: cfg
                 .private
                 .broadcast_secret_key
@@ -29,25 +32,14 @@ impl Keychain {
         }
     }
 
-    pub fn peer_id(&self) -> PeerId {
-        self.peer_id
-    }
-
-    pub fn peer_count(&self) -> usize {
-        self.public_keys.total()
-    }
-
-    pub fn threshold(&self) -> usize {
-        self.public_keys.threshold()
-    }
-
-    fn tagged_hash(&self, message: &[u8]) -> Message {
+    // Tagging messages with the hash of the public key set ensures that peers with
+    // an incorrect public key set cannot create signatures that are accepted by
+    // their peers.
+    fn tagged_message(&self, message: &[u8]) -> Message {
         let mut engine = sha256::HashEngine::default();
 
-        let public_key_tag = self.public_keys.consensus_hash::<sha256::Hash>();
-
         engine
-            .write_all(public_key_tag.as_ref())
+            .write_all(self.message_tag.as_ref())
             .expect("Writing to a hash engine can not fail");
 
         engine
@@ -62,7 +54,7 @@ impl Keychain {
 
 impl aleph_bft::Index for Keychain {
     fn index(&self) -> aleph_bft::NodeIndex {
-        self.peer_id.to_usize().into()
+        self.identity.to_usize().into()
     }
 }
 
@@ -71,13 +63,13 @@ impl aleph_bft::Keychain for Keychain {
     type Signature = SchnorrSignature;
 
     fn node_count(&self) -> aleph_bft::NodeCount {
-        self.peer_count().into()
+        self.pks.total().into()
     }
 
     fn sign(&self, message: &[u8]) -> Self::Signature {
         SchnorrSignature(
             self.keypair
-                .sign_schnorr(self.tagged_hash(message))
+                .sign_schnorr(self.tagged_message(message))
                 .as_ref()
                 .to_owned(),
         )
@@ -89,14 +81,12 @@ impl aleph_bft::Keychain for Keychain {
         signature: &Self::Signature,
         node_index: aleph_bft::NodeIndex,
     ) -> bool {
-        let peer_id = super::to_peer_id(node_index);
-
-        if let Some(public_key) = self.public_keys.get(&peer_id) {
+        if let Some(public_key) = self.pks.get(&super::to_peer_id(node_index)) {
             if let Ok(sig) = schnorr::Signature::from_slice(&signature.0) {
                 return secp256k1::SECP256K1
                     .verify_schnorr(
                         &sig,
-                        &self.tagged_hash(message),
+                        &self.tagged_message(message),
                         &public_key.x_only_public_key().0,
                     )
                     .is_ok();
@@ -115,13 +105,13 @@ impl aleph_bft::MultiKeychain for Keychain {
         signature: &Self::Signature,
         index: aleph_bft::NodeIndex,
     ) -> Self::PartialMultisignature {
-        let mut partial = aleph_bft::NodeMap::with_size(self.peer_count().into());
+        let mut partial = aleph_bft::NodeMap::with_size(self.pks.total().into());
         partial.insert(index, signature.clone());
         partial
     }
 
     fn is_complete(&self, msg: &[u8], partial: &Self::PartialMultisignature) -> bool {
-        if partial.iter().count() < self.threshold() {
+        if partial.iter().count() < self.pks.threshold() {
             return false;
         }
 
