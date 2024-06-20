31c8fc0d85a90104b4ab9669f91cd4f443fddb58
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-20 11:48:07 +0530

    fix::> Stencil to implement ConsensusVersion

--- fedimintd/src/lib.rs
+++ fedimintd/src/lib.rs
@@ -8,14 +8,45 @@
 
 /// Module for creating `fedimintd` binary with custom modules
 use bitcoin::Network;
+use fedimint_core::core::{Decoder, ModuleInstanceId, ModuleKind};
 use fedimint_core::envs::BitcoinRpcConfig;
+use fedimint_core::module::{
+    ApiEndpoint, CommonModuleInit, CoreConsensusVersion, InputMeta, ModuleCommon,
+    ModuleConsensusVersion, ModuleInit, PeerHandle, ServerModuleInit, ServerModuleInitArgs,
+    SupportedModuleApiVersions, TransactionItemAmount, CORE_CONSENSUS_VERSION,
+};
 use fedimint_core::util::SafeUrl;
+use fedimint_core::{plugin_types_trait_impl_common, Amount};
 pub use fedimintd::*;
 
 mod fedimintd;
 
 pub mod envs;
 use crate::envs::FM_PORT_ESPLORA_ENV;
+
+pub const KIND: ModuleKind = ModuleKind::from_static_str("fedimintd");
+pub const MODULE_CONSENSUS_VERSION: ModuleConsensusVersion = ModuleConsensusVersion::new(2, 0);
+
+pub struct FedimintdModuleTypes;
+
+// plugin_types_trait_impl_common!(FedimintdModuleTypes);
+
+#[derive(Debug)]
+pub struct FedimintdInit;
+
+impl CommonModuleInit for FedimintdInit {
+    const CONSENSUS_VERSION: ModuleConsensusVersion = MODULE_CONSENSUS_VERSION;
+    const KIND: ModuleKind = KIND;
+
+    type ClientConfig = DummyClientConfig;
+
+    fn decoder() -> Decoder {
+        FedimintdModuleTypes::decoder_builder().build()
+    }
+}
+
+// pub const MODULE_CONSENSUS_VERSION: ModuleConsensusVersion =
+// ModuleConsensusVersion::new(2, 0);
 
 pub fn default_esplora_server(network: Network) -> BitcoinRpcConfig {
     let url = match network {
