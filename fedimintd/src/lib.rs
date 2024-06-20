7a59734e5c416cb169cc4f11678cb38c1c1dd15a
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-20 16:48:32 +0530

    fix::>  Made Config.rs inside fedimimtd/lib.rs

--- fedimintd/src/lib.rs
+++ fedimintd/src/lib.rs
@@ -6,9 +6,15 @@
 #![allow(clippy::must_use_candidate)]
 #![allow(clippy::return_self_not_must_use)]
 
+use std::fmt;
+
 /// Module for creating `fedimintd` binary with custom modules
 use bitcoin::Network;
 use fedimint_core::core::{Decoder, ModuleInstanceId, ModuleKind};
+use fedimint_core::db::{
+    Database, DatabaseTransaction, DatabaseVersion, IDatabaseTransactionOpsCoreTyped,
+};
+use fedimint_core::encoding::{Decodable, Encodable};
 use fedimint_core::envs::BitcoinRpcConfig;
 use fedimint_core::module::{
     ApiEndpoint, CommonModuleInit, CoreConsensusVersion, InputMeta, ModuleCommon,
@@ -16,8 +22,11 @@ use fedimint_core::module::{
     SupportedModuleApiVersions, TransactionItemAmount, CORE_CONSENSUS_VERSION,
 };
 use fedimint_core::util::SafeUrl;
-use fedimint_core::{plugin_types_trait_impl_common, Amount};
+use fedimint_core::{
+    plugin_types_trait_impl_common, plugin_types_trait_impl_config, Amount, Amount,
+};
 pub use fedimintd::*;
+use serde::{Deserialize, Serialize};
 
 mod fedimintd;
 
@@ -27,26 +36,115 @@ use crate::envs::FM_PORT_ESPLORA_ENV;
 pub const KIND: ModuleKind = ModuleKind::from_static_str("fedimintd");
 pub const MODULE_CONSENSUS_VERSION: ModuleConsensusVersion = ModuleConsensusVersion::new(2, 0);
 
-pub struct FedimintdModuleTypes;
-
-// plugin_types_trait_impl_common!(FedimintdModuleTypes);
-
 #[derive(Debug)]
 pub struct FedimintdInit;
 
-impl CommonModuleInit for FedimintdInit {
+impl ModuleInit for FedimintdInit {
+    type Common = FedimintdCommonInit;
+    const DATABASE_VERSION: DatabaseVersion = DatabaseVersion(1);
+    // fn dump_database(
+    //     &self,
+    //     dbtx: &mut DatabaseTransaction<'_>,
+    //     prefix_names: Vec<String>,
+    // ) -> maybe_add_send!(
+    //     impl Future<
+    //         Output = Box<
+    //             dyn Iterator<Item = (String, Box<dyn erased_serde::Serialize +
+    // Send>)> + '_,         >,
+    //     >
+    // ) {
+    // }
+}
+
+#[derive(Debug)]
+pub struct FedimintdCommonInit;
+impl CommonModuleInit for FedimintdCommonInit {
     const CONSENSUS_VERSION: ModuleConsensusVersion = MODULE_CONSENSUS_VERSION;
     const KIND: ModuleKind = KIND;
 
-    type ClientConfig = DummyClientConfig;
+    type ClientConfig = FedimintdClientConfig;
 
     fn decoder() -> Decoder {
         FedimintdModuleTypes::decoder_builder().build()
     }
 }
 
-// pub const MODULE_CONSENSUS_VERSION: ModuleConsensusVersion =
-// ModuleConsensusVersion::new(2, 0);
+// -------------------------------------------------------
+
+impl fmt::Display for FedimintdClientConfig {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "FedimintdClientConfig")
+    }
+}
+
+// -------------------------------------------------------
+// config.rs
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FedimintdGenParams {
+    pub local: FedimintdGenParamsLocal,
+    pub consensus: FedimintdGenParamsConsensus,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FedimintdGenParamsLocal;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FedimintdGenParamsConsensus {
+    pub tx_fee: Amount,
+}
+
+impl Default for FedimintdGenParams {
+    fn default() -> Self {
+        Self {
+            local: FedimintdGenParamsLocal,
+            consensus: FedimintdGenParamsConsensus {
+                tx_fee: Amount::ZERO,
+            },
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct FedimintdConfig {
+    pub local: FedimintdConfigLocal,
+    pub private: FedimintdConfigPrivate,
+    pub consensus: FedimintdConfigConsensus,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, Encodable, Decodable, Hash)]
+pub struct FedimintdClientConfig {
+    /// Accessible to clients
+    pub tx_fee: Amount,
+}
+
+/// Locally unencrypted config unique to each member
+#[derive(Clone, Debug, Serialize, Deserialize, Decodable, Encodable)]
+pub struct FedimintdConfigLocal;
+
+/// Will be the same for every federation member
+#[derive(Clone, Debug, Serialize, Deserialize, Decodable, Encodable)]
+pub struct FedimintdConfigConsensus {
+    /// Will be the same for all peers
+    pub tx_fee: Amount,
+}
+
+/// Will be encrypted and not shared such as private key material
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct FedimintdConfigPrivate;
+
+plugin_types_trait_impl_config!(
+    FedimintdCommonInit,
+    FedimintdGenParams,
+    FedimintdGenParamsLocal,
+    FedimintdGenParamsConsensus,
+    FedimintdConfig,
+    FedimintdConfigLocal,
+    FedimintdConfigPrivate,
+    FedimintdConfigConsensus,
+    FedimintdClientConfig
+);
+// -------------------------------------------------------
 
 pub fn default_esplora_server(network: Network) -> BitcoinRpcConfig {
     let url = match network {
