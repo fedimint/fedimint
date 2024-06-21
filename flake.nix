922fb98fbac1f810f3dc912c1f001087ed1a4ca1
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-06-21 15:26:35 -0700

    chore: update nixpkgs to 24.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-23.11";
+      url = "github:nixos/nixpkgs/nixos-24.05";
     };
     flake-utils.url = "github:numtide/flake-utils";
     fenix = {
