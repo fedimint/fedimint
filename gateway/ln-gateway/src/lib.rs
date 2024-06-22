746592f60f52549b6047b3129138902cc0a481be
Tommy Volk <tvolk131@gmail.com>
2024-06-22 18:46:30 -0500

    chore: split gateway config structs into separate file

--- gateway/ln-gateway/src/lib.rs
+++ gateway/ln-gateway/src/lib.rs
@@ -16,6 +16,7 @@
 #![allow(clippy::wildcard_imports)]
 
 pub mod client;
+mod config;
 mod db;
 pub mod envs;
 mod federation_manager;
@@ -35,7 +36,6 @@ use std::env;
 use std::fmt::Display;
 use std::net::SocketAddr;
 use std::ops::ControlFlow;
-use std::path::PathBuf;
 use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
@@ -48,6 +48,8 @@ use bitcoin::{Address, Network, Txid};
 use bitcoin_hashes::sha256;
 use clap::Parser;
 use client::GatewayClientBuilder;
+use config::GatewayOpts;
+pub use config::GatewayParameters;
 use db::{
     GatewayConfiguration, GatewayConfigurationKey, GatewayDbtxNcExt, GATEWAYD_DATABASE_VERSION,
 };
@@ -88,7 +90,7 @@ use futures::stream::StreamExt;
 use gateway_lnrpc::intercept_htlc_response::{Action, Cancel};
 use gateway_lnrpc::{CloseChannelsWithPeerResponse, InterceptHtlcResponse};
 use hex::ToHex;
-use lightning::{ILnRpcClient, LightningBuilder, LightningMode, LightningRpcError};
+use lightning::{ILnRpcClient, LightningBuilder, LightningRpcError};
 use lightning_invoice::{Bolt11Invoice, RoutingFees};
 use rand::Rng;
 use rpc::{
@@ -152,85 +154,6 @@ const DEFAULT_MODULE_KINDS: [(ModuleInstanceId, &ModuleKind); 2] = [
     (LEGACY_HARDCODED_INSTANCE_ID_MINT, &MintCommonInit::KIND),
     (LEGACY_HARDCODED_INSTANCE_ID_WALLET, &WalletCommonInit::KIND),
 ];
-
-/// Command line parameters for starting the gateway. `mode`, `data_dir`,
-/// `listen`, and `api_addr` are all required.
-#[derive(Parser)]
-#[command(version)]
-struct GatewayOpts {
-    #[clap(subcommand)]
-    mode: LightningMode,
-
-    /// Path to folder containing gateway config and data files
-    #[arg(long = "data-dir", env = envs::FM_GATEWAY_DATA_DIR_ENV)]
-    data_dir: PathBuf,
-
-    /// Gateway webserver listen address
-    #[arg(long = "listen", env = envs::FM_GATEWAY_LISTEN_ADDR_ENV)]
-    listen: SocketAddr,
-
-    /// Public URL from which the webserver API is reachable
-    #[arg(long = "api-addr", env = envs::FM_GATEWAY_API_ADDR_ENV)]
-    api_addr: SafeUrl,
-
-    /// Gateway webserver authentication password
-    #[arg(long = "password", env = envs::FM_GATEWAY_PASSWORD_ENV)]
-    password: Option<String>,
-
-    /// Bitcoin network this gateway will be running on
-    #[arg(long = "network", env = envs::FM_GATEWAY_NETWORK_ENV)]
-    network: Option<Network>,
-
-    /// Configured gateway routing fees
-    /// Format: <base_msat>,<proportional_millionths>
-    #[arg(long = "fees", env = envs::FM_GATEWAY_FEES_ENV)]
-    fees: Option<GatewayFee>,
-
-    /// Number of route hints to return in invoices
-    #[arg(
-        long = "num-route-hints",
-        env = envs::FM_NUMBER_OF_ROUTE_HINTS_ENV,
-        default_value_t = DEFAULT_NUM_ROUTE_HINTS
-    )]
-    num_route_hints: u32,
-}
-
-impl GatewayOpts {
-    /// Converts the command line parameters into a helper struct the Gateway
-    /// uses to store runtime parameters.
-    fn to_gateway_parameters(&self) -> anyhow::Result<GatewayParameters> {
-        let versioned_api = self.api_addr.join(V1_API_ENDPOINT).map_err(|e| {
-            anyhow::anyhow!(
-                "Failed to version gateway API address: {api_addr:?}, error: {e:?}",
-                api_addr = self.api_addr,
-            )
-        })?;
-        Ok(GatewayParameters {
-            listen: self.listen,
-            versioned_api,
-            password: self.password.clone(),
-            network: self.network,
-            num_route_hints: self.num_route_hints,
-            fees: self.fees.clone(),
-        })
-    }
-}
-
-/// `GatewayParameters` is a helper struct that can be derived from
-/// `GatewayOpts` that holds the CLI or environment variables that are specified
-/// by the user.
-///
-/// If `GatewayConfiguration is set in the database, that takes precedence and
-/// the optional parameters will have no affect.
-#[derive(Clone, Debug)]
-pub struct GatewayParameters {
-    listen: SocketAddr,
-    versioned_api: SafeUrl,
-    password: Option<String>,
-    network: Option<Network>,
-    num_route_hints: u32,
-    fees: Option<GatewayFee>,
-}
 
 #[cfg_attr(doc, aquamarine::aquamarine)]
 /// ```mermaid
