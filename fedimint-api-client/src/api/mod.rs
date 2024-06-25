8cca6d251aeef94c36ce8464aa98094367e6086f
Leonardo Lima <oleonardolima@users.noreply.github.com>
2024-06-25 16:07:49 -0300

    refactor(api)!: add TODO's and comments about using `Connector`
    - add TODO's for DKG, server and ln-gateway `Connector::default()`
      usage.

--- fedimint-api-client/src/api/mod.rs
+++ fedimint-api-client/src/api/mod.rs
@@ -365,9 +365,10 @@ impl AsRef<dyn IGlobalFederationApi + 'static> for DynGlobalApi {
     }
 }
 
-// FIXME: (@leonardo) how should this handle the different [`Connector`]'s ?
-// Using [`Connector::default()`] for now.
 impl DynGlobalApi {
+    // FIXME: (@leonardo) Should we have the option to do DKG and config related
+    // actions through Tor ? Should we add the `Connector` choice to
+    // ConfigParams then ?
     pub fn from_pre_peer_id_admin_endpoint(url: SafeUrl, api_secret: &Option<String>) -> Self {
         // PeerIds are used only for informational purposes, but just in case, make a
         // big number so it stands out
