f4e414abef1d6889b1f76c70485e2b52a047323e
Leonardo Lima <oleonardolima@users.noreply.github.com>
2024-06-26 16:07:02 -0300

    chore(net): and and use `tor()` method instead of variant

--- fedimint-cli/src/lib.rs
+++ fedimint-cli/src/lib.rs
@@ -283,7 +283,7 @@ impl Opts {
 
     fn connector(&self) -> Connector {
         if self.use_tor {
-            Connector::Tor
+            Connector::tor()
         } else {
             Connector::default()
         }
