d8d925d89b52dc191a0645386fd6e29c48716368
Tommy Volk <tvolk131@gmail.com>
2024-06-28 10:18:42 -0500

    chore: add FederationManager::has_federation

--- gateway/ln-gateway/src/lib.rs
+++ gateway/ln-gateway/src/lib.rs
@@ -1027,14 +1027,7 @@ impl Gateway {
         let _join_federation = self.client_joining_lock.lock().await;
 
         // Check if this federation has already been registered
-        if self
-            .federation_manager
-            .clients
-            .read()
-            .await
-            .get(&federation_id)
-            .is_some()
-        {
+        if self.federation_manager.has_federation(federation_id).await {
             return Err(GatewayError::FederationAlreadyConnected);
         }
 
