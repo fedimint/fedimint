6767092a4fc94354c5991f12c32b73b5761c346c
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-07-11 16:18:05 -0700

    chore: fix some clippy warnings

--- modules/fedimint-lnv2-tests/src/bin/tests.rs
+++ modules/fedimint-lnv2-tests/src/bin/tests.rs
@@ -81,7 +81,7 @@ async fn test_self_payment(dev_fed: &DevJitFed) -> anyhow::Result<()> {
 
 async fn fetch_invoice(
     client: &Client,
-    gw_address: &String,
+    gw_address: &str,
 ) -> anyhow::Result<(Bolt11Invoice, OperationId)> {
     Ok(serde_json::from_value::<(Bolt11Invoice, OperationId)>(
         cmd!(
@@ -89,7 +89,7 @@ async fn fetch_invoice(
             "module",
             "lnv2",
             "receive",
-            gw_address.clone(),
+            gw_address.to_owned(),
             "1000000"
         )
         .out_json()
