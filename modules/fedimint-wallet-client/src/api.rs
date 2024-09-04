56e308d2cc9d2d37f4cee3ae13ff88c5c90dedaf
Brad Stachurski <bradley.stachurski@gmail.com>
2024-09-04 15:36:20 +0000

    feat(wallet): add cli commands for bitcoin rpc kind and config

--- modules/fedimint-wallet-client/src/api.rs
+++ modules/fedimint-wallet-client/src/api.rs
@@ -1,19 +1,29 @@
+use std::time::Duration;
+
 use bitcoin::Address;
 use fedimint_api_client::api::{FederationApiExt, FederationResult, IModuleFederationApi};
-use fedimint_core::module::ApiRequestErased;
+use fedimint_core::envs::BitcoinRpcConfig;
+use fedimint_core::module::{ApiAuth, ApiRequestErased};
 use fedimint_core::task::{MaybeSend, MaybeSync};
-use fedimint_core::{apply, async_trait_maybe_send};
-use fedimint_wallet_common::endpoint_constants::{BLOCK_COUNT_ENDPOINT, PEG_OUT_FEES_ENDPOINT};
+use fedimint_core::{apply, async_trait_maybe_send, PeerId};
+use fedimint_wallet_common::endpoint_constants::{
+    BITCOIN_KIND_ENDPOINT, BITCOIN_RPC_CONFIG_ENDPOINT, BLOCK_COUNT_ENDPOINT, PEG_OUT_FEES_ENDPOINT,
+};
 use fedimint_wallet_common::PegOutFees;
 
 #[apply(async_trait_maybe_send!)]
 pub trait WalletFederationApi {
     async fn fetch_consensus_block_count(&self) -> FederationResult<u64>;
+
     async fn fetch_peg_out_fees(
         &self,
         address: &Address,
         amount: bitcoin::Amount,
     ) -> FederationResult<Option<PegOutFees>>;
+
+    async fn fetch_bitcoin_rpc_kind(&self, peer_id: PeerId) -> FederationResult<String>;
+
+    async fn fetch_bitcoin_rpc_config(&self, auth: ApiAuth) -> FederationResult<BitcoinRpcConfig>;
 }
 
 #[apply(async_trait_maybe_send!)]
@@ -37,6 +47,25 @@ where
         self.request_current_consensus(
             PEG_OUT_FEES_ENDPOINT.to_string(),
             ApiRequestErased::new((address, amount.to_sat())),
+        )
+        .await
+    }
+
+    async fn fetch_bitcoin_rpc_kind(&self, peer_id: PeerId) -> FederationResult<String> {
+        self.request_single_peer_federation(
+            Some(Duration::from_secs(10)),
+            BITCOIN_KIND_ENDPOINT.to_string(),
+            ApiRequestErased::default(),
+            peer_id,
+        )
+        .await
+    }
+
+    async fn fetch_bitcoin_rpc_config(&self, auth: ApiAuth) -> FederationResult<BitcoinRpcConfig> {
+        self.request_admin(
+            BITCOIN_RPC_CONFIG_ENDPOINT,
+            ApiRequestErased::default(),
+            auth,
         )
         .await
     }
