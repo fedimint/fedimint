be9593d64066b6035709d57b9116a90e2918f87a
Justin Moeller <jumoell@protonmail.com>
2024-09-24 15:38:50 -0500

    fix: devimint changes for backwards compatibility

--- devimint/src/devfed.rs
+++ devimint/src/devfed.rs
@@ -218,8 +218,12 @@ impl DevJitFed {
             let esplora = esplora.clone();
             let process_mgr = process_mgr.to_owned();
             move || async move {
+                // TODO(support:v0.4.0): Only run LDK gateway when the federation supports LNv2
+                let fedimintd_version = crate::util::FedimintdCmd::version_or_default().await;
                 let gatewayd_version = crate::util::Gatewayd::version_or_default().await;
-                if gatewayd_version >= *VERSION_0_5_0_ALPHA {
+                if gatewayd_version >= *VERSION_0_5_0_ALPHA
+                    && fedimintd_version >= *VERSION_0_4_0_ALPHA
+                {
                     esplora.get_try().await?;
                     Ok(Arc::new(Some(
                         Gatewayd::new(&process_mgr, LightningNode::Ldk).await?,
@@ -233,11 +237,15 @@ impl DevJitFed {
             let gw_ldk = gw_ldk.clone();
             let fed = fed.clone();
             move || async move {
-                let gw_ldk = gw_ldk.get_try().await?.deref();
-                if let Some(gw_ldk) = gw_ldk {
-                    let fed = fed.get_try().await?.deref();
-                    if !skip_setup {
-                        gw_ldk.connect_fed(fed).await?;
+                // TODO(support:v0.4.0): Only run LDK gateway when the federation supports LNv2
+                let fedimintd_version = crate::util::FedimintdCmd::version_or_default().await;
+                if fedimintd_version >= *VERSION_0_4_0_ALPHA {
+                    let gw_ldk = gw_ldk.get_try().await?.deref();
+                    if let Some(gw_ldk) = gw_ldk {
+                        let fed = fed.get_try().await?.deref();
+                        if !skip_setup {
+                            gw_ldk.connect_fed(fed).await?;
+                        }
                     }
                 }
                 Ok(Arc::new(()))
@@ -259,11 +267,13 @@ impl DevJitFed {
 
                 let gateway_cli_version = crate::util::GatewayCli::version_or_default().await;
                 let gatewayd_version = crate::util::Gatewayd::version_or_default().await;
+                let fedimintd_version = crate::util::FedimintdCmd::version_or_default().await;
 
                 let bitcoind = bitcoind.get_try().await?.deref().clone();
 
                 if gateway_cli_version < *VERSION_0_4_0_ALPHA
                     || gatewayd_version < *VERSION_0_4_0_ALPHA
+                    || fedimintd_version < *VERSION_0_4_0_ALPHA
                 {
                     let lnd = lnd.get_try().await?.deref().clone();
                     let cln = cln.get_try().await?.deref().clone();
