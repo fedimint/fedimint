93214fffe995d8f4d116317037d3345b2f3defa8
Tommy Volk <tvolk131@gmail.com>
2024-10-15 17:57:30 +0100

    chore: devimint logs channel open success/failure

--- devimint/src/external.rs
+++ devimint/src/external.rs
@@ -907,18 +907,26 @@ pub async fn open_channels_between_gateways(
     let open_channel_tasks = gateway_pairs.iter()
         .map(|((gw_a, gw_a_name), (gw_b, gw_b_name))| {
             let gw_a = (*gw_a).clone();
-            let gw_a_name = gw_a_name.to_string();
+            let gw_a_name = (*gw_a_name).to_string();
             let gw_b = (*gw_b).clone();
-            let gw_b_name = gw_b_name.to_string();
+            let gw_b_name = (*gw_b_name).to_string();
 
             let sats_per_side = 5_000_000;
             info!(target: LOG_DEVIMINT, "Opening channel from {gw_a_name} gateway to {gw_b_name} gateway with {sats_per_side} sats on each side...");
             tokio::task::spawn(async move {
                 // Sometimes channel openings just after funding the lightning nodes don't work right away.
-                poll_with_timeout(&format!("Open channel from {gw_a_name} to {gw_b_name}"), Duration::from_secs(30), || async {
+                let res = poll_with_timeout(&format!("Open channel from {gw_a_name} to {gw_b_name}"), Duration::from_secs(30), || async {
                     gw_a.open_channel(&gw_b, sats_per_side * 2, Some(sats_per_side)).await.map_err(ControlFlow::Continue)
                 })
-                .await
+                .await;
+
+                if res.is_ok() {
+                    info!(target: LOG_DEVIMINT, "Opened channel from {gw_a_name} gateway to {gw_b_name} gateway");
+                } else {
+                    info!(target: LOG_DEVIMINT, "Failed to open channel from {gw_a_name} gateway to {gw_b_name} gateway");
+                }
+
+                res
             })
         })
         .collect::<Vec<_>>();
