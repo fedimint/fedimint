60642992f9a06ab7750d36d1015b8d7fbf4a587f
Tommy Volk <tvolk131@gmail.com>
2024-10-22 11:00:20 +0100

    chore: revert additions

--- gateway/ln-gateway/src/lightning/ldk.rs
+++ gateway/ln-gateway/src/lightning/ldk.rs
@@ -453,22 +453,6 @@ impl ILnRpcClient for GatewayLdkClient {
         channel_size_sats: u64,
         push_amount_sats: u64,
     ) -> Result<OpenChannelResponse, LightningRpcError> {
-        let funding_txid_or = self
-            .node
-            .list_channels()
-            .iter()
-            .find(|channel| {
-                channel.counterparty_node_id == bitcoin30_to_bitcoin32_secp256k1_pubkey(&pubkey)
-            })
-            .and_then(|channel| channel.funding_txo)
-            .map(|funding_txo| funding_txo.txid);
-
-        if let Some(funding_txid) = funding_txid_or {
-            return Ok(OpenChannelResponse {
-                funding_txid: funding_txid.to_string(),
-            });
-        }
-
         let push_amount_msats_or = if push_amount_sats == 0 {
             None
         } else {
