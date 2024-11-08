941960b5e45bedb3c7aab68b92fffe9e3e8b73f9
Justin Moeller <jumoell@protonmail.com>
2024-11-08 09:36:06 -0600

    refactor: move endpoint constants to proper crates

--- modules/fedimint-lnv2-common/src/endpoint_constants.rs
+++ modules/fedimint-lnv2-common/src/endpoint_constants.rs
@@ -1,13 +1,13 @@
+// Federation endpoints
 pub const ADD_GATEWAY_ENDPOINT: &str = "add_gateway";
 pub const AWAIT_INCOMING_CONTRACT_ENDPOINT: &str = "await_incoming_contract";
 pub const AWAIT_PREIMAGE_ENDPOINT: &str = "await_preimage";
 pub const CONSENSUS_BLOCK_COUNT_ENDPOINT: &str = "consensus_block_count";
-pub const CREATE_BOLT11_INVOICE_ENDPOINT: &str = "/create_bolt11_invoice";
-pub const CREATE_BOLT11_INVOICE_FOR_OPERATOR_ENDPOINT: &str = "/create_bolt11_invoice_for_operator";
 pub const GATEWAYS_ENDPOINT: &str = "gateways";
 pub const OUTGOING_CONTRACT_EXPIRATION_ENDPOINT: &str = "outgoing_contract_expiration";
-pub const PAY_INVOICE_FOR_OPERATOR_ENDPOINT: &str = "/pay_invoice_for_operator";
 pub const REMOVE_GATEWAY_ENDPOINT: &str = "remove_gateway";
+
+// Gateway endpoints
+pub const CREATE_BOLT11_INVOICE_ENDPOINT: &str = "/create_bolt11_invoice";
 pub const ROUTING_INFO_ENDPOINT: &str = "/routing_info";
 pub const SEND_PAYMENT_ENDPOINT: &str = "/send_payment";
-pub const SEND_ONCHAIN_ENDPOINT: &str = "/send_onchain";
