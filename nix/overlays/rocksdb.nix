5d7e7ad5bcf494c02b6b9df6935670785c8a7ca1
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-11-12 16:00:06 -0800

    chore: update rocksdb 8.10.0 -> 8.10.2

--- nix/overlays/rocksdb.nix
+++ nix/overlays/rocksdb.nix
@@ -1,13 +1,13 @@
 final: prev: {
 
   rocksdb = prev.rocksdb.overrideAttrs (oldAttrs: rec {
-    version = "8.10.0";
+    version = "8.10.2";
 
     src = final.fetchFromGitHub {
       owner = "facebook";
       repo = oldAttrs.pname;
       rev = "v${version}";
-      hash = "sha256-KGsYDBc1fz/90YYNGwlZ0LUKXYsP1zyhP29TnRQwgjQ=";
+      hash = "sha256-96c1coG2euj8yWpSr3MYPEjxfo/7wuQgk0G+UBr3m0Q=";
     };
   });
 }
