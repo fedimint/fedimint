471b18a1bcbcb03948167607461b933dbdec2448
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-11-14 10:58:37 -0800

    chore: make fedimint-cli use jemalloc

--- fedimint-cli/src/main.rs
+++ fedimint-cli/src/main.rs
@@ -1,5 +1,12 @@
 use fedimint_cli::FedimintCli;
 use fedimint_core::fedimint_build_code_version_env;
+#[cfg(not(target_env = "msvc"))]
+use jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+// rocksdb suffers from memory fragmentation when using standard allocator
+static GLOBAL: Jemalloc = Jemalloc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
