50f17b03071063d84229768c29c6a3349ec2a752
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-11-13 23:54:17 -0800

    chore: make fedimintd use jemalloc
    This is said to improve rocksdb memory usage
    caused by memory fragmentation.

--- fedimintd/src/bin/main.rs
+++ fedimintd/src/bin/main.rs
@@ -1,5 +1,12 @@
 use fedimint_core::fedimint_build_code_version_env;
 use fedimintd::Fedimintd;
+#[cfg(not(target_env = "msvc"))]
+use jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+// rocksdb suffers from memory fragmentation when using standard allocator
+static GLOBAL: Jemalloc = Jemalloc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
