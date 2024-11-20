d88fec95fd778edd90558103e8d9d176926632a9
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-11-19 16:24:57 -0800

    chore(ci): CI can work faster now that some flakes are fixed

--- scripts/tests/test-ci-all.sh
+++ scripts/tests/test-ci-all.sh
@@ -347,9 +347,7 @@ if [ -n "${FM_TEST_CI_ALL_JOBS:-}" ]; then
   # when specifically set, use the env var
   parallel_args+=(--jobs "${FM_TEST_CI_ALL_JOBS}")
 elif [ -n "${CI:-}" ] || [ "${CARGO_PROFILE:-}" == "ci" ]; then
-  # our CI runners have 96 CPUs, which is a lot of CPUs, and a lot of jobs
-  # to run in parallel
-  parallel_args+=(--jobs $(($(nproc) / 3 + 1)))
+  parallel_args+=(--jobs $(($(nproc) / 2 + 1)))
 else
   # on dev computers default to `num_cpus / 2 + 1` max parallel jobs
   parallel_args+=(--jobs "${FM_TEST_CI_ALL_JOBS:-$(($(nproc) / 2 + 1))}")
@@ -357,7 +355,8 @@ fi
 
 parallel_args+=(--timeout "${FM_TEST_CI_ALL_TIMEOUT:-360}")
 
-parallel_args+=(--load "${FM_TEST_CI_ALL_MAX_LOAD:-$(($(nproc) / 3 + 1))}")
+parallel_args+=(--load "${FM_TEST_CI_ALL_MAX_LOAD:-$(($(nproc)))}")
+
 # --delay to let nix start extracting and bump the load
 # usually not needed, as '--jobs' will keep a cap on the load anyway
 parallel_args+=(--delay "${FM_TEST_CI_ALL_DELAY:-0}")
