e12936f5a2ca3575847161c59d43a8d5c57ecaac
Brad Stachurski <bradley.stachurski@gmail.com>
2024-12-23 07:50:00 -0700

    wip: run multiple paths in one upgrade test run
    - needs some more thought, but initial results look promising
    - kind of a bummer TEST_KINDS doesn't work as well, but complexity is
      starting to bloat

--- test.sh
+++ test.sh
@@ -7,23 +7,36 @@
 # done
 
 
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.3.3 current &> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.3.4-rc.1 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.0 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.1 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.2 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.3 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.4 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.5.0 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.3.3 v0.4.0 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 TEST_KINDS=fedimint-cli,gateway just test-upgrades v0.3.1 current &>> results.log
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.3.3 current &> results.log
+# sleep 60
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.3.4-rc.1 current &>> results.log
+# sleep 60
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.0 current &>> results.log
+# sleep 60
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.1 current &>> results.log
+# sleep 60
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.2 current &>> results.log
+# sleep 60
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.3 current &>> results.log
+# sleep 60
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.4 current &>> results.log
+# sleep 60
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.5.0 current &>> results.log
+# sleep 60
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.3.3 v0.4.0 current &>> results.log
+# sleep 60
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 TEST_KINDS=fedimint-cli,gateway just test-upgrades v0.3.1 current &>> results.log
 
+rm results.log
+
+start=$(date +%s)  # Record start time in nanoseconds
+
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.3.4-rc.1 current &>> results.log
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.4 current &>> results.log
+# FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.5.0 current &>> results.log
+
+FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades "v0.3.4-rc.1 current, v0.4.4 current, v0.5.0 current" &> results.log
+
+end=$(date +%s)    # Record end time in nanoseconds
+runtime=$((end - start)) # Calculate the difference
+echo "Runtime: $runtime seconds" >> results.log
