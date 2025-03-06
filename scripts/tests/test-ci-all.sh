b5ece6c9b9bc7d94a65c571ae4b215e460e1d699
Brad Stachurski <bradley.stachurski@gmail.com>
2025-03-05 19:34:44 -0700

    demo: run back-compat and upgrade tests vs v0.6.1-rc.0

--- scripts/tests/test-ci-all.sh
+++ scripts/tests/test-ci-all.sh
@@ -262,7 +262,7 @@ else
   tagged_versions=("$@")
 fi
 num_versions="$#"
-versions=( "${tagged_versions[@]}" "current" )
+versions=( "${tagged_versions[@]}" )
 if [[ "$num_versions" == "0" ]]; then
   mapfile -t version_matrix < <(generate_current_only_matrix "${versions[@]}")
 else
@@ -361,6 +361,7 @@ for version_combo in "${version_matrix[@]}"; do
       tests_with_versions+=("run_test_for_versions $test $version_combo $lnv2")
     done
   done
+
 done
 
 parsed_test_commands=$(printf "%s\n" "${tests_with_versions[@]}")
@@ -396,7 +397,7 @@ joblog="$tmpdir/joblog"
 PATH="$(pwd)/scripts/dev/run-test/:$PATH"
 
 parallel_args+=(
-  --halt-on-error 1
+  --retries 5
   --joblog "$joblog"
   --noswap
   --memfree 2G
