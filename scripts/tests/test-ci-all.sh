e52e8dd483c78cf3a372deeaa2e09d373e21236f
Brad Stachurski <bradley.stachurski@gmail.com>
2025-04-02 00:00:16 -0600

    demo: run back-compat and upgrade tests vs v0.7.0-beta.0

--- scripts/tests/test-ci-all.sh
+++ scripts/tests/test-ci-all.sh
@@ -267,7 +267,7 @@ else
   tagged_versions=("$@")
 fi
 num_versions="$#"
-versions=( "${tagged_versions[@]}" "current" )
+versions=( "${tagged_versions[@]}" )
 if [[ "$num_versions" == "0" ]]; then
   mapfile -t version_matrix < <(generate_current_only_matrix "${versions[@]}")
 else
@@ -401,6 +401,7 @@ joblog="$tmpdir/joblog"
 
 PATH="$(pwd)/scripts/dev/run-test/:$PATH"
 
+# may need to reintroduce retries
 parallel_args+=(
   --halt-on-error 1
   --joblog "$joblog"
