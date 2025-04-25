bd3828144f77a40247946d0c8cf987d4fca8e05a
joschisan <joschisan@MacBook-Pro-von-Joscha.local>
2025-04-25 07:11:07 +0200

    test: only poll once a second

--- fedimint-server/src/consensus/mod.rs
+++ fedimint-server/src/consensus/mod.rs
@@ -102,11 +102,7 @@ pub async fn run(
 
     let server_bitcoin_rpc_monitor = ServerBitcoinRpcMonitor::new(
         dyn_server_bitcoin_rpc,
-        if is_running_in_test_env() {
-            Duration::from_millis(100)
-        } else {
-            Duration::from_secs(60)
-        },
+        Duration::from_secs(if is_running_in_test_env() { 1 } else { 60 }),
         task_group,
     );
 
