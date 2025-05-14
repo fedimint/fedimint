1f7ebf95293e4beb6cc040d3a0a8a3e490f4994e
maan2003 <manmeetmann2003@gmail.com>
2025-05-14 23:20:36 +0530

    chore: replace our custom runtime abstract with n0-future

--- fedimint-core/src/time.rs
+++ fedimint-core/src/time.rs
@@ -1,16 +1,7 @@
-// nosemgrep: ban-system-time-now
-use std::time::SystemTime;
+use n0_future::time::SystemTime;
 
-#[cfg(not(target_family = "wasm"))]
 pub fn now() -> SystemTime {
-    // nosemgrep: ban-system-time-now
     SystemTime::now()
-}
-
-#[cfg(target_family = "wasm")]
-pub fn now() -> SystemTime {
-    SystemTime::UNIX_EPOCH
-        + std::time::Duration::from_secs_f64(js_sys::Date::new_0().get_time() / 1000.)
 }
 
 /// Returns the duration since the Unix epoch
