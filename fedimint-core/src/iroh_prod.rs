515c794ac4daadd4e449376802f47c2f51cd7c30
elsirion <elsirion@protonmail.com>
2025-07-08 17:45:43 +0200

    fix: use correct iroh pkarr URL

--- fedimint-core/src/iroh_prod.rs
+++ fedimint-core/src/iroh_prod.rs
@@ -1,7 +1,7 @@
 /// The Iroh/Pkarr DNS server hosted by Fedimint project
 pub const FM_DNS_PKARR_RELAY_PROD: [&str; 2] = [
-    "https://dns.irohdns-eu-01.dev.fedimint.org",
-    "https://dns.irohdns-us-01.dev.fedimint.org",
+    "https://dns.irohdns-eu-01.dev.fedimint.org/pkarr",
+    "https://dns.irohdns-us-01.dev.fedimint.org/pkarr",
 ];
 
 /// The Iroh relays hosted by Fedimint project
