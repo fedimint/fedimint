3f6e69e0b22246143675a47c6a190221e9f2ad5e
Dawid Ciężarkiewicz <dpc@dpc.pw>
2025-11-10 12:40:55 -0800

    chore(tests): federation setup with more guardians

---
+++ scripts/tests/large-setup-test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+set -euo pipefail
+export RUST_LOG="${RUST_LOG:-info}"
+
+source scripts/_common.sh
+build_workspace
+add_target_dir_to_path
+make_fm_test_marker
+
+export FM_FED_SIZE=$(((RANDOM % 10) + 4))
+
+>&2 echo "Testing ${FM_FED_SIZE} peer dkg"
+
+env RUST_LOG="${RUST_LOG:-info,jsonrpsee-client=off}" \
+  devimint "$@" dev-fed \
+    --exec true
