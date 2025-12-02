6d985b7031b7d8c538baf27f6e3b4e64129d3f6f
user <benalleng@gmail.com>
2025-12-02 09:01:59 -0500

    chore: Update flake to nixos stable 25.11

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
   inputs = {
     nixpkgs = {
-      # TODO: nixos-25.11, which should be soon(TM)
-      url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "github:nixos/nixpkgs/nixos-25.11";
     };
     nixpkgs-unstable = {
       url = "github:nixos/nixpkgs/nixos-unstable";
