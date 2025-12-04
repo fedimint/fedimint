0cb50e64401ae5bf05bd01368ff37d62827f55b8
Dawid Ciężarkiewicz <dpc@dpc.pw>
2025-12-04 13:48:20 -0800

    fix(dev): apply cargo-llvm-cov workaround in an overlay
    This way it is available for all derivations.

--- flake.nix
+++ flake.nix
@@ -86,6 +86,13 @@
             (final: prev: {
               cargo-deluxe = cargo-deluxe.packages.${system}.default;
               cargo-udeps = nixpkgs-unstable.legacyPackages.${system}.cargo-udeps;
+
+              cargo-llvm-cov = (
+                # https://github.com/NixOS/nixpkgs/issues/467882
+                prev.cargo-llvm-cov.overrideAttrs (old: {
+                  doCheck = false;
+                })
+              );
             })
           ];
         };
@@ -334,9 +341,7 @@
                     pkgs.cargo-workspaces
 
                     # marked as broken on MacOS
-                    (pkgs.cargo-llvm-cov.overrideAttrs (old: {
-                      doCheck = false;
-                    }))
+                    pkgs.cargo-llvm-cov
                   ];
 
                 shellHook = ''
