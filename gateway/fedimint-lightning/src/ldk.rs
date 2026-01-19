e15a87d9c8f1e19dc42199138b245fba01b7da23
m1sterc001guy <jumoell@protonmail.com>
2026-01-19 09:33:12 -0600

    fix: use default esplora sync config

--- gateway/fedimint-lightning/src/ldk.rs
+++ gateway/fedimint-lightning/src/ldk.rs
@@ -16,7 +16,6 @@ use fedimint_gateway_common::{
 };
 use fedimint_ln_common::contracts::Preimage;
 use fedimint_logging::LOG_LIGHTNING;
-use ldk_node::config::{BackgroundSyncConfig, EsploraSyncConfig};
 use ldk_node::lightning::ln::msgs::SocketAddress;
 use ldk_node::lightning::routing::gossip::NodeAlias;
 use ldk_node::payment::{PaymentDirection, PaymentKind, PaymentStatus, SendingParameters};
@@ -129,16 +128,7 @@ impl GatewayLdkClient {
                 );
             }
             ChainSource::Esplora { server_url } => {
-                node_builder.set_chain_source_esplora(
-                    get_esplora_url(server_url)?,
-                    Some(EsploraSyncConfig {
-                        background_sync_config: Some(BackgroundSyncConfig {
-                            onchain_wallet_sync_interval_secs: 30,
-                            lightning_wallet_sync_interval_secs: 30,
-                            fee_rate_cache_update_interval_secs: 30,
-                        }),
-                    }),
-                );
+                node_builder.set_chain_source_esplora(get_esplora_url(server_url)?, None);
             }
         };
         let Some(data_dir_str) = data_dir.to_str() else {
